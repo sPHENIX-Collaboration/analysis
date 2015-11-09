77ac58c58e591ffcb17002c68d63cc9030ce6162
Jin Huang <jhuang@bnl.gov>
2015-11-09 17:25:43 -0500

    Update path for include file getClass.h

--- EMCal-analysis/EMCalAna/EMCalAna.C
+++ EMCal-analysis/EMCalAna/EMCalAna.C
@@ -7,7 +7,7 @@
 #include <fun4all/PHTFileServer.h>
 #include <phool/PHCompositeNode.h>
 #include <fun4all/Fun4AllReturnCodes.h>
-#include <fun4all/getClass.h>
+#include <phool/getClass.h>
 #include <fun4all/Fun4AllHistoManager.h>
 
 #include <phool/PHCompositeNode.h>
