bde83d2ab15d7fa0db017b0c8d28e32614c74740
Ilana Bromberg <ilana.bromberg@stonybrook.edu>
2017-06-22 11:31:12 -0400

    changed material of magnets to galactic from iron

--- EIC-IR/macros_fun4all/G4_IR_EIC.C
+++ EIC-IR/macros_fun4all/G4_IR_EIC.C
@@ -8,30 +8,30 @@ double IRSetup(PHG4Reco* g4Reco,
 	      int verbosity = 0) {
 
   /* Increase world size to fit extended IR magnets */
-  g4Reco->SetWorldSizeZ(20000);
+  g4Reco->SetWorldSizeZ(25000);
 
   /* Define outer radius for all extneded IR magnets */
   double ir_magnet_outer_radius = 50;
 
   /* Read IR configuration file- this needs to go somewhere else using parameters and a .root file to store them*/
-  string irfile = "linac-ring-proton-magnets-Version3.01-21apr2016.dat";
+  string irfile = "updated-magnets-2017.dat";
   ifstream irstream(irfile.c_str());
 
-  while(!irstream.eof()){
+   while(!irstream.eof()){
     string str;
     getline(irstream, str);
     if(str[0] == '#') continue; //for comments
     stringstream ss(str);
 
     string name;
-    double center_x, center_y, center_z, aperture_radius, length, angle, B, gradient;
+    double center_z, center_x, center_y, aperture_radius, length, angle, B, gradient;
 
-    ss >> name >> center_x >> center_y >> center_z >> aperture_radius >> length >> angle >> B >> gradient;
+    ss >> name >> center_z >> center_x >> center_y >> aperture_radius >> length >> angle >> B >> gradient;
 
     if ( name == "" ) continue; //for empty lines
 
     /* convert units from m to cm */
-    center_x *= 100;
+     center_x *= 100;
     center_y *= 100;
     center_z *= 100;
     aperture_radius *= 100;
@@ -40,13 +40,13 @@ double IRSetup(PHG4Reco* g4Reco,
     /* flip sign of dipole field component- positive y axis in Geant4 is defined as 'up',
      * positive z axis  as the hadron-going direction
      * in a right-handed coordinate system x,y,z */
-    B *= -1;
+     B *= -1;
 
     /* convert angle from millirad to degrees */
     angle = (angle / 1000.) * (180./TMath::Pi());
 
     /* Place IR component */
-    cout << "New IR component: " << name << " at z = " << center_z << endl;
+     cout << "New IR component: " << name << " at z = " << center_z << endl;
 
     string volname = "IRMAGNET_";
     volname.append(name);
@@ -60,8 +60,8 @@ double IRSetup(PHG4Reco* g4Reco,
     ir_magnet_i->set_double_param("place_y",center_y);
     ir_magnet_i->set_double_param("place_z",center_z);
     ir_magnet_i->set_double_param("rot_y",angle);
-    //ir_magnet_i->set_string_param("material","G4_Galactic");
-    ir_magnet_i->set_string_param("material","G4_Fe");
+    ir_magnet_i->set_string_param("material","G4_Galactic");
+    //ir_magnet_i->set_string_param("material","G4_Fe");
 
     if ( B != 0 && gradient == 0.0 )
       {
@@ -73,17 +73,23 @@ double IRSetup(PHG4Reco* g4Reco,
 	ir_magnet_i->set_string_param("magtype","quadrupole");
 	ir_magnet_i->set_double_param("fieldgradient",gradient);
       }
+    else if ( B == 0 && gradient == 0.0 )
+      {
+	ir_magnet_i->set_string_param("magtype","dipole");
+	ir_magnet_i->set_double_param("fieldgradient",0);
+	ir_magnet_i->set_double_param("field_y",0);
+      }
     else
       {
 	cout << "Error in G4_IR_EIC: Could not identify magnet type. Abort." << endl;
-	return 1;
+		return 1;
       }
 
     ir_magnet_i->SetActive(false);
     //ir_magnet_i->SuperDetector("FLUXRET_ETA_PLUS");
     ir_magnet_i->OverlapCheck(overlapcheck);
     g4Reco->registerSubsystem(ir_magnet_i);
-  }
+    }
 
   return 0;
 }
