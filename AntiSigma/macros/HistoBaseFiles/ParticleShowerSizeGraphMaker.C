8032ead0d5e1ef5a1c8ebb4d7056b8114dd69527
Chris Pinkenburg <pinkenburg@bnl.gov>
2017-08-12 15:49:24 -0400

    use 4 GeV

--- AntiSigma/macros/HistoBaseFiles/ParticleShowerSizeGraphMaker.C
+++ AntiSigma/macros/HistoBaseFiles/ParticleShowerSizeGraphMaker.C
@@ -2,8 +2,8 @@ void ParticleShowerSizeHistoMaker(const int nevents)
 {
   float min = -0.5;
   float max = 0.5;
-  int nchan = 50;
-   const char *file1 = "/sphenix/user/jpinkenburg/ShowerSize/ntuple/neutron_eta0_12GeV.root";
+  int nchan = 52;
+   const char *file1 = "/sphenix/user/jpinkenburg/ShowerSize/ntuple/neutron_eta0_4GeV.root";
 
   TFile *f1 = TFile::Open(file1);
   if (! f1)
@@ -14,24 +14,24 @@ void ParticleShowerSizeHistoMaker(const int nevents)
     gROOT->cd();
  TNtuple *de1 = (TNtuple *) f1->Get("de");
  TCanvas *c1 = new TCanvas();
- TH2F *h2 = new TH2F("h2","Particle Shower Sizes",nchan,min,max,nchan,min,max);
+ TH2F *h2 = new TH2F("neutron","Particle Shower Sizes",nchan,min,max,nchan,min,max);
   h2->SetXTitle("dtheta");
   h2->SetYTitle("dphi");
-  h2->SetZTitle("Number of Hits");
+  h2->SetZTitle("dE/event");
   h2->GetXaxis()->SetTitleOffset(2);
   h2->GetYaxis()->SetTitleOffset(2);
   h2->GetZaxis()->SetTitleOffset(1.2);
   if (nevents > 0)
   {
-de1->Project("h2","dphi:dtheta","(ID<=2)*edep","",nevents);
+ de1->Project("neutron","dphi:dtheta","(ID<=2)*edep/10000.","",nevents);
   }
   else
   {
-de1->Project("h2","dphi:dtheta","(ID<=2)*edep","");
+ de1->Project("neutron","dphi:dtheta","(ID<=2)*edep/10000.","");
   }
   h2->Draw("surf");
   f1->Close();
-  const char *file2 = "/sphenix/user/jpinkenburg/ShowerSize/ntuple/gamma_eta0_12GeV.root";
+  const char *file2 = "/sphenix/user/jpinkenburg/ShowerSize/ntuple/gamma_eta0_4GeV.root";
   TFile *f2 = TFile::Open(file2);
   if (! f2)
     {
@@ -42,27 +42,27 @@ de1->Project("h2","dphi:dtheta","(ID<=2)*edep","");
  
   TNtuple *de2 = (TNtuple *) f2->Get("de");
   cout << nevents << endl;
-  TH2F *h2a = new TH2F("h2a","Particle Shower Sizes",nchan,min,max,nchan,min,max);
+  TH2F *h2a = new TH2F("gamma","Particle Shower Sizes",nchan,min,max,nchan,min,max);
   h2a->SetXTitle("dtheta");
   h2a->SetYTitle("dphi");
-  h2a->SetZTitle("Number of Hits");
+  h2a->SetZTitle("dE/event");
   h2a->GetXaxis()->SetTitleOffset(2);
   h2a->GetYaxis()->SetTitleOffset(2);
   h2a->GetZaxis()->SetTitleOffset(1.2);
   if (nevents > 0)
   {
-  de2->Project("h2a","dphi:dtheta","(ID<=2)*edep","",nevents);
+  de2->Project("gamma","dphi:dtheta","(ID<=2)*edep/10000.","",nevents);
   }
   else
   {
-  de2->Project("h2a","dphi:dtheta","(ID<=2)*edep","");
+  de2->Project("gamma","dphi:dtheta","(ID<=2)*edep/10000.","");
   }
 
  h2a->SetLineColor(2);
 h2a->Draw("surf same");
 // f1->Close();
   f2->Close();
-  TFile *f3 = TFile::Open("test.root","UPDATE");
+  TFile *f3 = TFile::Open("neutron_gamma_edep_4gev.root","UPDATE");
 
   h2->Write();
   h2a->Write();
