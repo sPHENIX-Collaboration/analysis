8f0cfbbd6a3730feb4841fd659abcf672a6cd49c
Chris Pinkenburg <pinkenburg@bnl.gov>
2017-08-12 14:23:00 -0400

    save h2

--- AntiSigma/macros/HistoBaseFiles/ParticleShowerSizeGraphMaker.C
+++ AntiSigma/macros/HistoBaseFiles/ParticleShowerSizeGraphMaker.C
@@ -1,5 +1,8 @@
 void ParticleShowerSizeHistoMaker(const int nevents)
 {
+  float min = -0.5;
+  float max = 0.5;
+  int nchan = 50;
    const char *file1 = "/sphenix/user/jpinkenburg/ShowerSize/ntuple/neutron_eta0_12GeV.root";
 
   TFile *f1 = TFile::Open(file1);
@@ -11,16 +14,22 @@ void ParticleShowerSizeHistoMaker(const int nevents)
     gROOT->cd();
  TNtuple *de1 = (TNtuple *) f1->Get("de");
  TCanvas *c1 = new TCanvas();
- TH2F *h2 = new TH2F("h2","Particle Shower Sizes",20,-.2,.2,10,-.2,.2);
+ TH2F *h2 = new TH2F("h2","Particle Shower Sizes",nchan,min,max,nchan,min,max);
   h2->SetXTitle("dtheta");
   h2->SetYTitle("dphi");
   h2->SetZTitle("Number of Hits");
   h2->GetXaxis()->SetTitleOffset(2);
   h2->GetYaxis()->SetTitleOffset(2);
   h2->GetZaxis()->SetTitleOffset(1.2);
+  if (nevents > 0)
+  {
 de1->Project("h2","dphi:dtheta","(ID<=2)*edep","",nevents);
+  }
+  else
+  {
+de1->Project("h2","dphi:dtheta","(ID<=2)*edep","");
+  }
   h2->Draw("surf");
-  h2->Write();
   f1->Close();
   const char *file2 = "/sphenix/user/jpinkenburg/ShowerSize/ntuple/gamma_eta0_12GeV.root";
   TFile *f2 = TFile::Open(file2);
@@ -33,20 +42,29 @@ de1->Project("h2","dphi:dtheta","(ID<=2)*edep","",nevents);
  
   TNtuple *de2 = (TNtuple *) f2->Get("de");
   cout << nevents << endl;
-  TH2F *h2a = new TH2F("h2a","Particle Shower Sizes",20,-.2,.2,10,-.2,.2);
+  TH2F *h2a = new TH2F("h2a","Particle Shower Sizes",nchan,min,max,nchan,min,max);
   h2a->SetXTitle("dtheta");
   h2a->SetYTitle("dphi");
   h2a->SetZTitle("Number of Hits");
   h2a->GetXaxis()->SetTitleOffset(2);
   h2a->GetYaxis()->SetTitleOffset(2);
   h2a->GetZaxis()->SetTitleOffset(1.2);
+  if (nevents > 0)
+  {
   de2->Project("h2a","dphi:dtheta","(ID<=2)*edep","",nevents);
+  }
+  else
+  {
+  de2->Project("h2a","dphi:dtheta","(ID<=2)*edep","");
+  }
+
  h2a->SetLineColor(2);
 h2a->Draw("surf same");
 // f1->Close();
   f2->Close();
   TFile *f3 = TFile::Open("test.root","UPDATE");
 
+  h2->Write();
   h2a->Write();
   f3->Write();
   f3->Close();
