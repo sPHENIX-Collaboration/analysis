cca1fa034e403a0c62df3716e1bda5e15ff38a81
Ron Belmont <belmonrj@gmail.com>
2017-10-03 18:37:07 -0400

    Initial commit for Prototype3 2017 analysis (code from Abhisek Sen)

---
+++ Prototype3/HCAL/ShowerCalib/Proto3ShowerCalibLinkDef.h
@@ -0,0 +1,9 @@
+#ifdef __CINT__
+
+#pragma link C++ class Proto3ShowerCalib-!;
+#pragma link C++ class Proto3ShowerCalib::Eval_Run+;
+#pragma link C++ class Proto3ShowerCalib::HCAL_shower+;
+#pragma link C++ class Proto3ShowerCalib::Time_Samples+;
+#pragma link C++ class Proto3ShowerCalib::Scientillators+;
+
+#endif /* __CINT__ */
