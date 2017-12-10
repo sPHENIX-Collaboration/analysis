622f182010dd352074e9d1cde22c2a5083a40cf5
Nils Feege <nils.feege@stonybrook.edu>
2017-12-09 20:01:16 -0500

    Add output of truth event information

--- EICAnalysis/Leptoquarks.C
+++ EICAnalysis/Leptoquarks.C
@@ -22,18 +22,26 @@ using namespace std;
 
 Leptoquarks::Leptoquarks(std::string filename) :
   SubsysReco("Leptoquarks" ),
+  _foutname(filename),
+  _fout_root(NULL),
+  _tree_event(NULL),
+  _ebeam_E(10),
+  _pbeam_E(250),
   _embedding_id(1)
 {
-  _foutname = filename;
-  _ebeam_E = 10;
-  _pbeam_E = 250;
+
 }
 
 int
 Leptoquarks::Init(PHCompositeNode *topNode)
 {
-  _verbose = false;
   _ievent = 0;
+
+  _fout_root = new TFile(_foutname.c_str(), "RECREATE");
+
+  _tree_event = new TNtuple("ntp_event","event information from LQ events",
+                            "ievent:has_tau:has_uds:tau_eta:tau_phi:tau_e:tau_p:tau_pt:tau_decay_prong:tau_decay_hcharged:tau_decay_lcharged:uds_eta:uds_phi:uds_e:uds_p:uds_pt");
+
 
   return 0;
 }
@@ -43,95 +51,202 @@ Leptoquarks::process_event(PHCompositeNode *topNode)
 {
   _ievent ++;
 
-  cout << endl;
-  cout << "Processing event " << _ievent << endl;
-
+  /* Access GenEventMap */
   PHHepMCGenEventMap * geneventmap = findNode::getClass<PHHepMCGenEventMap>(topNode, "PHHepMCGenEventMap");
   if (!geneventmap)
-  {
-    std::cout <<PHWHERE<<" - Fatal error - missing node PHHepMCGenEventMap"<<std::endl;
-    return Fun4AllReturnCodes::ABORTRUN;
-  }
+    {
+      std::cout <<PHWHERE<<" - Fatal error - missing node PHHepMCGenEventMap"<<std::endl;
+      return Fun4AllReturnCodes::ABORTRUN;
+    }
 
+  /* Access GenEvent */
   PHHepMCGenEvent *genevt = geneventmap->get(_embedding_id);
   if (!genevt)
-  {
-    std::cout <<PHWHERE<<" - Fatal error - node PHHepMCGenEventMap missing subevent with embedding ID "<<_embedding_id;
-    std::cout <<". Print PHHepMCGenEventMap:";
-    geneventmap->identify();
-    return Fun4AllReturnCodes::ABORTRUN;
-  }
+    {
+      std::cout <<PHWHERE<<" - Fatal error - node PHHepMCGenEventMap missing subevent with embedding ID "<<_embedding_id;
+      std::cout <<". Print PHHepMCGenEventMap:";
+      geneventmap->identify();
+      return Fun4AllReturnCodes::ABORTRUN;
+    }
 
+  /* Access event */
   HepMC::GenEvent* theEvent = genevt->getEvent();
 
   if ( !theEvent )
     {
       cout << "Missing GenEvent!" << endl;
-    }
-  else
-    {
-      cout << "Found a GenEvent with " << theEvent->particles_size() << " particles and " << theEvent->vertices_size() << " vetices" << endl;
+      return Fun4AllReturnCodes::ABORTRUN;
     }
 
-  /* Loop over all particles, look for pions */
-  for (HepMC::GenEvent::particle_const_iterator p = theEvent->particles_begin();
-       p != theEvent->particles_end(); ++p)
+  /* Look for leptoquark and tau particle in the event */
+  HepMC::GenParticle* particle_lq = NULL;
+  HepMC::GenParticle* particle_tau = NULL;
+  HepMC::GenParticle* particle_quark = NULL;
+
+  bool tau_found = false;
+  bool quark_found = false;
+
+  double tau_eta = 0;
+  double tau_phi = 0;
+  double tau_e = 0;
+  double tau_p = 0;
+  double tau_pt = 0;
+
+  int tau_decay_prong = 0;
+  int tau_decay_hcharged = 0;
+  int tau_decay_lcharged = 0;
+
+  double quark_eta = 0;
+  double quark_phi = 0;
+  double quark_e = 0;
+  double quark_p = 0;
+  double quark_pt = 0;
+
+  /* Loop over all truth particles in event generator collection
+   *
+   * particle   ...   PGD code
+   * -------------------------
+   * LQ_ue      ...    39
+   * tau-       ...    15
+   * pi+        ...    211
+   * pi-        ...   -211
+   * u          ...    2
+   * d          ...    1 */
+  for ( HepMC::GenEvent::particle_iterator p = theEvent->particles_begin();
+        p != theEvent->particles_end(); ++p ) {
+
+    /* check particle ID == LQ_ue */
+    if ( (*p)->pdg_id() == 39 )
+      {
+        particle_lq = (*p);
+
+        /* Where does it end (=decay)? */
+        if ( particle_lq->end_vertex() )
+          {
+            /* Loop over mother particles at production vertex */
+            for ( HepMC::GenVertex::particle_iterator lq_child
+                    = particle_lq->end_vertex()->
+                    particles_begin(HepMC::children);
+                  lq_child != particle_lq->end_vertex()->
+                    particles_end(HepMC::children);
+                  ++lq_child )
+              {
+                /* Is child a tau? */
+                if ( (*lq_child)->pdg_id() == 15 )
+                  {
+                    particle_tau = (*lq_child);
+                    UpdateFinalStateParticle( particle_tau );
+                  }
+                /* Is child a quark? */
+                else if ( abs((*lq_child)->pdg_id()) > 0 && abs((*lq_child)->pdg_id() < 7) )
+                  {
+                    particle_quark = (*lq_child);
+                    UpdateFinalStateParticle( particle_quark );
+                  }
+                /* What else could it be? */
+                else
+                  {
+                    cerr << PHWHERE << " ERROR: Didn't expect to find a leptoquark child with pdg_id " << (*lq_child)->pdg_id() << endl;
+                    return Fun4AllReturnCodes::ABORTEVENT;
+                  }
+              }
+          }
+
+        /* end loop if leptoquark found */
+        break;
+      } // end if leptoquark //
+  } // end loop over all particles in event //
+
+  /* If TAU in event: Tag the tau candidate (i.e. jet) with smalles delta_R from this tau */
+  if( particle_tau )
     {
+      tau_found = true;
+      tau_eta = particle_tau->momentum().eta();
+      tau_phi = particle_tau->momentum().phi();
 
-      TParticlePDG * pdg_p = TDatabasePDG::Instance()->GetParticle( (*p)->pdg_id() );
+      /* Event record uses 0 < phi < 2Pi, while Fun4All uses -Pi < phi < Pi.
+       * Therefore, correct angle for 'wraparound' at phi == Pi */
+      if(tau_phi>TMath::Pi()) tau_phi = tau_phi-2*TMath::Pi();
 
-      /* check for tau */
-      if ( (TString(pdg_p->GetName()) == "tau-") ||
-	   (TString(pdg_p->GetName()) == "LQ_ue") ||
-	   (TString(pdg_p->GetName()) == "proton") ||
-	   (TString(pdg_p->GetName()) == "e-") ||
-	   (TString(pdg_p->GetName()) == "pi0")
-	   )
-//      if ( (TString(pdg_p->GetName()) == "tau")
-//	   )
+      tau_e = particle_tau->momentum().e();
+      tau_p = sqrt( pow( particle_tau->momentum().px(), 2 ) +
+		    pow( particle_tau->momentum().py(), 2 ) +
+		    pow( particle_tau->momentum().pz(), 2 ) );
+      tau_pt = particle_tau->momentum().perp();
+
+      /* Add information about tau decay */
+      /* Loop over all particle at end vertex */
+      if ( particle_tau->end_vertex() )
         {
-          cout << "Found a " << TString(pdg_p->GetName()) << " with status " << (*p)->status() << " at eta = " << (*p)->momentum().eta() << " and phi = " << (*p)->momentum().phi() << endl;
-
-          if ( (*p)->end_vertex() )
+          for ( HepMC::GenVertex::particle_iterator tau_decay
+                  = particle_tau->end_vertex()->
+                  particles_begin(HepMC::children);
+                tau_decay != particle_tau->end_vertex()->
+                  particles_end(HepMC::children);
+                ++tau_decay )
             {
-              cout << "  ** end vertex at x = " << (*p)->end_vertex()->position().x() << endl;
+              /* Get entry from TParticlePDG because HepMC::GenPArticle does not provide charge or class of particle */
+              TParticlePDG * pdg_p = TDatabasePDG::Instance()->GetParticle( (*tau_decay)->pdg_id() );
 
-              cout << "  **** All INCOMING particles at end vertex: ";
-              for (HepMC::GenVertex::particles_in_const_iterator p_in = (*p)->end_vertex()->particles_in_const_begin();
-                   p_in != (*p)->end_vertex()->particles_in_const_end(); p_in++ )
+              /* Check if particle is charged */
+              if ( pdg_p->Charge() != 0 )
                 {
-                  cout << TDatabasePDG::Instance()->GetParticle( (*p_in)->pdg_id() )->GetName() << " ";
-                }
-              cout << endl;
+                  tau_decay_prong += 1;
 
-              cout << "  **** All OUTGOING particles at end vertex: ";
-              for (HepMC::GenVertex::particles_out_const_iterator p_out = (*p)->end_vertex()->particles_out_const_begin();
-                   p_out != (*p)->end_vertex()->particles_out_const_end(); p_out++ )
-                {
-                  cout << TDatabasePDG::Instance()->GetParticle( (*p_out)->pdg_id() )->GetName() << " ";
+                  /* Check if particle is lepton */
+                  if ( string( pdg_p->ParticleClass() ) == "Lepton" )
+                    tau_decay_lcharged += 1;
+
+                  /* Check if particle is hadron, i.e. Meson or Baryon */
+                  else if ( ( string( pdg_p->ParticleClass() ) == "Meson"  ) ||
+                            ( string( pdg_p->ParticleClass() ) == "Baryon" ) )
+                    tau_decay_hcharged += 1;
                 }
-	      cout << endl;
             }
-          else
-            cout << "  ** no end vertex for this particle " << endl;
         }
+    }
 
+  /* If QUARK (->jet) in event: Tag the tau candidate (i.e. jet) with smalles delta_R from this quark */
+  /* @TODO: This is a copy from the loop to tag the tau candidate with smallest delta_R w.r.t. final state tau-
+   * instead of copying the code, make a function that can be called for both (GetMinDeltaRElement or similar) */
+  if( particle_quark )
+    {
+      quark_found = true;
+      quark_eta = particle_quark->momentum().eta();
+      quark_phi = particle_quark->momentum().phi();
 
+      /* Event record uses 0 < phi < 2Pi, while Fun4All uses -Pi < phi < Pi.
+       * Therefore, correct angle for 'wraparound' at phi == Pi */
+      if(quark_phi>TMath::Pi()) quark_phi = quark_phi-2*TMath::Pi();
 
+      quark_e  = particle_quark->momentum().e();
+      quark_p  = sqrt( pow( particle_quark->momentum().px(), 2 ) +
+		       pow( particle_quark->momentum().py(), 2 ) +
+		       pow( particle_quark->momentum().pz(), 2 ) );
+      quark_pt = particle_quark->momentum().perp();
 
-      //      /* Check for pions and ancestors */
-      //      if ( (TString(pdg_p->GetName()) == "tau-") ||
-      //           (TString(pdg_p->GetName()) == "pi+") )
-      //        {
-      //          cout << "Found a " << TString(pdg_p->GetName()) << " with status " << (*p)->status() << " at eta = " << (*p)->momentum().eta() << " and phi = " << (*p)->momentum().phi() << endl;
-      //
+    }
 
+  /* Fill event information to ntupple */
+  float event_data[16] = { (float) _ievent,
+                           (float) tau_found,
+                           (float) quark_found,
+                           (float) tau_eta,
+                           (float) tau_phi,
+                           (float) tau_e,
+                           (float) tau_p,
+                           (float) tau_pt,
+                           (float) tau_decay_prong,
+                           (float) tau_decay_hcharged,
+                           (float) tau_decay_lcharged,
+                           (float) quark_eta,
+                           (float) quark_phi,
+                           (float) quark_e,
+                           (float) quark_p,
+                           (float) quark_pt
+  };
 
-      //          cout << " *** All INCOMING particles at production vertex: " << (*p)->production_vertex()->position[0] << endl;
-
-      //cout << (*p)->barcode() << " ; " << TString(pdg_p->GetName()) << " ; " << (*p)->status() << " ; " << (*p)->production_vertex() << " ; " << (*p)->end_vertex() << " ; " << (*p)->momentum().e() << " ; " << (*p)->momentum().theta() << endl;
-
-    } /* end loop over all particles in event record */
+  _tree_event->Fill( event_data );
 
   return 0;
 }
@@ -139,9 +254,34 @@ Leptoquarks::process_event(PHCompositeNode *topNode)
 int
 Leptoquarks::End(PHCompositeNode *topNode)
 {
-  //_fout_root->cd();
-  //_tree_lq->Write();
-  //_fout_root->Close();
+  _fout_root->cd();
+  _tree_event->Write();
+  _fout_root->Close();
 
   return 0;
+}
+
+
+void
+Leptoquarks::UpdateFinalStateParticle( HepMC::GenParticle *&particle )
+{
+  bool final_state = false;
+  while ( !final_state )
+    {
+      /* Loop over all children at the end vertex of this particle */
+      for ( HepMC::GenVertex::particle_iterator child
+              = particle->end_vertex()->particles_begin(HepMC::children);
+            child != particle->end_vertex()->particles_end(HepMC::children);
+            ++child )
+        {
+          /* If there is a child of same particle ID, this was not the final state particle- update pointer to particle and repeat */
+          if ( (*child)->pdg_id() == particle->pdg_id() )
+            {
+              particle = (*child);
+              break;
+            }
+          final_state = true;
+        }
+    }
+  return;
 }
