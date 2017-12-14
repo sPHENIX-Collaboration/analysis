c64407d76af8f50559d61d80b25cf60224e5124a
Ron Belmont <belmonrj@gmail.com>
2017-12-14 16:53:54 -0500

    I really dislike variable names starting with underscore

--- TreeMaker.h
+++ TreeMaker.h
@@ -29,128 +29,128 @@ class TreeMaker: public SubsysReco
 
   std::string _foutname;
 
-  int _b_tower_n;
-  int _b_tower_layer[10000];
-  float _b_tower_E[10000];
-  float _b_tower_eta[10000];
-  float _b_tower_phi[10000];
+  int b_tower_n;
+  int b_tower_layer[10000];
+  float b_tower_E[10000];
+  float b_tower_eta[10000];
+  float b_tower_phi[10000];
 
-  float _b_tower_total_0;
-  float _b_tower_total_1;
-  float _b_tower_total_2;
+  float b_tower_total_0;
+  float b_tower_total_1;
+  float b_tower_total_2;
 
-  int _b_bkg_n;
-  int _b_bkg_layer[100];
-  int _b_bkg_eta[100];
-  float _b_bkg_E[100];
+  int bbkg_n;
+  int bbkg_layer[100];
+  int bbkg_eta[100];
+  float bbkg_E[100];
 
-  int _b_bkg2_n;
-  int _b_bkg2_layer[100];
-  int _b_bkg2_eta[100];
-  float _b_bkg2_E[100];
+  int bbkg2_n;
+  int bbkg2_layer[100];
+  int bbkg2_eta[100];
+  float bbkg2_E[100];
 
-  int _b_cluster_n;
-  float _b_cluster_pt[500];
-  float _b_cluster_eta[500];
-  float _b_cluster_phi[500];
-  int _b_cluster_layer[500];
+  int b_cluster_n;
+  float b_cluster_pt[500];
+  float b_cluster_eta[500];
+  float b_cluster_phi[500];
+  int b_cluster_layer[500];
 
-  int _b_jet5_n;
-  float _b_jet5_pt[200];
-  float _b_jet5_eta[200];
-  float _b_jet5_phi[200];
+  int b_jet5_n;
+  float b_jet5_pt[200];
+  float b_jet5_eta[200];
+  float b_jet5_phi[200];
 
-  int _b_jet4_n;
-  float _b_jet4_pt[200];
-  float _b_jet4_eta[200];
-  float _b_jet4_phi[200];
+  int b_jet4_n;
+  float b_jet4_pt[200];
+  float b_jet4_eta[200];
+  float b_jet4_phi[200];
 
-  int _b_jet3_n;
-  float _b_jet3_pt[200];
-  float _b_jet3_eta[200];
-  float _b_jet3_phi[200];
+  int b_jet3_n;
+  float b_jet3_pt[200];
+  float b_jet3_eta[200];
+  float b_jet3_phi[200];
 
-  int _b_jet2_n;
-  float _b_jet2_pt[200];
-  float _b_jet2_eta[200];
-  float _b_jet2_phi[200];
+  int b_jet2_n;
+  float b_jet2_pt[200];
+  float b_jet2_eta[200];
+  float b_jet2_phi[200];
 
-  int _b_jet4sub_n;
-  float _b_jet4sub_pt[200];
-  float _b_jet4sub_eta[200];
-  float _b_jet4sub_phi[200];
+  int b_jet4sub_n;
+  float b_jet4sub_pt[200];
+  float b_jet4sub_eta[200];
+  float b_jet4sub_phi[200];
 
-  int _b_jet5sub_n;
-  float _b_jet5sub_pt[200];
-  float _b_jet5sub_eta[200];
-  float _b_jet5sub_phi[200];
+  int b_jet5sub_n;
+  float b_jet5sub_pt[200];
+  float b_jet5sub_eta[200];
+  float b_jet5sub_phi[200];
 
-  int _b_jet3sub_n;
-  float _b_jet3sub_pt[200];
-  float _b_jet3sub_eta[200];
-  float _b_jet3sub_phi[200];
+  int b_jet3sub_n;
+  float b_jet3sub_pt[200];
+  float b_jet3sub_eta[200];
+  float b_jet3sub_phi[200];
 
-  int _b_jet2sub_n;
-  float _b_jet2sub_pt[200];
-  float _b_jet2sub_eta[200];
-  float _b_jet2sub_phi[200];
+  int b_jet2sub_n;
+  float b_jet2sub_pt[200];
+  float b_jet2sub_eta[200];
+  float b_jet2sub_phi[200];
 
-  int _b_jet2seed_n;
-  float _b_jet2seed_pt[200];
-  float _b_jet2seed_eta[200];
-  float _b_jet2seed_phi[200];
+  int b_jet2seed_n;
+  float b_jet2seed_pt[200];
+  float b_jet2seed_eta[200];
+  float b_jet2seed_phi[200];
 
-  int _b_truthjet5_n;
-  float _b_truthjet5_pt[200];
-  float _b_truthjet5_eta[200];
-  float _b_truthjet5_phi[200];
+  int b_truthjet5_n;
+  float b_truthjet5_pt[200];
+  float b_truthjet5_eta[200];
+  float b_truthjet5_phi[200];
 
-  int _b_truthjet4_n;
-  float _b_truthjet4_pt[200];
-  float _b_truthjet4_eta[200];
-  float _b_truthjet4_phi[200];
+  int b_truthjet4_n;
+  float b_truthjet4_pt[200];
+  float b_truthjet4_eta[200];
+  float b_truthjet4_phi[200];
 
-  int _b_truthjet3_n;
-  float _b_truthjet3_pt[200];
-  float _b_truthjet3_eta[200];
-  float _b_truthjet3_phi[200];
+  int b_truthjet3_n;
+  float b_truthjet3_pt[200];
+  float b_truthjet3_eta[200];
+  float b_truthjet3_phi[200];
 
-  int _b_truthjet2_n;
-  float _b_truthjet2_pt[200];
-  float _b_truthjet2_eta[200];
-  float _b_truthjet2_phi[200];
+  int b_truthjet2_n;
+  float b_truthjet2_pt[200];
+  float b_truthjet2_eta[200];
+  float b_truthjet2_phi[200];
 
-  float _b_trigEM2_E;
-  float _b_trigEM2_eta;
-  float _b_trigEM2_phi;
+  float b_trigEM2_E;
+  float b_trigEM2_eta;
+  float b_trigEM2_phi;
 
-  float _b_trigEM4_E;
-  float _b_trigEM4_eta;
-  float _b_trigEM4_phi;
+  float b_trigEM4_E;
+  float b_trigEM4_eta;
+  float b_trigEM4_phi;
 
-  float _b_trigFull2_E;
-  float _b_trigFull4_E;
-  float _b_trigFull6_E;
-  float _b_trigFull8_E;
-  float _b_trigFull10_E;
+  float b_trigFull2_E;
+  float b_trigFull4_E;
+  float b_trigFull6_E;
+  float b_trigFull8_E;
+  float b_trigFull10_E;
 
-  float _b_trigFull2_eta;
-  float _b_trigFull4_eta;
-  float _b_trigFull6_eta;
-  float _b_trigFull8_eta;
-  float _b_trigFull10_eta;
+  float b_trigFull2_eta;
+  float b_trigFull4_eta;
+  float b_trigFull6_eta;
+  float b_trigFull8_eta;
+  float b_trigFull10_eta;
 
-  float _b_trigFull2_phi;
-  float _b_trigFull4_phi;
-  float _b_trigFull6_phi;
-  float _b_trigFull8_phi;
-  float _b_trigFull10_phi;
+  float b_trigFull2_phi;
+  float b_trigFull4_phi;
+  float b_trigFull6_phi;
+  float b_trigFull8_phi;
+  float b_trigFull10_phi;
 
-  int _b_particle_n;
-  float _b_particle_pt[1000];
-  float _b_particle_eta[1000];
-  float _b_particle_phi[1000];
-  int _b_particle_pid[1000];
+  int b_particle_n;
+  float b_particle_pt[1000];
+  float b_particle_eta[1000];
+  float b_particle_phi[1000];
+  int b_particle_pid[1000];
 
 };
 
