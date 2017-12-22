b1cfe03c2b0e910f7ffef8afff9b9f75dce485ce
Nils Feege <nils.feege@stonybrook.edu>
2017-12-22 15:33:02 -0500

    Update tree to CSV file conversion

--- EICAnalysis/macros/leptoquarks_analysis/tree2csv.C
+++ EICAnalysis/macros/leptoquarks_analysis/tree2csv.C
@@ -4,34 +4,45 @@
  * Written by nils.feege@stonybrook.edu
  */
 
-int tree2csv()
+int tree2csv( TString filelist, TString csv_out = TString("output_raw.txt") )
 {
-  gStyle->SetOptStat(0);
+  /* Create a TChain from trees in multiple input files */
+  TChain chain("candidates");
 
-  /* open inout files and merge trees */
-  TChain chain("ntp_jet2");
-  chain.Add("data_3pions/p250_e20_0events_file1093_LeptoAna_r05.root");
-  chain.Add("data_3pions/p250_e20_0events_file1096_LeptoAna_r05.root");
-  chain.Add("data_3pions/p250_e20_0events_file1101_LeptoAna_r05.root");
-  chain.Add("data_3pions/p250_e20_0events_file1115_LeptoAna_r05.root");
-  chain.Add("data_3pions/p250_e20_0events_file1122_LeptoAna_r05.root");
-  chain.Add("data_3pions/p250_e20_0events_file1127_LeptoAna_r05.root");
-  chain.Add("data_3pions/p250_e20_0events_file1131_LeptoAna_r05.root");
-  chain.Add("data_3pions/p250_e20_0events_file1164_LeptoAna_r05.root");
+  /* open stream from input file list */
+  ifstream in;
+  in.open( filelist );
 
-  /* select output file */
-  TString csv_out("LeptoAna_r05_p250_e20.csv");
+  /* add files from input list to TChain */
+  TString fname;
+  while (1) {
+    in >> fname;
+    if (!in.good()) break;
+    cout << "Adding input file " << fname << endl;
+    chain.Add(fname);
+  }
+
+  /* close stream from input file list */
+  in.close();
 
   /* particle selection */
-  TCut select_jetcandidates("abs(jet_eta)<1.0 && jet_ptrans>5");
+  TCut select_jetcandidates("abs(jet_eta)<1.0 && jet_ptrans>5 && ( (evtgen_is_tau == 0) || (evtgen_is_tau==1 && sqrt( (evtgen_tau_eta-jet_eta)*(evtgen_tau_eta-jet_eta) + (evtgen_tau_phi-jet_phi)*(evtgen_tau_phi-jet_phi) ) < 0.1) )");
 
   /* select columns to keep */
-  TString cols_keep("is_tau:is_uds:tracks_count_r1:tracks_count_r2:tracks_rmax_r2:jetshape_radius:jet_mass");
+  TString cols_keep("*");
 
+  /* Set up TTreePlayer to print output to ascii file */
   ((TTreePlayer*)(chain.GetPlayer()))->SetScanRedirect(true);
   ((TTreePlayer*)(chain.GetPlayer()))->SetScanFileName( csv_out );
   chain.SetScanField(0);
-  chain.Scan( cols_keep , select_jetcandidates );
+
+  /* Call the Scan command that writes the tree content.
+   *
+   * Options:
+   *
+   * colsize=X --> set column size (length of character). This should correspond to the longest branch name.
+   */
+  chain.Scan( cols_keep , select_jetcandidates , "colsize=30");
 
   return 0;
 }
