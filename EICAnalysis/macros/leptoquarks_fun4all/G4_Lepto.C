9dcea8006f467eb47ab0da0bad942bfccdcbd18a
Nils Feege <nils.feege@stonybrook.edu>
2018-01-20 12:30:22 -0500

    Add macros to test DISKinematicsReco

--- EICAnalysis/macros/leptoquarks_fun4all/G4_Lepto.C
+++ EICAnalysis/macros/leptoquarks_fun4all/G4_Lepto.C
@@ -4,21 +4,36 @@ int G4_Lepto(string filename)
 
   cout << "**************************************" << endl;
   Fun4AllServer *se = Fun4AllServer::instance();
-  LeptoquarksReco *mcana1 = new LeptoquarksReco(filename+"_r05.root");
-  LeptoquarksReco *mcana2 = new LeptoquarksReco(filename+"_r06.root");
-  LeptoquarksReco *mcana3 = new LeptoquarksReco(filename+"_r07.root");
-  LeptoquarksReco *mcana4 = new LeptoquarksReco(filename+"_r08.root");
 
-  
-  mcana1->set_reco_jet_collection("AntiKt_Tower_r05");
-  mcana2->set_reco_jet_collection("AntiKt_Tower_r06");
-  mcana3->set_reco_jet_collection("AntiKt_Tower_r07");
-  mcana4->set_reco_jet_collection("AntiKt_Tower_r08");
-  
-  se->registerSubsystem( mcana1 );
-  se->registerSubsystem( mcana2 );
-  se->registerSubsystem( mcana3 );
-  se->registerSubsystem( mcana4 );
+  Leptoquarks *truthana = new Leptoquarks(filename+"_TruthEvent.root");
+  se->registerSubsystem( truthana );
+
+  LeptoquarksReco *mcana_r05 = new LeptoquarksReco(filename+"_r05.root");
+  LeptoquarksReco *mcana_r10 = new LeptoquarksReco(filename+"_r10.root");
+
+  LeptoquarksReco *mcana_r05truth = new LeptoquarksReco(filename+"_r05truth.root");
+  LeptoquarksReco *mcana_r10truth = new LeptoquarksReco(filename+"_r10truth.root");
+
+  mcana_r05->set_reco_jet_collection("AntiKt_Tower_r05");
+  mcana_r05->set_save_towers( true );
+  mcana_r05->set_save_tracks( true );
+
+  mcana_r10->set_reco_jet_collection("AntiKt_Tower_r10");
+  mcana_r10->set_save_towers( true );
+  mcana_r10->set_save_tracks( true );
+
+  mcana_r05truth->set_reco_jet_collection("AntiKt_Truth_r05");
+  mcana_r05truth->set_save_towers( true );
+  mcana_r05truth->set_save_tracks( true );
+
+  mcana_r10truth->set_reco_jet_collection("AntiKt_Truth_r10");
+  mcana_r10truth->set_save_towers( true );
+  mcana_r10truth->set_save_tracks( true );
+
+  se->registerSubsystem( mcana_r05 );
+//  se->registerSubsystem( mcana_r10 );
+//  se->registerSubsystem( mcana_r05truth );
+//  se->registerSubsystem( mcana_r10truth );
   cout << "**************************************" << endl;
 
   return 0;
