5060a4e0a17ea8faff46b1e7f4b7178956967e82
Rourke Sekelsky <rourke.sekelsky@stonybrook.edu>
2018-01-28 16:41:20 -0500

    Updated work-in-progress particle momentum function

--- ParticleID/RICHAnalysis/RICHParticleID.C
+++ ParticleID/RICHAnalysis/RICHParticleID.C
@@ -100,101 +100,128 @@ RICHParticleID::process_event(PHCompositeNode *topNode)
   /* abort if no truth container found */
   assert(truthinfo);
 
+
   /* Loop over tracks */
-  for (SvtxTrackMap::ConstIter track_itr = trackmap->begin();
-       track_itr != trackmap->end(); track_itr++) {
+  for (SvtxTrackMap::ConstIter track_itr = trackmap->begin(); track_itr != trackmap->end(); track_itr++)
+    {
+      
+      SvtxTrack_FastSim* track_j = dynamic_cast<SvtxTrack_FastSim*>(track_itr->second);
 
-    SvtxTrack_FastSim* track_j = dynamic_cast<SvtxTrack_FastSim*>(track_itr->second);
 
-    /* Get mean emission point from track in RICH */
-    double m_emi[3] = {0.,0.,0.};
+      /* Get mean emission point from track in RICH */
+      double m_emi[3] = {0.,0.,0.};
+      
+      /* 'Continue' with next track if RICH not found in state list for this track */
+      if ( ! get_position_from_track_state( track_j, _trackstate_name, m_emi ) )
+	continue;
 
-    /* 'Continue' with next track if RICH not found in state list for this track */
-    if ( ! get_position_from_track_state( track_j, _trackstate_name, m_emi ) )
+      
+      /* Fill momv object which is the normalized momentum vector of the track in the RICH (i.e. its direction) */
+      double momv[3] = {0.,0.,0.};
+      
+      /* 'Continue' with next track if RICH not found in state list for this track */
+      //if ( ! get_momentum_from_track_state( track_j, _trackstate_name, momv ) )
+      //continue;
+      
+      /* Fill with truth momentum instead of reco */
+      if ( ! get_true_momentum( truthinfo, track_j, momv) )
       continue;
-
-    /* Fill momv object which is the normalized momentum vector of the track in the RICH (i.e. its direction) */
-    double momv[3] = {0.,0.,0.};
-
-    /* 'Continue' with next track if RICH not found in state list for this track */
-    if ( ! get_momentum_from_track_state( track_j, _trackstate_name, momv ) )
-      continue;
-
-    double momv_norm = sqrt( momv[0]*momv[0] + momv[1]*momv[1] + momv[2]*momv[2] );
-    momv[0] /= momv_norm;
-    momv[1] /= momv_norm;
-    momv[2] /= momv_norm;
-
-    /* Calculate true emission angle for output tree */
-    _theta_true = 0;
-    _mass_true = 0;
-
-    /* If truth info found use it to calculate truth emission angle and truth mass */
-    if ( truthinfo )
-      {
-        _theta_true = calculate_true_emission_angle( truthinfo , track_j , _refractive_index );
-	_mass_true = calculate_true_mass( truthinfo, track_j );
-      }
+      
+      /* Fill with vector constructed from emission points (from truth) */
+      //if ( ! get_true_momentum_2( truthinfo, richhits, track_j, momv) )
+      //continue;
 
 
-    /* Loop over all G4Hits in container (i.e. RICH photons in event) */
-    PHG4HitContainer::ConstRange rich_hits_begin_end = richhits->getHits();
-    PHG4HitContainer::ConstIterator rich_hits_iter;
+      double momv_norm = sqrt( momv[0]*momv[0] + momv[1]*momv[1] + momv[2]*momv[2] );
+      momv[0] /= momv_norm;
+      momv[1] /= momv_norm;
+      momv[2] /= momv_norm;
+      
+      // Estimate mean emission point instead of real mean track point (temporary) */
+      m_emi[0] = ((220.)/momv[2])*momv[0];
+      m_emi[1] = ((220.)/momv[2])*momv[1];
+      m_emi[2] = ((220.)/momv[2])*momv[2];
+      
 
-    for (rich_hits_iter = rich_hits_begin_end.first; rich_hits_iter !=  rich_hits_begin_end.second; ++rich_hits_iter)
-      {
-        PHG4Hit *hit_i = rich_hits_iter->second;
-	PHG4Particle* particle = NULL;
-	PHG4Particle* parent = NULL;
-	if ( truthinfo )
-          {  
-            particle = truthinfo->GetParticle( hit_i->get_trkid() );
-            parent = truthinfo->GetParticle( particle->get_parent_id() );
-	  }
-	
-	_hit_x0 =  hit_i->get_x(0);
-        _hit_y0 =  hit_i->get_y(0);
-        _hit_z0 =  hit_i->get_z(0);
+      /* Calculate true emission angle for output tree */
+      _theta_true = 0;
+      _mass_true = 0;
+      
+      /* If truth info found use it to calculate truth emission angle and truth mass */
+      if ( truthinfo )
+	{
+	  _theta_true = calculate_true_emission_angle( truthinfo , track_j , _refractive_index );
+	  _mass_true = calculate_true_mass( truthinfo, track_j );
+	}
+      
+      
+      /* Loop over all G4Hits in container (i.e. RICH photons in event) */
+      PHG4HitContainer::ConstRange rich_hits_begin_end = richhits->getHits();
+      PHG4HitContainer::ConstIterator rich_hits_iter;
+      
+      for (rich_hits_iter = rich_hits_begin_end.first; rich_hits_iter !=  rich_hits_begin_end.second; ++rich_hits_iter)
+	{
+	  PHG4Hit *hit_i = rich_hits_iter->second;
+	  PHG4Particle* particle = NULL;
+	  PHG4Particle* parent = NULL;
+	  PHG4VtxPoint* vertex = NULL;
+	  if ( truthinfo )
+	    {  
+	      particle = truthinfo->GetParticle( hit_i->get_trkid() );
+	      parent = truthinfo->GetParticle( particle->get_parent_id() );
+	      vertex = truthinfo->GetVtx( particle->get_vtx_id() );
+	    }
 
-	_track_px = particle->get_px();
-        _track_py = particle->get_py();
-        _track_pz = particle->get_pz();
-
-        _mtrack_px = parent->get_px();
-        _mtrack_py = parent->get_py();
-        _mtrack_pz = parent->get_pz();
-
-        _track_e = particle->get_e();
-        _mtrack_e = parent->get_e();
-        _edep = hit_i->get_edep();
-
-	_bankid = 0;
-        _volumeid = hit_i->get_detid();
-        _hitid = hit_i->get_hit_id();
-	_pid = particle->get_pid();
-        _mpid = parent->get_pid();
-        _trackid = particle->get_track_id();
-        _mtrackid = parent->get_track_id();
-	_otrackid = _mtrackid;
-
-	_ptot = momv_norm;
-
-        /* Set reconstructed emission angle and reconstructed mass for output tree */
-        _theta_reco = calculate_emission_angle( m_emi, momv, hit_i );
-	_mass_reco = calculate_reco_mass( momv_norm, _theta_reco, _refractive_index );
-
-        /* Fill tree */
-        _tree_rich->Fill();
-
-      } // END loop over photons
-
-  } // END loop over tracks
-
+	  cout << "track_j->get_truth_track_id() gives:" << track_j->get_truth_track_id() << endl;
+	  cout << "parent->get_track_id() gives:" << parent->get_track_id() << endl;
+	  
+	  _hit_x0 =  hit_i->get_x(0);
+	  _hit_y0 =  hit_i->get_y(0);
+	  _hit_z0 =  hit_i->get_z(0);
+	  
+	  _emi_x = vertex->get_x();
+	  _emi_y = vertex->get_y();
+	  _emi_z = vertex->get_z();
+	  
+	  _track_px = particle->get_px();
+	  _track_py = particle->get_py();
+	  _track_pz = particle->get_pz();
+	  
+	  _mtrack_px = parent->get_px();
+	  _mtrack_py = parent->get_py();
+	  _mtrack_pz = parent->get_pz();
+	  
+	  _track_e = particle->get_e();
+	  _mtrack_e = parent->get_e();
+	  _edep = hit_i->get_edep();
+	  
+	  _bankid = 0;
+	  _volumeid = hit_i->get_detid();
+	  _hitid = hit_i->get_hit_id();
+	  _pid = particle->get_pid();
+	  _mpid = parent->get_pid();
+	  _trackid = particle->get_track_id();
+	  _mtrackid = parent->get_track_id();
+	  _otrackid = _mtrackid;
+	  
+	  _ptot = momv_norm;
+	  
+	  /* Set reconstructed emission angle and reconstructed mass for output tree */
+	  _theta_reco = calculate_emission_angle( m_emi, momv, hit_i );
+	  _mass_reco = calculate_reco_mass( momv_norm, _theta_reco, _refractive_index );
+	  
+	  /* Fill tree */
+	  _tree_rich->Fill();
+	  
+	} // END loop over photons
+      
+    } // END loop over tracks
+  
   return 0;
 }
 
 
-double RICHParticleID::calculate_emission_angle( double m_emi[3], double momv[3], PHG4Hit *hit_i )
+double RICHParticleID::calculate_emission_angle( double m_emi[3], double momv[3], PHG4Hit * hit_i )
 {
   /* Input parameters for indirect ray tracing algorithm */
   double Ex = m_emi[0];
@@ -223,6 +250,13 @@ double RICHParticleID::calculate_emission_angle( double m_emi[3], double momv[3]
   /* Call algorithm to determine emission angle of photon i w.r.t. track j */
   float theta_c = _analyzer->ind_ray(Ex, Ey, Ez, Dx, Dy, Dz, vx, vy, vz, select_radiator); //Indirect Ray Tracing
 
+  /* Output values */
+  /*cout<<"V: "<<vx<<"  "<<vy<<"  "<<vz<<endl;
+  cout<<"E: "<<Ex<<"  "<<Ey<<"  "<<Ez<<endl;
+  cout<<"D: "<<Dx<<"  "<<Dy<<"  "<<Dz<<endl;
+  cout<<"R: "<<cx<<"  "<<cy<<"  "<<cz<<endl;
+  cout<<"Theta: "<<theta_c<<endl;
+  */
   return theta_c;
 }
 
@@ -239,9 +273,9 @@ double RICHParticleID::calculate_true_emission_angle( PHG4TruthInfoContainer* tr
   double mass = _pdg->GetParticle(pid)->Mass();
 
   /* Get particle total momentum */
-  double ptotal = sqrt( track->get_px() * track->get_px() +
-                        track->get_py() * track->get_py() +
-                        track->get_pz() * track->get_pz() );
+  double ptotal = sqrt( particle->get_px() * particle->get_px() +
+                        particle->get_py() * particle->get_py() +
+                        particle->get_pz() * particle->get_pz() );
 
   /* Calculate beta = v/c */
   double beta = ptotal / sqrt( mass * mass + ptotal * ptotal );
@@ -334,6 +368,80 @@ RICHParticleID::get_momentum_from_track_state(  SvtxTrack_FastSim * track, strin
 }
 
 
+bool
+RICHParticleID::get_true_momentum( PHG4TruthInfoContainer* truthinfo, SvtxTrack_FastSim * track, double arr_mom[3] )
+{
+  arr_mom[0] = 0;
+  arr_mom[1] = 0;
+  arr_mom[2] = 0;
+
+  /* Get the track's particle, then get truth momentum */
+  PHG4Particle* particle = truthinfo->GetParticle( track->get_truth_track_id() );
+  if (particle){
+    arr_mom[0] = particle->get_px();
+    arr_mom[1] = particle->get_py();
+    arr_mom[2] = particle->get_pz();
+    return true;
+  }
+  
+  return false;
+}
+
+
+bool
+RICHParticleID::get_true_momentum_2( PHG4TruthInfoContainer* truthinfo, PHG4HitContainer* richhits, SvtxTrack_FastSim * track, double arr_mom[3] )
+{
+  // THIS IS NOT COMPLETE
+  arr_mom[0] = 0;
+  arr_mom[1] = 0;
+  arr_mom[2] = 0;
+
+  /* Get photon emission points, then get truth momentum using the vector from first to last emitted photon */
+  vector<double> emix;
+  vector<double> emiy;
+  vector<double> emiz;
+
+  /* Loop over all G4Hits in container (i.e. RICH photons in event) */
+  PHG4HitContainer::ConstRange rich_hits_begin_end = richhits->getHits();
+  PHG4HitContainer::ConstIterator rich_hits_iter;
+  
+  for (rich_hits_iter = rich_hits_begin_end.first; rich_hits_iter !=  rich_hits_begin_end.second; ++rich_hits_iter)
+    {
+      PHG4Hit *hit_i = rich_hits_iter->second;
+      PHG4Particle *particle = truthinfo->GetParticle( hit_i->get_trkid() ); 
+      PHG4Particle *parent = truthinfo->GetParticle( particle->get_parent_id() );
+      PHG4VtxPoint *vertex = truthinfo->GetVtx( particle->get_vtx_id() );
+
+      /* Have to fix this comparison! Don't use this function yet. */
+      if ( parent == truthinfo->GetParticle( track->get_truth_track_id() ) ){
+	emix.push_back( vertex->get_x() );
+	emiy.push_back( vertex->get_y() );
+	emiz.push_back( vertex->get_z() );
+      }
+    }
+
+
+  vector<double>::iterator first;
+  vector<double>::iterator last;
+  double dx, dy, dz;
+
+  if (emiz.size() != 0){
+    first = std::min_element(emiz.begin(),emiz.end());
+    last = std::max_element(emiz.begin(),emiz.end());
+    double p1 = std::distance(emiz.begin(),first);
+    double p2 = std::distance(emiz.begin(),last);
+    
+    dx = emix.at(p2) - emix.at(p1);
+    dy = emiy.at(p2) - emiy.at(p1);
+    dz = emiz.at(p2) - emiz.at(p1);
+  }
+
+  cout << "DELTAS: " << dx << " " << dy << " " << dz << endl;
+
+  return true;
+}
+
+
 int
 RICHParticleID::End(PHCompositeNode *topNode)
 {
@@ -351,6 +459,10 @@ RICHParticleID::reset_tree_vars()
   _hit_x0 = -999;
   _hit_y0 = -999;
   _hit_z0 = -999;
+
+  _emi_x = -999;
+  _emi_y = -999;
+  _emi_z = -999;
 
   _track_px = -999;
   _track_py = -999;
@@ -393,6 +505,9 @@ RICHParticleID::init_tree()
   _tree_rich->Branch("hit_x", &_hit_x0, "Global x-hit /D");
   _tree_rich->Branch("hit_y", &_hit_y0, "Global y-hit /D");
   _tree_rich->Branch("hit_z", &_hit_z0, "Global z-hit /D");
+  _tree_rich->Branch("emi_x", &_emi_x, "Photon emission x-coord. /D");  
+  _tree_rich->Branch("emi_y", &_emi_y, "Photon emission y-coord. /D");
+  _tree_rich->Branch("emi_z", &_emi_z, "Photon emission z-coord. /D");
   _tree_rich->Branch("px", &_track_px, "Particle x-momentum /D");
   _tree_rich->Branch("py", &_track_py, "Particle y-momentum /D");
   _tree_rich->Branch("pz", &_track_pz, "Particle z-momentum /D");
