95468eddef9a8379365021eaf8248a65028e123a
Ron Belmont <belmonrj@gmail.com>
2018-02-06 13:20:54 -0500

    Working on removing some unneed lib loads...

--- run_macro.C
+++ run_macro.C
@@ -4,17 +4,17 @@ int run_macro(
               )
 {
 
+  // --- need to check whether some of these are redundant, see e.g. the Makefile...
   gSystem->Load("libfun4all.so");
   gSystem->Load("libg4detectors.so");
   gSystem->Load("libphhepmc.so");
   gSystem->Load("libg4testbench.so");
   gSystem->Load("libg4hough.so");
-  //gSystem->Load("libcemc.so");
   gSystem->Load("libcalo_io.so");
   gSystem->Load("libcalo_util.so");
-  gSystem->Load("libg4eval.so");
-  gSystem->Load("libcalotrigger.so");
-  gSystem->Load("libjetbackground.so");
+  //gSystem->Load("libg4eval.so");
+  //gSystem->Load("libcalotrigger.so");
+  //gSystem->Load("libjetbackground.so");
 
   gSystem->Load("libTreeMaker.so");
 
