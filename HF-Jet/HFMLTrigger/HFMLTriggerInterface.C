07cac1de089d01c65371b84cd477dd9ad490c9a5
Jin Huang <jhuang@bnl.gov>
2018-11-08 14:56:46 -0500

    Init module

---
+++ HF-Jet/HFMLTrigger/HFMLTriggerInterface.C
@@ -0,0 +1,90 @@
+#include "HFMLTriggerInterface.h"
+
+#include <fun4all/Fun4AllReturnCodes.h>
+#include <fun4all/Fun4AllServer.h>
+#include <phool/getClass.h>
+
+#include <phool/PHCompositeNode.h>
+
+#include <g4main/PHG4Hit.h>
+#include <g4main/PHG4Particle.h>
+#include <g4main/PHG4TruthInfoContainer.h>
+
+#include <TDatabasePDG.h>
+#include <TFile.h>
+#include <TH2D.h>
+#include <TLorentzVector.h>
+#include <TString.h>
+#include <TTree.h>
+#include <g4jets/Jet.h>
+#include <g4jets/JetMap.h>
+
+#include <HepMC/GenEvent.h>
+#include <HepMC/GenVertex.h>
+#include <phhepmc/PHHepMCGenEvent.h>
+#include <phhepmc/PHHepMCGenEventMap.h>
+
+#include <cassert>
+#include <cmath>
+#include <cstddef>
+#include <iostream>
+
+HFMLTriggerInterface::HFMLTriggerInterface(std::string filename)
+  : SubsysReco("HFMLTriggerInterface")
+  , _ievent(0)
+  , _f(nullptr)
+  , _eta_min(-1)
+  , _eta_max(1)
+  , _embedding_id(0)
+{
+  _foutname = filename;
+}
+
+int HFMLTriggerInterface::Init(PHCompositeNode* topNode)
+{
+  _ievent = 0;
+
+  _f = new TFile(_foutname.c_str(), "RECREATE");
+
+  //  _h2 = new TH2D("h2", "", 100, 0, 100.0, 40, -2, +2);
+  //  _h2_b = new TH2D("h2_b", "", 100, 0, 100.0, 40, -2, +2);
+  //  _h2_c = new TH2D("h2_c", "", 100, 0, 100.0, 40, -2, +2);
+  //  _h2all = new TH2D("h2all", "", 100, 0, 100.0, 40, -2, +2);
+
+  return 0;
+}
+
+int HFMLTriggerInterface::process_event(PHCompositeNode* topNode)
+{
+  PHHepMCGenEventMap* geneventmap = findNode::getClass<PHHepMCGenEventMap>(topNode, "PHHepMCGenEventMap");
+  if (!geneventmap)
+  {
+    std::cout << PHWHERE << " - Fatal error - missing node PHHepMCGenEventMap" << std::endl;
+    return Fun4AllReturnCodes::ABORTRUN;
+  }
+
+  PHHepMCGenEvent* genevt = geneventmap->get(_embedding_id);
+  if (!genevt)
+  {
+    std::cout << PHWHERE << " - Fatal error - node PHHepMCGenEventMap missing subevent with embedding ID " << _embedding_id;
+    std::cout << ". Print PHHepMCGenEventMap:";
+    geneventmap->identify();
+    return Fun4AllReturnCodes::ABORTRUN;
+  }
+
+  HepMC::GenEvent* theEvent = genevt->getEvent();
+  assert(theEvent);
+  if (Verbosity())
+    theEvent->print();
+
+  return Fun4AllReturnCodes::EVENT_OK;
+}
+
+int HFMLTriggerInterface::End(PHCompositeNode* topNode)
+{
+  _f->cd();
+  _f->Write();
+  //_f->Close();
+
+  return 0;
+}
---
+++ HF-Jet/HFMLTrigger/HFMLTriggerInterface.h
@@ -0,0 +1,86 @@
+#ifndef __HFMLTriggerInterface_H__
+#define __HFMLTriggerInterface_H__
+
+// --- need to check all these includes...
+#include <fun4all/SubsysReco.h>
+#include <limits.h>
+#include <cmath>
+#include <string>
+#include <vector>
+
+class TTree;
+class TFile;
+class TH2D;
+
+class PHCompositeNode;
+class Jet;
+namespace HepMC
+{
+class GenEvent;
+}
+
+class HFMLTriggerInterface : public SubsysReco
+{
+ public:
+  HFMLTriggerInterface(std::string filename);
+
+  int Init(PHCompositeNode *);
+  int process_event(PHCompositeNode *);
+  int End(PHCompositeNode *);
+
+  double
+  get_eta_max() const
+  {
+    return _eta_max;
+  }
+
+  void
+  set_eta_max(double etaMax)
+  {
+    _eta_max = etaMax;
+  }
+
+  double
+  get_eta_min() const
+  {
+    return _eta_min;
+  }
+
+  void
+  set_eta_min(double etaMin)
+  {
+    _eta_min = etaMin;
+  }
+
+  //! The embedding ID for the HepMC subevent to be analyzed.
+  //! embedding ID for the event
+  //! positive ID is the embedded event of interest, e.g. jetty event from pythia
+  //! negative IDs are backgrounds, .e.g out of time pile up collisions
+  //! Usually, ID = 0 means the primary Au+Au collision background
+  int get_embedding_id() const { return _embedding_id; }
+  //
+  //! The embedding ID for the HepMC subevent to be analyzed.
+  //! embedding ID for the event
+  //! positive ID is the embedded event of interest, e.g. jetty event from pythia
+  //! negative IDs are backgrounds, .e.g out of time pile up collisions
+  //! Usually, ID = 0 means the primary Au+Au collision background
+  void set_embedding_id(int id) { _embedding_id = id; }
+
+ private:
+  int _ievent;
+
+  TFile *_f;
+
+  std::string _foutname;
+
+  double _eta_min;
+  double _eta_max;
+
+  //! The embedding ID for the HepMC subevent to be analyzed.
+  //! positive ID is the embedded event of interest, e.g. jetty event from pythia
+  //! negative IDs are backgrounds, .e.g out of time pile up collisions
+  //! Usually, ID = 0 means the primary Au+Au collision background
+  int _embedding_id;
+};
+
+#endif  // __HFMLTriggerInterface_H__
