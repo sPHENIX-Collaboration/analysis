658fa07323e168ae08878f9b09697e7c2da2b766
Xu Sun <sunxuhit@gmail.com>
2018-11-12 22:58:42 -0500

    implement tower-by-tower calibration

---
+++ Prototype4/HCal/TowerCalib/Proto4TowerCalibLinkDef.h
@@ -0,0 +1,7 @@
+#ifdef __CINT__
+
+#pragma link C++ class Proto4TowerCalib-!;
+#pragma link C++ class Proto4TowerCalib::Eval_Run+;
+#pragma link C++ class Proto4TowerCalib::HCAL_Sample+;
+
+#endif /* __CINT__ */
--- Prototype4/HCal/macros/Fun4All_TestBeam_HCAL_SampleFrac.C
+++ Prototype4/HCal/macros/Fun4All_TestBeam_HCAL_SampleFrac.C
@@ -8,8 +8,8 @@
  */
 
 int
-// Fun4All_TestBeam_HCAL_SampleFrac(const int nEvents = 1000, const int posID = 0, const int addID = 0, const string det = "HCALOUT")
-Fun4All_TestBeam_HCAL_SampleFrac(const int nEvents = 1000, const int runID = 0, const string det = "HCALIN")
+Fun4All_TestBeam_HCAL_SampleFrac(const int nEvents = 1000, const int posID = 0, const int addID = 0, const string det = "HCALOUT")
+// Fun4All_TestBeam_HCAL_SampleFrac(const int nEvents = 1000, const int runID = 0, const string det = "HCALIN")
 {
 
   //---------------
@@ -37,8 +37,10 @@ Fun4All_TestBeam_HCAL_SampleFrac(const int nEvents = 1000, const int runID = 0, 
 
   // Hits file
   Fun4AllInputManager *hitsin = new Fun4AllDstInputManager("DSTin");
-  // std::string inputFile = Form("/sphenix/user/xusun/software/macros/macros/prototype4/data/Simulation/Cosmic_%s_%d%d.root",det.c_str(),posID,addID);
-  std::string inputFile = Form("/sphenix/user/xusun/software/macros/macros/prototype4/data/Simulation/BeamTest_SF_%d.root",runID);
+  std::string inputFile = Form("/sphenix/user/xusun/software/macros/macros/prototype4/data/Simulation/Cosmic_orig/Cosmic_%s_%d%d.root",det.c_str(),posID,addID);
+  // std::string inputFile = Form("/sphenix/user/xusun/software/macros/macros/prototype4/data/Simulation/Cosmic_Scint/Cosmic_%s_%d%d.root",det.c_str(),posID,addID);
+  // std::string inputFile = Form("/sphenix/user/xusun/software/macros/macros/prototype4/data/Simulation/BeamTest_SF_orig/BeamTest_SF_%d.root",runID);
+  // std::string inputFile = Form("/sphenix/user/xusun/software/macros/macros/prototype4/data/Simulation/BeamTest_SF_Scint/BeamTest_SF_%d.root",runID);
   hitsin->fileopen(inputFile.c_str());
 //  hitsin->AddListFile(inputFile); // you can also choose this and give a list of DST file names in the file.
   se->registerInputManager(hitsin);
@@ -47,8 +49,8 @@ Fun4All_TestBeam_HCAL_SampleFrac(const int nEvents = 1000, const int runID = 0, 
   gSystem->Load("libProto4_HCalSampleFrac.so");
 
   //load your analysis module.
-  // std::string outputFile = Form("OutPut/SampleFrac/Proto4SampleFrac_%s_%d%d.root",det.c_str(),posID,addID);
-  std::string outputFile = Form("OutPut/SampleFrac/Proto4SampleFrac_%s_%d.root",det.c_str(),runID);
+  std::string outputFile = Form("OutPut/SampleFrac/Proto4SampleFrac_%s_%d%d.root",det.c_str(),posID,addID);
+  // std::string outputFile = Form("OutPut/SampleFrac/Proto4SampleFrac_%s_%d.root",det.c_str(),runID);
   Proto4SampleFrac* hcal_ana = new Proto4SampleFrac(det.c_str(),outputFile.c_str());
   se->registerSubsystem(hcal_ana);
 
