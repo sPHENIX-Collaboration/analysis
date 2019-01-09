8f8f36baf9949f6f7ef12258704064e3ce7c3e1e
FrancescoVassalli <FrancescoVassalli@colorado.edu>
2019-01-09 16:30:38 -0700

    doxygen commenting

--- PhotonConversion/truthconversion/Conversion.h
+++ PhotonConversion/truthconversion/Conversion.h
@@ -1,3 +1,10 @@
+/**
+* @Conversion.h
+* @author Francesco Vassalli <Francesco.Vassalli@colorado.edu>
+* @version 1.0
+*
+* @brief Simple class to hold conversion information
+*/
 #ifndef CONVERSION_H__
 #define CONVERSION_H__
 
@@ -11,24 +18,30 @@ class SvtxTrackEval;
 class Conversion
 {
 public:
-  Conversion(){}
-  Conversion(SvtxTrackEval* trackeval){
+  Conversion(){verbosity=0;}
+  Conversion(SvtxTrackEval* trackeval,int verbosity=0){
     this->trackeval=trackeval;
+    this->verbosity=verbosity
   }
-  Conversion(PHG4VtxPoint* vtx){
+  Conversion(PHG4VtxPoint* vtx,int verbosity=0){
     this->vtx=vtx;
+    this->verbosity=verbosity
   }
-  Conversion(PHG4VtxPoint* vtx,SvtxTrackEval *trackeval){
+  Conversion(PHG4VtxPoint* vtx,SvtxTrackEval *trackeval,int verbosity=0){
     this->trackeval=trackeval;
     this->vtx=vtx;
+    this->verbosity=verbosity
   }
   ~Conversion(){
     //dont delete the points as you are not the owner and did not make your own copies
   }
+  /** sets the daughters of the conversion
+  * use this to set the electron and positron
+  * initializes both points but does not determine charge*/
   inline void setElectron(PHG4Particle* e){
     if (e1)
     {
-      if (e2)
+      if (e2&&verbosity>0)
       {
         std::cout<<"WARNING: oversetting conversion electrons"<<std::endl;
       }
@@ -43,6 +56,10 @@ public:
   inline void setTrackEval(SvtxTrackEval *trackeval){
     this->trackeval=trackeval;
   }
+  /**Sets the negative charge to {@link e1}
+  * @return false otherwise
+  * will print warnings if something is wrong with the Conversion
+  **/
   inline bool setElectron(){
     if (hasPair())
     {
@@ -54,14 +71,25 @@ public:
       }
       if (e1->get_pid()<0)
       {
-        std::cout<<"WARNING: bad charges in conversion"<<std::endl;
+        if (verbosity>0)
+        {
+          std::cout<<"Warning in Conversion bad charges in conversion"<<std::endl;
+        }
         return false;
       }
     }
     else{
-      if (e1->get_pid()<0)
+      if (!e1)
       {
-        std::cout<<"WARNING: only 1 positron in conversion"<<std::endl;
+        if (verbosity>0)
+        {
+          std::cout<<"Warning in Conversion no truth tracks set"<<std::endl;
+        }
+        return false;
+      }
+      else if (e1->get_pid()<0)
+      {
+        std::cout<<"Warning in Conversion only 1 positron in conversion"<<std::endl;
         return false;
       }
     }
@@ -73,6 +101,7 @@ public:
   inline PHG4VtxPoint* getVtx()const{
     return vtx;
   }
+  ///true if both electrons and photon are set
   inline bool isComplete()const{
     if (e1&&e2&&photon)
     {
@@ -82,6 +111,7 @@ public:
       return false;
     }
   }
+  ///check if both electrons are initialized
   inline bool hasPair()const{
     if (e1&&e2)
     {
@@ -91,12 +121,16 @@ public:
       return false;
     }
   }
+  ///sets the photon
   inline void setParent(PHG4Particle* parent){
     photon=parent;
   }
+  /** match the truth particles to reco tracks
+  * if the trackeval was not previously set it needs to be set now
+  * @return the number of matched tracks [0,2]*/
   int setRecoTracks(SvtxTrackEval* trackeval);
   int setRecoTracks();
-
+  /// @return the number of matched reco tracks
   inline int recoCount(){
     int r=0;
     if (reco1)
@@ -109,10 +143,12 @@ public:
     }
     return r;
   }
+  ///calls {@link setElectron()} then returns {@link e1}
   inline PHG4Particle* getElectron(){
     setElectron();
     return e1;
   }
+  ///calls {@link setElectron()} if true returns {@link e2} else returns {@link e1}
   inline PHG4Particle* getPositron(){
     if(setElectron()){
       return e2;
@@ -127,7 +163,10 @@ public:
   inline int getEmbed() const {return embedID;}
 
   inline void setEmbed(int embedID) {this->embedID=embedID;}
-
+  /**finds the cluster associated with {@link reco1} 
+  * if the trackeval was not previously set it needs to be set now
+  * if {@link reco1} not set will attempt to set
+  * @return -1 if no cluster is found*/
   int get_cluster_id();
   int get_cluster_id(SvtxTrackEval *trackeval);
 private:
@@ -138,10 +177,7 @@ private:
   SvtxTrack* reco1=NULL;
   SvtxTrack* reco2=NULL;
   SvtxTrackEval* trackeval=NULL;
-
   int embedID=0;
-  /*inline bool inAcceptance(){
-
-  }*/
+  int verbosity;
 };
 #endif //CONVERSION_H__
