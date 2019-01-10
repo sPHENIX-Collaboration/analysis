4de17ca5b0befbdc8894be7b69ef908f1565e548
FrancescoVassalli <FrancescoVassalli@colorado.edu>
2019-01-10 15:51:01 -0700

    added cut calculations

--- PhotonConversion/truthconversion/Conversion.cc
+++ PhotonConversion/truthconversion/Conversion.cc
@@ -71,3 +71,55 @@ int Conversion::get_cluster_id(SvtxTrackEval *trackeval){
   }
   return reco1->get_cal_cluster_id(SvtxTrack::CAL_LAYER(1));//id of the emcal
 }
+
+bool Conversion::hasSilicon(){
+  switch(recoCount){
+          case 2:
+            SvtxCluster *c1 = _svtxClusterMap->get(*(reco1->begin_clusters()));
+            SvtxCluster *c2 = _svtxClusterMap->get(*(reco2->begin_clusters()));
+            return c1->get_layer()<=_kNSiliconLayer||c2->get_layer()<=_kNSiliconLayer;
+            break;
+          case 1:
+            SvtxCluster *c1;
+            if (reco1)
+            {
+              c1 = _svtxClusterMap->get(*(reco1->begin_clusters()));
+            }
+            else{
+              c1 = _svtxClusterMap->get(*(reco2->begin_clusters()));
+            }
+            return c1->get_layer()<=_kNSiliconLayer;
+            break;
+          default:
+            return false;
+            break;
+        }
+}
+
+int Conversion::dLayer(){
+  if (recoCount()==2){
+          SvtxCluster *c1 = _svtxClusterMap->get(*(reco1->begin_clusters()));
+          SvtxCluster *c2 = _svtxClusterMap->get(*(reco2->begin_clusters()));
+          SvtxHit *h1 = _hitMap->get(*(c1->begin_hits()));
+          SvtxHit *h2 = _hitMap->get(*(c2->begin_hits()));
+          return abs(h1->get_layer()-h2->get_layer());
+        }
+        else return -1;
+        
+        //check that the first hits are close enough
+        if (c1->get_layer()>_kNSiliconLayer&&c2->get_layer()>_kNSiliconLayer)
+        {
+          if (abs(h1->get_layer()-h2->get_layer())>_kFirstHitStrict)
+          {
+            return false;
+          }
+        }
+        else{
+          if (abs(h1->get_layer()-h2->get_layer())>_kFirstHit)
+          {
+            return false;
+          }
+        }
+        //check the approach distance
+        return true;
+}
