3773883479c2d518e74d2317621cbd35e2435d76
weihuma <maweihui@fudan.edu.cn>
2021-02-22 21:32:45 -0500

    add out tree

--- TrackPid/ElectronPid.h
+++ TrackPid/ElectronPid.h
@@ -41,6 +41,32 @@ int GetNodes(PHCompositeNode *topNode);
 
  TrackPidAssoc *_track_pid_assoc;
  SvtxTrackMap *_track_map;
+
+ TTree *PID_tracktree;
+ 
+ void initializeVariables();
+ void initializeTrees();
+
+ double PID_tr_p;
+ double PID_tr_pt;
+
+ double PID_cemcdphi; 
+ double PID_cemcdeta; 
+ double PID_cemce3x3; 
+ 
+ double PID_hcalindphi; 
+ double PID_hcalindeta; 
+ double PID_hcaline3x3;
+
+ double PID_hcaloute3x3;
+
+ double PID_EcemcOP;
+ double PID_EcemcOP_cut;
+ 
+ double PID_EhcalOP;
+ double PID_EhcalOP_cut;
+
+
 };
 
 #endif
