fc1ecb5817fb3321c084f1deabeba4ac47c829b8
weihuma <maweihui@fudan.edu.cn>
2021-02-25 02:49:59 -0500

    modify

--- TrackPid/ElectronPid.h
+++ TrackPid/ElectronPid.h
@@ -41,10 +41,16 @@ public:
   int process_event(PHCompositeNode *topNode);
   int End(PHCompositeNode *topNode);
 
-  /// Set the cemce3x3/p cut limits
-  void setEOPcutlimits(float EOPlowerlimit, float EOPhigherlimit) { EOP_lowerlimit = EOPlowerlimit;EOP_higherlimit = EOPhigherlimit; }
-  /// Set the (hcaline3x3+hcaloute3x3)/p cut lower limit
+  /// Set the cemce3x3/p cut limits for electrons; default: 0.0<cemce3x3/p<100.0, means without cuts
+  void setEMOPcutlimits(float EMOPlowerlimit, float EMOPhigherlimit) { EMOP_lowerlimit = EMOPlowerlimit; EMOP_higherlimit = EMOPhigherlimit; }
+  /// Set the hcaline3x3/cemce3x3 cut limit for electrons; default: hcaline3x3/cemce3x3<100.0, means without cut
+  void setHinOEMcutlimit(float HinOEMhigherlimit) { HinOEM_higherlimit = HinOEMhigherlimit; }
+  /// Set the pt cut limit for Upsilon decay electrons: 0.0<pt<100.0 GeV, means without cut
+  void setPtcutlimit(float Ptlowerlimit, float Pthigherlimit) { Pt_higherlimit = Pthigherlimit; }
+
+  /// Set the (hcaline3x3+hcaloute3x3)/p cut lower limit for hadrons; default: 0.0<(hcaline3x3+hcaloute3x3)/p, means without cut
   void setHOPcutlimit(float HOPlowerlimit) { HOP_lowerlimit = HOPlowerlimit; }
+
 
   void set_output_ntuple(bool outputntuple) {output_ntuple = outputntuple;}
 
@@ -53,7 +59,12 @@ protected:
 
   TFile* OutputNtupleFile;
   std::string OutputFileName;
-  TNtuple* ntp2;
+  TNtuple* ntpbeforecut; //write ntuple before any cuts
+  TNtuple* ntpcutEMOP; //write ntuple with only EMOP cut
+  TNtuple* ntpcutEMOP_HinOEM; //write ntuple with EMOP & HinOM cuts
+  TNtuple* ntpcutEMOP_HinOEM_Pt; //write ntuple with EMOP & HinOEM & Pt cuts
+
+  TNtuple* ntpcutHOP; //write ntuple with only HOP cut
 
   int EventNumber;
 
@@ -65,21 +76,20 @@ int GetNodes(PHCompositeNode *topNode);
  SvtxTrackMap *_track_map;
 
 /// A float lower limit for cutting on cemce3x3/p
-  float EOP_lowerlimit;
-
+  float EMOP_lowerlimit;
 /// A float higher limit for cutting on cemce3x3/p
-  float EOP_higherlimit;
+  float EMOP_higherlimit;
+
+/// A float higher limit for cutting on hcaline3x3/cemce3x3
+  float HinOEM_higherlimit;
+
+/// A float lower limit for cutting on pt
+  float Pt_lowerlimit;
+/// A float higher limit for cutting on pt
+  float Pt_higherlimit;
 
 /// A float lower limit for cutting on (hcaline3x3+hcaloute3x3)/p
   float HOP_lowerlimit;
-
- // bool output_ntuple;
-
- // TFile* OutputNtupleFile;
- // std::string OutputFileName;
- // TNtuple* ntp2;
-
- // int EventNumber;
 
 
 
