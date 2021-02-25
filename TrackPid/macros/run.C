d3468b1c0c42afc0e870898507edbeb808de1d16
weihuma <maweihui@fudan.edu.cn>
2021-02-25 03:09:59 -0500

    modify

--- TrackPid/macros/run.C
+++ TrackPid/macros/run.C
@@ -2,21 +2,6 @@
 
 #include <GlobalVariables.C>
 
-#include <DisplayOn.C>
-#include <G4Setup_sPHENIX.C>
-#include <G4_Bbc.C>
-#include <G4_CaloTrigger.C>
-#include <G4_DSTReader.C>
-#include <G4_Global.C>
-#include <G4_HIJetReco.C>
-#include <G4_Input.C>
-#include <G4_Jets.C>
-#include <G4_ParticleFlow.C>
-#include <G4_Production.C>
-#include <G4_TopoClusterReco.C>
-#include <G4_Tracking.C>
-#include <G4_User.C>
-
 #include <fun4all/SubsysReco.h>
 #include <fun4all/Fun4AllServer.h>
 #include <fun4all/Fun4AllInputManager.h>
