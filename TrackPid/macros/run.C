83a867ccd8609462743a2daa6455703d6aea8461
weihuma <maweihui@fudan.edu.cn>
2021-02-25 06:35:28 -0500

    modify

--- TrackPid/macros/run.C
+++ TrackPid/macros/run.C
@@ -21,7 +21,7 @@ R__LOAD_LIBRARY(libtrackpid.so)
 
 void run(
   const char *inputFile = "/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_12fm_50kHz_bkg_0_12fm-0000000001-01998.root",
-  const string &outputroot = "/sphenix/u/weihuma/RunOutput/embedDST_sHijing_upsilon_0_12fm_EOP_0.7_1.5",
+  const string &outputroot = "/sphenix/u/weihuma/RunOutput/embedDST_sHijing_upsilon_0_12fm",
   bool output_ntuple = true
 )
 {
@@ -36,16 +36,15 @@ void run(
   ePid->Verbosity(0);
   ePid->setEMOPcutlimits(0.7,1.5);
   ePid->setHinOEMcutlimit(0.2);
-  //ePid->setPtcutlimit(2.0,30.0);
-  //ePid->setHOPcutlimit(0.3);
+  ePid->setPtcutlimit(2.0,30.0);
+  ePid->setHOPcutlimit(0.3);
   se->registerSubsystem(ePid);
 
   Fun4AllInputManager *in = new Fun4AllDstInputManager("DST_TRACKS");
   in->Verbosity(1);
-  in->fileopen(inputFile);
- // in->AddListFile("filelist.txt");
+  //in->fileopen(inputFile);
+  in->AddListFile("filelist.txt");
   se->registerInputManager(in);
-
 
   if(output_ntuple) {
   	Fun4AllOutputManager *outePid = new Fun4AllDstOutputManager("outePid",outputroot+"_ElectronPid_DST.root");
