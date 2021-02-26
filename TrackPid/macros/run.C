9c7260f87dfda059e0b7849ae9ab9ddc415226cb
weihuma <maweihui@fudan.edu.cn>
2021-02-26 01:45:01 -0500

    modify

--- TrackPid/macros/run.C
+++ TrackPid/macros/run.C
@@ -20,7 +20,7 @@ R__LOAD_LIBRARY(libtrackpid.so)
 #endif
 
 void run(
-  const char *inputFile = "/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_12fm_50kHz_bkg_0_12fm-0000000001-04611.root",
+  const char *inputFile = "/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_12fm_50kHz_bkg_0_12fm-0000000001-01998.root",
   const string &outputroot = "/sphenix/u/weihuma/RunOutput/embedDST_sHijing_upsilon_0_12fm",
   bool output_ntuple = true
 )
@@ -43,9 +43,9 @@ void run(
   Fun4AllInputManager *in = new Fun4AllDstInputManager("DST_TRACKS");
   in->Verbosity(1);
   //in->fileopen(inputFile);
-  in->AddListFile("filelist_0_12fm.txt");
- // in->AddListFile("filelist_0_20fm.txt");
- // in->AddListFile("filelist_0_488fm.txt");
+ // in->AddListFile("filelist_0_12fm.txt");
+  in->AddListFile("filelist_0_20fm.txt");
+ // in->AddListFile("filelist_0_4d88fm.txt");
   se->registerInputManager(in);
 
   if(output_ntuple) {
