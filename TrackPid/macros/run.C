32303dd4de47b5d1143565901ba3431f8924f25e
weihuma <maweihui@fudan.edu.cn>
2021-02-25 23:37:25 -0500

    run 0-20fm

--- TrackPid/macros/run.C
+++ TrackPid/macros/run.C
@@ -20,8 +20,8 @@ R__LOAD_LIBRARY(libtrackpid.so)
 #endif
 
 void run(
-  const char *inputFile = "/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_12fm_50kHz_bkg_0_12fm-0000000001-01998.root",
-  const string &outputroot = "/sphenix/u/weihuma/RunOutput/embedDST_sHijing_upsilon_0_12fm",
+  const char *inputFile = "/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04611.root",
+  const string &outputroot = "/sphenix/u/weihuma/RunOutput/embedDST_sHijing_upsilon_0_20fm",
   bool output_ntuple = true
 )
 {
