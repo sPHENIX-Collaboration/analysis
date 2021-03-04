903e41dc0b5f9027aff1bba58907bc6f101e570e
weihuma <maweihui@fudan.edu.cn>
2021-03-04 00:48:43 -0500

    modify

--- TrackPid/macros/run.C
+++ TrackPid/macros/run.C
@@ -11,19 +11,17 @@
 #include <anatutorial/AnaTutorial.h>
 #include </sphenix/u/weihuma/install/include/trackpidassoc/ElectronPid.h>
 #include </sphenix/u/weihuma/install/include/trackpidassoc/TrackPidAssoc.h>
-//#include </direct/phenix+u/workarea/lebedev/sPHENIX_new/analysis/EventMix/install/include/eventmix/sPHElectronPair.h>
+
 
 R__LOAD_LIBRARY(libanatutorial.so)
 R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libtrackpid.so)
-//R__LOAD_LIBRARY(libeventmix.so)
+
 #endif
 
 void run(
-//const char *inputFile = "/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_12fm_50kHz_bkg_0_12fm-0000000001-01998.root",
   const char *inputFile = "/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04594.root",
-  const string &outputroot = "/sphenix/u/weihuma/RunOutput/embedDST_sHijing_upsilon_0_12fm",
- // const string &outputroot = "/sphenix/u/weihuma/RunOutput/sHijing_0_20fm_HepMC",
+  const string &outputroot = "/sphenix/u/weihuma/RunOutput/embedDST_sHijing_upsilon_0_20fm",
   bool output_ntuple = true
 )
 {
@@ -33,7 +31,7 @@ void run(
   Fun4AllServer *se = Fun4AllServer::instance();
   se->Verbosity(1);
 
-  ElectronPid *ePid = new ElectronPid("ElectronPid",outputroot+"_ElectronPid_0001.root");
+  ElectronPid *ePid = new ElectronPid("ElectronPid",outputroot+"_ElectronPid.root");
   ePid->set_output_ntuple(output_ntuple);
   ePid->Verbosity(0);
   ePid->setEMOPcutlimits(0.7,1.5);
@@ -44,19 +42,15 @@ void run(
 
   Fun4AllInputManager *in = new Fun4AllDstInputManager("DST_TRACKS");
   in->Verbosity(1);
-  //in->fileopen(inputFile);
+  in->fileopen(inputFile);
  //in->AddListFile("filelist_0_4d88fm.txt"); //sHijing with Upsilon embeded;
-  in->AddListFile("filelist_0_12fm.txt"); //sHijing with Upsilon embeded;
+ // in->AddListFile("filelist_0_12fm.txt"); //sHijing with Upsilon embeded;
   //in->AddListFile("filelist_0_20fm.txt"); //sHijing with Upsilon embeded;
-
- //in->AddListFile("filelist_0_4d88fm_sHijing.txt"); //sHijing without Upsilon embeded;
-// in->AddListFile("filelist_0_12fm_sHijing.txt"); //sHijing without Upsilon embeded;
- // in->AddListFile("filelist_0_20fm_sHijing.txt"); //sHijing without Upsilon embeded;
   
   se->registerInputManager(in);
 
   if(output_ntuple) {
-  	Fun4AllOutputManager *outePid = new Fun4AllDstOutputManager("outePid",outputroot+"_ElectronPid_DST_0001.root");
+  	Fun4AllOutputManager *outePid = new Fun4AllDstOutputManager("outePid",outputroot+"_ElectronPid_DST.root");
   	outePid->AddNode("TrackPidAssoc");
   	se->registerOutputManager(outePid);
 	outePid->Verbosity(1);
