0eaa49f76fde766747dfe19241013ba99f8a19f9
weihuma <maweihui@fudan.edu.cn>
2021-03-09 20:35:09 -0500

    modify

--- EventMix/macro/runall.C
+++ EventMix/macro/runall.C
@@ -23,24 +23,30 @@ void runall()
   Fun4AllServer *se = Fun4AllServer::instance();
   se->Verbosity(1);
 
-  ElectronPid* eid = new ElectronPid("ElectronPid","Upsilon_electrons_cutting_ntuple.root");
+  ElectronPid* eid = new ElectronPid("ElectronPid","/sphenix/u/weihuma/RunOutput/EVENTMIX/Upsilon_electrons_cutting_ntuple.root");
   eid->setEMOPcutlimits(0.7,1.5);
   eid->setHinOEMcutlimit(0.2);
   eid->setPtcutlimit(2.0,30.0);
   eid->setHOPcutlimit(0.3);
   se->registerSubsystem(eid);
 
-  PairMaker *pmaker = new PairMaker("PairMaker","Upsilon_electrons_cutting_dummy.root");
+  PairMaker *pmaker = new PairMaker("PairMaker","/sphenix/u/weihuma/RunOutput/EVENTMIX/Upsilon_electrons_cutting_dummy.root");
   se->registerSubsystem(pmaker);
 
   Fun4AllInputManager *in = new Fun4AllDstInputManager("in");
   in->Verbosity(1);
   se->registerInputManager(in);
-  in->AddFile("/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-02990.root");  
-  in->AddFile("/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-02991.root");
+  in->AddFile("/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04594.root");  
+  in->AddFile("/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04595.root");
+  in->AddFile("/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04596.root");
+  in->AddFile("/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04597.root");
+  in->AddFile("/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04598.root");
+  in->AddFile("/sphenix/sim/sim01/sphnxpro/MDC1/embed/embedDST_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000001-04599.root");
+
+
   //in->AddListFile("listmb2.txt");
 
-  Fun4AllOutputManager *outee = new Fun4AllDstOutputManager("outee","Upsilon_electrons_cutting_test.root");
+  Fun4AllOutputManager *outee = new Fun4AllDstOutputManager("outee","/sphenix/u/weihuma/RunOutput/EVENTMIX/Upsilon_electrons_cutting_0_20fm.root");
   outee->Verbosity(1);
   outee->AddNode("ElectronPairs");
   se->registerOutputManager(outee);
