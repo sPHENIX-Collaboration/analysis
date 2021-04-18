fc804257afe2157daee79a5ea572722d625b1be6
weihuma <maweihui@fudan.edu.cn>
2021-04-18 08:25:17 -0400

    modify

--- TrackPid/src/ElectronPid.cc
+++ TrackPid/src/ElectronPid.cc
@@ -141,7 +141,7 @@ int ElectronPid::process_event(PHCompositeNode* topNode)
       nmvtx = 0;
       nintt = 0;
       ntpc = 0;
-/*
+
       for (SvtxTrack::ConstClusterKeyIter iter = track->begin_cluster_keys();
         iter != track->end_cluster_keys();
         ++iter)
@@ -157,7 +157,7 @@ int ElectronPid::process_event(PHCompositeNode* topNode)
         if (_nlayers_intt > 0 && layer >= _nlayers_maps && layer < _nlayers_maps + _nlayers_intt) nintt++;
         if (_nlayers_tpc > 0 && layer >= (_nlayers_maps + _nlayers_intt) && layer < (_nlayers_maps + _nlayers_intt + _nlayers_tpc)) ntpc++;
       }
- */
+ 
 
       double px = track->get_px();
       double py = track->get_py();
