58647f306b74e9f574f6c8db657e484b0aa62d0d
Joe Osborn <osbornjd@ornl.gov>
2022-08-15 16:27:51 -0400

    fix job sub script

--- Tracking/BenchmarkingTools/DailyCronJobScripts/MinBias50kHzHijing/submitJobs.sh
+++ Tracking/BenchmarkingTools/DailyCronJobScripts/MinBias50kHzHijing/submitJobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/bin/sphenix_setup.csh -n
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-8.3/opt/sphenix/core/bin/sphenix_setup.sh -n
 
 cd /sphenix/sim/sim01/sphnxpro/TrackingDailyBuild/
 if [ ! -d "$(date +"%Y")" ]; then
@@ -9,7 +9,9 @@ fi
 
 cd "$(date +"%Y")"
 
-mkdir "$(date +"%d-%m")"
+if [ ! -d "$(date +"%d-%m")" ]; then
+  mkdir "$(date +"%d-%m")"
+fi
 cd "$(date +"%d-%m")"
 
 git clone git@github.com:sPHENIX-Collaboration/analysis.git
