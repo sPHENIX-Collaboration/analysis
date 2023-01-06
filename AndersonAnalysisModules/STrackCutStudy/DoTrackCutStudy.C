1227702f8e1a1522f8675aae45634063e5af7364
ruse-traveler <derek.murphy.anderson@protonmail.com>
2023-01-06 15:14:29 -0500

    Add track cut study

---
+++ AndersonAnalysisModules/STrackCutStudy/DoTrackCutStudy.C
@@ -0,0 +1,54 @@
+// 'DoTrackCutStudy.C'
+// Derek Anderson
+// 12.15.2022
+//
+// Runs the 'STrackCutStudy'
+// class.
+
+#ifndef DOTRACKCUTSTUDY_C
+#define DOTRACKCUTSTUDY_C
+
+// root includes
+#include <TROOT.h>
+#include <TString.h>
+// user includes
+#include </sphenix/u/danderson/install/include/strackcutstudy/STrackCutStudy.h>
+
+// load libraries
+R__LOAD_LIBRARY(/sphenix/u/danderson/install/lib/libstrackcutstudy.so)
+
+
+
+void DoTrackCutStudy() {
+
+  // lower verbosity
+  gErrorIgnoreLevel = kWarning;
+
+  // i/o parameters
+  const TString sOutFile("trackCutStudy.embedOnly_withVtxDiffPlots.pt020n5pim.d19m12y2022.root");
+  const TString sInFileEO("input/merge/sPhenixG4_forTrackCutStudy_embedOnly0t599_g4svtxeval.pt020n5pim.d19m12y2022.root");
+  const TString sInFilePU("input/test/sPhenixG4_testWithPileup001_g4svtxEval.d18m12y2022.root");
+  const TString sInTupleEO("ntp_track");
+  const TString sInTuplePU("ntp_gtrack");
+
+  // study parameters
+  const Bool_t   doIntNorm(true);
+  const Double_t normalPtFracMin(0.20);
+  const Double_t normalPtFracMax(1.20);
+
+  /* TODO: add functions to specify applied cuts */
+
+  // run track cut study
+  STrackCutStudy *study = new STrackCutStudy();
+  study -> SetInputOutputFiles(sInFileEO, sInFilePU, sOutFile);
+  study -> SetInputTuples(sInTupleEO, sInTuplePU);
+  study -> SetStudyParameters(doIntNorm, normalPtFracMin, normalPtFracMax);
+  study -> Init();
+  study -> Analyze();
+  study -> End();
+
+}  // end 'DoTrackCutStudy()'
+
+#endif
+
+// end ------------------------------------------------------------------------
