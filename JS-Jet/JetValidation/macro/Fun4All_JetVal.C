550a4aab00a8d197c73ca42beb756b56d557af52
timothyrinn <trinn2@illinois.edu>
2023-02-27 11:49:18 -0500

    updated to use new towerinfo objects and updated macro to use the default jet reconstruction chain

--- JS-Jet/JetValidation/macro/Fun4All_JetVal.C
+++ JS-Jet/JetValidation/macro/Fun4All_JetVal.C
@@ -12,19 +12,11 @@
 #include <phool/PHRandomSeed.h>
 #include <phool/recoConsts.h>
 
-#include <g4jets/FastJetAlgo.h>
-#include <g4jets/JetReco.h>
-#include <g4jets/TowerJetInput.h>
-#include <g4jets/TruthJetInput.h>
-
 #include <g4centrality/PHG4CentralityReco.h>
 
-#include <jetbackground/CopyAndSubtractJets.h>
-#include <jetbackground/DetermineTowerBackground.h>
-#include <jetbackground/FastJetAlgoSub.h>
-#include <jetbackground/RetowerCEMC.h>
-#include <jetbackground/SubtractTowers.h>
-#include <jetbackground/SubtractTowersCS.h>
+
+#include <HIJetReco.C>
+
 
 #include <jetvalidation/JetValidation.h>
 
@@ -35,22 +27,12 @@ R__LOAD_LIBRARY(libJetValidation.so)
 R__LOAD_LIBRARY(libg4centrality.so)
 R__LOAD_LIBRARY(libg4dst.so)
 
-namespace Enable
-{
-  bool HIJETS = true;
-  int HIJETS_VERBOSITY = 1;
-}  // namespace Enable
 
-namespace G4HIJETS
-{
-  bool do_flow = true;
-  bool do_CS = false;
-}  // namespace G4HIJETS
 #endif
 
 
-void Fun4All_JetVal(const char *filelisttruth = "dst_truth_test.list",
-                     const char *filelistcalo = "dst_calo_test.list",  
+void Fun4All_JetVal(const char *filelisttruth = "dst_truth_jet.list",
+                     const char *filelistcalo = "dst_calo_cluster.list",  
                      const char *outname = "outputest.root")
 {
 
@@ -66,60 +48,8 @@ void Fun4All_JetVal(const char *filelisttruth = "dst_truth_test.list",
   cent->GetCalibrationParameters().ReadFromFile("centrality", "xml", 0, 0, string(getenv("CALIBRATIONROOT")) + string("/Centrality/"));
   se->registerSubsystem( cent );
 
-  RetowerCEMC *rcemc = new RetowerCEMC(); 
-  rcemc->Verbosity(verbosity); 
-  se->registerSubsystem(rcemc);
-
-  JetReco *towerjetreco = new JetReco();
-  towerjetreco->add_input(new TowerJetInput(Jet::CEMC_TOWER_RETOWER));
-  towerjetreco->add_input(new TowerJetInput(Jet::HCALIN_TOWER));
-  towerjetreco->add_input(new TowerJetInput(Jet::HCALOUT_TOWER));
-  towerjetreco->add_algo(new FastJetAlgo(Jet::ANTIKT, 0.2), "AntiKt_Tower_HIRecoSeedsRaw_r02");
-  towerjetreco->set_algo_node("ANTIKT");
-  towerjetreco->set_input_node("TOWER");
-  towerjetreco->Verbosity(verbosity); 
-  se->registerSubsystem(towerjetreco);
-  
-  DetermineTowerBackground *dtb = new DetermineTowerBackground();
-  dtb->SetBackgroundOutputName("TowerBackground_Sub1");
-  dtb->SetFlow(G4HIJETS::do_flow);
-  dtb->SetSeedType(0);
-  dtb->SetSeedJetD(3);
-  dtb->Verbosity(verbosity); 
-  se->registerSubsystem(dtb);
-  
-  CopyAndSubtractJets *casj = new CopyAndSubtractJets();
-  casj->SetFlowModulation(G4HIJETS::do_flow);
-  casj->Verbosity(verbosity); 
-  se->registerSubsystem(casj);
-  
-  DetermineTowerBackground *dtb2 = new DetermineTowerBackground();
-  dtb2->SetBackgroundOutputName("TowerBackground_Sub2");
-  dtb2->SetFlow(G4HIJETS::do_flow);
-  dtb2->SetSeedType(1);
-  dtb2->SetSeedJetPt(7);
-  dtb2->Verbosity(verbosity); 
-  se->registerSubsystem(dtb2);
-  
-  SubtractTowers *st = new SubtractTowers();
-  st->SetFlowModulation(G4HIJETS::do_flow);
-  st->Verbosity(verbosity);
-  se->registerSubsystem(st);
-  
-  towerjetreco = new JetReco();
-  towerjetreco->add_input(new TowerJetInput(Jet::CEMC_TOWER_SUB1));
-  towerjetreco->add_input(new TowerJetInput(Jet::HCALIN_TOWER_SUB1));
-  towerjetreco->add_input(new TowerJetInput(Jet::HCALOUT_TOWER_SUB1));
-  towerjetreco->add_algo(new FastJetAlgoSub(Jet::ANTIKT, 0.2, 1), "AntiKt_Tower_r02_Sub1");
-  towerjetreco->add_algo(new FastJetAlgoSub(Jet::ANTIKT, 0.3, 1), "AntiKt_Tower_r03_Sub1");
-  towerjetreco->add_algo(new FastJetAlgoSub(Jet::ANTIKT, 0.4, 1), "AntiKt_Tower_r04_Sub1");
-  towerjetreco->add_algo(new FastJetAlgoSub(Jet::ANTIKT, 0.5, 1), "AntiKt_Tower_r05_Sub1");
-  towerjetreco->set_algo_node("ANTIKT");
-  towerjetreco->set_input_node("TOWER");
-  towerjetreco->Verbosity(verbosity);
-  se->registerSubsystem(towerjetreco);
-  
-
+  HIJetReco();
+ 
 
   JetValidation *myJetVal = new JetValidation("AntiKt_Tower_r04_Sub1", "AntiKt_Truth_r04", outname);
 
@@ -139,7 +69,7 @@ void Fun4All_JetVal(const char *filelisttruth = "dst_truth_test.list",
   se->registerInputManager(in2);
 
   
-  se->run(-1);
+  se->run(10);
   se->End();
 
   gSystem->Exit(0);
