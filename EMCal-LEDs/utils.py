65ed5e26f3a2dfd6c7719d30ee5b733db7656f35
Apurva Narde <apurva.narde@rutgers.edu>
2023-05-23 12:03:39 -0400

    utils: added function to make event display

--- EMCal-LEDs/utils.py
+++ EMCal-LEDs/utils.py
@@ -8,8 +8,9 @@ import shutil
 parser = argparse.ArgumentParser()
 subparser = parser.add_subparsers(dest='command')
 
-create = subparser.add_parser('create', help='Create file lists.')
-run    = subparser.add_parser('run', help='Run LEDTowerBuilder on the given file list.')
+create  = subparser.add_parser('create', help='Create file lists.')
+run     = subparser.add_parser('run', help='Run LEDTowerBuilder on the given file list.')
+evtDisp = subparser.add_parser('evtDisp', help='Create event display (json) given prdf and event number.')
 
 create.add_argument('-i', '--run-list', type=str, nargs='+' , help='List of run numbers.')
 create.add_argument('-p', '--prdf-dir', type=str, default='/direct/sphenix+lustre01/sphnxpro/rawdata/commissioning/emcal/calib', help='Directory containing the prdf files. Default: /direct/sphenix+lustre01/sphnxpro/rawdata/commissioning/emcal/calib')
@@ -17,8 +18,13 @@ create.add_argument('-o', '--output-dir', type=str, default='files', help='Direc
 
 run.add_argument('-i', '--file-list', type=str, help='File list containing prdfs to analyze.', required=True)
 run.add_argument('-n', '--nevents', type=int, default = -1, help='Number of events to analyze. Default: -1 (analyze all)')
+run.add_argument('-s', '--skip', type=int, default = 0, help='Number of events to skip. Default: 0 (no skip)')
 run.add_argument('-o', '--output', type=str, default = 'data/LEDTowerBuilder.root', help='Output root file. Default: data/LEDTowerBuilder.root')
 run.add_argument('-m', '--max', type=int, default = 10000, help='Maximum number of events to analyze at once. Default: 10000')
+
+evtDisp.add_argument('-i', '--prdf', type=str, help='Prdfs to analyze.', required=True)
+evtDisp.add_argument('-n', '--event', type=int, default = 1, help='Event number to use. Default: 1.')
+evtDisp.add_argument('-o', '--output', type=str, default = 'event-display/test.json', help='Output json file. Default: test.json.')
 
 args = parser.parse_args()
 
@@ -44,6 +50,7 @@ def run_analysis():
     file_list          = os.path.abspath(args.file_list)
     nevents            = args.nevents
     output             = args.output
+    skip               = args.skip
     max_events_per_run = args.max
 
     print(f'file list: {file_list}')
@@ -57,25 +64,25 @@ def run_analysis():
             command = f'dpipe -d n -s f -i {line}'
             dpipe_process = subprocess.run(command.split(), stdout=subprocess.PIPE)
             events = subprocess.run(['wc','-l'], input=dpipe_process.stdout, stdout=subprocess.PIPE)
-            events = int(events.stdout.decode("utf-8"))
+            events = int(events.stdout.decode('utf-8'))
 
             total_events += events
             print(f'prdf: {line}, events: {events}')
-            if(total_events > nevents and nevents >= 0):
+            if(total_events > nevents+skip and nevents >= 0):
                 break
 
     nevents = total_events if nevents == -1 else nevents
     print(f'total events: {total_events}')
     if(nevents != total_events):
         print(f'events to analyze: {nevents}')
+    print(f'skip: {skip}')
 
-    runs = int(np.ceil(nevents / max_events_per_run))
+    runs = int(np.ceil((nevents+skip) / max_events_per_run))
     max_events_per_run = min(max_events_per_run, nevents)
 
     log = os.path.basename(output).split('.')[0]
     output_dir = os.path.dirname(output)
     process_events = max_events_per_run
-    skip = 0
 
     if(runs > 1):
         print(f'Runs: {runs}')
@@ -84,7 +91,7 @@ def run_analysis():
         merge_files = []
         for i in range(runs):
             subprocess.run(['echo', f'Run: {i}'])
-            command = f"./bin/Fun4All_LEDTowerBuilder {process_events} {skip} {file_list} {output_dir}/test-{i}.root &> {output_dir}/log/log-test-{i}.txt &"
+            command = f'./bin/Fun4All_LEDTowerBuilder {process_events} {skip} {file_list} {output_dir}/test-{i}.root &> {output_dir}/log/log-test-{i}.txt &'
             print(command)
             skip += max_events_per_run
             process_events = min(max_events_per_run, nevents-(i+1)*max_events_per_run)
@@ -95,7 +102,7 @@ def run_analysis():
         print(f'hadd -n 50 {output} {merge_files}')
 
     else:
-        command = f"./bin/Fun4All_LEDTowerBuilder {process_events} {skip} {file_list} {output} &> {output_dir}/log/log-{log}.txt &"
+        command = f'./bin/Fun4All_LEDTowerBuilder {process_events} {skip} {file_list} {output} &> {output_dir}/log/log-{log}.txt &'
         print(command)
 
 
@@ -104,9 +111,39 @@ def run_analysis():
     # print(command)
     # subprocess.run(['root','-b','-l','-q',f'macro/Fun4All_LEDTowerBuilder.C({nevents}, \"{file_list}\", \"{output}\")'])
 
+def event_display():
+    prdf_input = os.path.abspath(args.prdf)
+    event      = args.event
+    output     = args.output
+
+    print(f'prdf: {prdf_input}')
+    print(f'event: {event}')
+    print(f'output: {output}')
+
+    # isolate the specified event from the prdf
+    command = f'eventcombiner -v -i -e {event} -n 1 -f -p data/temp/test.prdf {prdf_input}'
+    print(f'command: {command.split()}')
+    subprocess.run(command.split())
+
+    # create temp file list containing the prdf
+    with open('files/temp.txt','w') as f:
+        f.write(f'{prdf_input}\n')
+
+    # running the LEDTowerBuilder over the prdf
+    # using 2 events as the first one is skipped
+    command = f'bin/Fun4All_LEDTowerBuilder 2 0 files/temp.txt data/temp/test.root'
+    print(f'command: {command.split()}')
+    subprocess.run(command.split())
+
+    # running the event-display which generates the json output
+    command = f'bin/event-display data/temp/test.root {output}'
+    print(f'command: {command.split()}')
+    subprocess.run(command.split())
 
 if __name__ == '__main__':
     if(args.command == 'create'):
         create_file_list()
     if(args.command == 'run'):
         run_analysis()
+    if(args.command == 'evtDisp'):
+        event_display()
