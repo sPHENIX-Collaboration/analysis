fd28648b0060b365af1a1bfaa337dabc78f9fc60
Apurva Narde <apurva.narde@rutgers.edu>
2023-07-14 10:58:26 -0400

    Update build_package.sh
    - No need to sync from git each time before building
    - Compiling CaloReco separately currently to account for the TowerInfoMask which is not yet in the official repository.

--- EMCal-position-dependent-calibration/scripts/build_package.sh
+++ EMCal-position-dependent-calibration/scripts/build_package.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
-rm -rf src/g4eval
-cd /gpfs/mnt/gpfs02/sphenix/user/anarde/sPHENIX/coresoftware
-git pull
-cd -
-
-cp -r  /gpfs/mnt/gpfs02/sphenix/user/anarde/sPHENIX/coresoftware/simulation/g4simulation/g4eval/ src
-cp src/CaloEvaluator.cc src/CaloEvaluator.h src/g4eval
-
 rm -rf build
-mkdir -p build
-cd build
+mkdir -p build/CaloReco build/g4eval
+cd build/g4eval
 
-echo "Running autogen"
-$(readlink -f ../src/g4eval)/autogen.sh --prefix=$MYINSTALL
+echo "Running autogen: g4eval"
+$(readlink -f ../../src/g4eval)/autogen.sh --prefix=$MYINSTALL
 
-echo "Running make"
-make -j 8
+echo "Running make install"
+make -j 8 install
+
+cd ../CaloReco
+
+echo "Running autogen: CaloReco"
+$(readlink -f ../../src/CaloReco)/autogen.sh --prefix=$MYINSTALL
 
 echo "Running make install"
 make -j 8 install
