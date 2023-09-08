8540ce0613a91664b3f139ee3e93992139847e82
Silas K Grossberndt <silas.grossberndt@gmail.com>
2023-09-08 12:23:46 -0400

    minimal installation version, working on getting everything in the proper compiliation

--- CaloTransverseEnergy/src/GetET.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-#Run analysis over a set of runs, taken from csv file
-source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana
-./GetET "/sphenix/tg/tg01/jets/ahodges/run23_production/21518/DST-00021518-0000.root"
