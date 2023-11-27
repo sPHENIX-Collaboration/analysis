8038bf731d0280c75dc4b46783b4e7403ff4cba0
Apurva Narde <apurva.narde@rutgers.edu>
2023-11-26 20:41:18 -0600

    Update genCuts.py
    - Instead of explicitly plugging in values to get the range, created a getRange function to do the job.

--- pi0-vn/emcalTreeMaker/genCuts.py
+++ pi0-vn/emcalTreeMaker/genCuts.py
@@ -8,14 +8,18 @@ parser.add_argument('-o', '--output', type=str, default='cuts.txt', help='Name o
 
 args = parser.parse_args()
 
+def getRange(low, high, step, precision=4):
+    n = int((high-low)/step)+1 # number of samples from low to high
+    return np.round(np.linspace(low,high,n),precision)
+
 if __name__ == '__main__':
     output = os.path.realpath(args.output)
     print(f'Output: {output}')
 
-    e      = np.round(np.linspace(0.5,2,int((2-0.5)/0.25)+1),4)
+    e      = getRange(0.5,2,0.25)
     chi    = [4]
-    deltaR = np.round(np.linspace(0.05,0.1,int((0.1-0.05)/0.01)+1),4)
-    asym   = np.round(np.linspace(0.3, 0.8, int((0.8-0.3)/0.1)+1),4)
+    deltaR = getRange(0.05,0.1,0.01)
+    asym   = getRange(0.3,0.8,0.1)
 
     print(f'Cluster E:           {e}')
     print(f'Cluster chi:         {chi}')
