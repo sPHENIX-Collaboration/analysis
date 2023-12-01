4c61e29b4b240e7ba3deb1c677ea12d9c2e913a2
Apurva Narde <apurva.narde@rutgers.edu>
2023-12-01 11:36:54 -0600

    Added genFun4Allv2 script
    - used by condor for processing validation jobs

---
+++ EMCal-position-dependent-calibration/scripts/genFun4Allv2.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${LOGNAME}
+export MYINSTALL="$HOME/Documents/sPHENIX/install"
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n new
+source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
+
+exe=${1}
+calo=${2}
+g4=${2}
+output=${3}
+
+# print the environment - needed for debugging
+printenv
+
+mkdir -p output
+$exe $calo $g4 $output
