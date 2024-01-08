dc81e57f821b1c0bfe6029d85f66d0656d2da814
Apurva Narde <apurva.narde@rutgers.edu>
2024-01-08 00:10:10 -0500

    Update utils.py
    - added two dummy utility functions (to be cleaned and simplified later)

--- pi0-vn/emcalTreeMaker/utils.py
+++ pi0-vn/emcalTreeMaker/utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import pandas as pd
 import numpy as np
 import subprocess
 import argparse
@@ -22,22 +23,53 @@ pi0Ana = subparser.add_parser('pi0Ana', help='Create condor submission directory
 pi0Ana.add_argument('-i', '--ntp-list', type=str, help='List of Ntuples', required=True)
 pi0Ana.add_argument('-c', '--cuts', type=str, help='List of cuts', required=True)
 pi0Ana.add_argument('-n', '--events', type=int, help='Number of events to analyze.', required=True)
-pi0Ana.add_argument('-j', '--jobs', type=int, default=19999, help='Number of jobs to submit. Default: 1.')
+pi0Ana.add_argument('-j', '--jobs', type=int, default=19999, help='Number of jobs to submit. Default: 19999.')
 pi0Ana.add_argument('-e', '--script', type=str, default='genPi0Ana.sh', help='Job script to execute. Default: genPi0Ana.sh')
 pi0Ana.add_argument('-b', '--executable', type=str, default='bin/pi0Ana', help='Executable. Default: bin/pi0Ana')
 pi0Ana.add_argument('-d', '--output', type=str, default='test', help='Output Directory. Default: ./test')
 pi0Ana.add_argument('-s', '--memory', type=int, default=1, help='Memory (units of GB) to request per condor submission. Default: 1 GB.')
 pi0Ana.add_argument('-l', '--log', type=str, default='/tmp/anarde/dump/job-$(ClusterId)-$(Process).log', help='Condor log file.')
 
+dummy = subparser.add_parser('dummy', help='Identify events per run from input text file.')
+dummy.add_argument('-i', '--entries', type=str, help='Text file of all runs and events', required=True)
+
+dummy2 = subparser.add_parser('dummy2', help='Create pi0Ana submission command for multiple runs.')
+dummy2.add_argument('-i', '--fp', type=str, help='Text file of all runs and events', required=True)
+dummy2.add_argument('-d', '--directory', type=str, default='.', help='output directory. Default: .')
+dummy2.add_argument('-j', '--jobs', type=str, default=999, help='Number of jobs. Default: 999')
+
 args = parser.parse_args()
 
+def process_dummy2():
+    fp     = os.path.realpath(args.fp)
+    output = args.directory
+    jobs   = args.jobs
+
+    print(f'Input file: {fp}')
+    print(f'Output Directory: {output}')
+    print(f'Jobs: {jobs}')
+
+    with open(fp) as file:
+        for line in file:
+            print(f'./utils.py pi0Ana -i {line.split()[0]}.list -c cuts.txt -n {line.split()[1]} -j {jobs} -d {output}/{line.split()[0]} && ', end='')
+
+
+def process_dummy():
+    entries = os.path.realpath(args.entries)
+
+    print(f'Input file: {entries}')
+
+    df = pd.read_csv(entries, delimiter=':', names=['run','entries'])
+    df.run = df.run.apply(lambda x: x.split('/')[6])
+    print(df.groupby(by='run').sum())
+
 def create_f4a_jobs():
-    run_list_dir   = os.path.realpath(args.run_list_dir)
-    output_dir = os.path.realpath(args.output)
-    f4a        = os.path.realpath(args.f4a)
-    executable = os.path.realpath(args.executable)
-    memory     = args.memory
-    log        = args.log
+    run_list_dir = os.path.realpath(args.run_list_dir)
+    output_dir   = os.path.realpath(args.output)
+    f4a          = os.path.realpath(args.f4a)
+    executable   = os.path.realpath(args.executable)
+    memory       = args.memory
+    log          = args.log
 
     print(f'Run List Directory: {run_list_dir}')
     print(f'Output Directory: {output_dir}')
@@ -130,3 +162,7 @@ if __name__ == '__main__':
         create_f4a_jobs()
     if(args.command == 'pi0Ana'):
         create_pi0Ana_jobs()
+    if(args.command == 'dummy'):
+        process_dummy()
+    if(args.command == 'dummy2'):
+        process_dummy2()
