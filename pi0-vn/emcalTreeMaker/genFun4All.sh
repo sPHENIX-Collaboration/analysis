fcee61c615cbb0b4a9af33231a180f0b423051b6
Apurva Narde <apurva.narde@rutgers.edu>
2024-01-26 15:39:41 -0600

    Update condor submission scripts

--- pi0-vn/emcalTreeMaker/genFun4All.sh
+++ pi0-vn/emcalTreeMaker/genFun4All.sh
@@ -10,11 +10,10 @@ source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
 exe=${1}
 input=${2}
 qa=${3}
-ntp=${4}
-pi0=${5}
+diphoton=${4}
 
 # print the environment - needed for debugging
 printenv
 
 mkdir -p output
-$exe $input $qa $ntp $pi0
+$exe $input $qa $diphoton
