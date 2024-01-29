f55176551f3c64bae1ef8205dddc1521e210408b
Hao-Ren Jheng <hrjheng@mit.edu>
2024-01-29 00:24:04 -0500

    condor scripts

--- dNdEta_Run2023/analysis_INTT/condor_BeamspotReco.sh
+++ dNdEta_Run2023/analysis_INTT/condor_BeamspotReco.sh
@@ -5,7 +5,7 @@ export HOME=/sphenix/u/${LOGNAME}
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n new
 
-export MYINSTALL=/sphenix/u/hjheng/install
+export MYINSTALL=$HOME/install
 export LD_LIBRARY_PATH=$MYINSTALL/lib:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=$MYINSTALL/include:$ROOT_INCLUDE_PATH
 
