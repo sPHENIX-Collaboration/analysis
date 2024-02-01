0b1bbab5cda9788103d12fcdcb0a8d07a32d4bbe
bkimelman <benjamin.kimelman@vanderbilt.edu>
2024-02-01 17:55:14 -0500

    commit

--- JS-Jet/JetValidation/macro/Fun4All_JetVal.C
+++ JS-Jet/JetValidation/macro/Fun4All_JetVal.C
@@ -4,6 +4,7 @@
 #include <fun4all/Fun4AllServer.h>
 #include <fun4all/Fun4AllInputManager.h>
 #include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllNoSyncDstInputManager.h>
 
 #include <fun4all/Fun4AllDstOutputManager.h>
 #include <fun4all/Fun4AllOutputManager.h>
@@ -65,11 +66,11 @@ void Fun4All_JetVal(const char *filelisttruth = "dst_truth_jet.list",
   intrue->AddListFile(filelisttruth,1);
   se->registerInputManager(intrue);
 
-  Fun4AllInputManager *in2 = new Fun4AllDstInputManager("DSTcalo");
+  Fun4AllInputManager *in2 = new Fun4AllNoSyncDstInputManager("DSTcalo");
   in2->AddListFile(filelistcalo,1);
   se->registerInputManager(in2);
 
-  Fun4AllInputManager *in3 = new Fun4AllDstInputManager("DSTglobal");
+  Fun4AllInputManager *in3 = new Fun4AllNoSyncDstInputManager("DSTglobal");
   in3->AddListFile(filelistglobal,1);
   se->registerInputManager(in3);
   
