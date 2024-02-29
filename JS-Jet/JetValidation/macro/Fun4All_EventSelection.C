a8913fee77152c729c4975543ce9b9c6831c6c20
jamesjl3 <jennifer.l.james@vanderbilt.edu>
2024-02-29 10:47:13 -0500

    Fun4all for EventSelection.cc/h

--- JS-Jet/JetValidation/macro/Fun4All_EventSelection.C
+++ JS-Jet/JetValidation/macro/Fun4All_EventSelection.C
@@ -6,44 +6,47 @@
 // Include necessary headers
 #include <fun4all/Fun4AllServer.h>
 #include <fun4all/Fun4AllDstInputManager.h>
-#include <fun4all/Fun4AllSyncManager.h>
 #include <fun4all/Fun4AllDstOutputManager.h>
+#include <phool/phool.h>
+#include <fun4all/Fun4AllSyncManager.h>
 #include <fun4all/Fun4AllInputManager.h>
 #include <fun4all/Fun4AllOutputManager.h>
 #include <phool/PHRandomSeed.h>
 #include <phool/recoConsts.h>
-#include <caloembedding/caloTowerEmbed.h>
 #include <jetvalidation/EventSelection.h>
 
 // Load required libraries
 R__LOAD_LIBRARY(libg4dst.so)
 R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libJetValidation.so)
+
 
 // Define the Fun4All macro
-  void Fun4All_EventSelection(
-			      //			      const int nEvents = 1000,
-			      const char* filelistdata = "dst_data.list",
-			      const char* outputFile = "eventselectest.root"
-			      ) {
+  void Fun4All_EventSelection(const char *filelisttruth = "dst_truth_jet.list",
+			    const char* outname = "eventselectest.root") {
   // Create Fun4All server instance
   Fun4AllServer *se = Fun4AllServer::instance();
   int verbosity = 0; // Adjust verbosity level as needed
   se->Verbosity(verbosity);
 
-  // Initialize constants and configurations
-  recoConsts *rc = recoConsts::instance();
-  rc->set_StringFlag("CDB_GLOBALTAG", "ProdA_2023");
-  rc->set_uint64Flag("TIMESTAMP", 23745);
-
   // Register the EventSelection subsystem
-  EventSelection *myEventSelection = new EventSelection();
+  EventSelection *myEventSelection = new EventSelection("AntiKt_Truth_r04", outname); // Assuming you want to pass a double and an empty string
   se->registerSubsystem(myEventSelection);
 
+  Fun4AllInputManager *intrue = new Fun4AllDstInputManager("DSTtruth");
+  intrue->AddListFile(filelisttruth,1);
+  se->registerInputManager(intrue);
+  /*
   // Set up Fun4All input manager for DST data
-  Fun4AllInputManager *inData = new Fun4AllDstInputManager("DSTData", "DST");
+  Fun4AllDstInputManager *inData = new Fun4AllDstInputManager("DSTData");
   inData->AddListFile(filelistdata);
   se->registerInputManager(inData);
-
+  
+  // Set up Fun4All output manager
+  Fun4AllDstOutputManager *outData = new Fun4AllDstOutputManager();
+  outData->outfileopen(outname);
+  se->registerOutputManager(outData);
+*/
   // Run the analysis
   se->run(1000);
   se->End();
