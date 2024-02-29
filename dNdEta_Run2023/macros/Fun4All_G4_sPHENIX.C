945191e704c855b607bb5a42d74e753064cd4bab
Hao-Ren Jheng <hrjheng@mit.edu>
2024-02-29 17:21:39 -0500

    HepMC, PHG4Particle, InttRawHit info; format

--- dNdEta_Run2023/macros/Fun4All_G4_sPHENIX.C
+++ dNdEta_Run2023/macros/Fun4All_G4_sPHENIX.C
@@ -1,6 +1,11 @@
 #ifndef MACRO_FUN4ALLG4SPHENIX_C
 #define MACRO_FUN4ALLG4SPHENIX_C
 
+#include <ffamodules/CDBInterface.h>
+#include <ffamodules/FlagHandler.h>
+#include <ffamodules/HeadReco.h>
+#include <ffamodules/SyncReco.h>
+
 #include <GlobalVariables.C>
 
 #include <G4Setup_sPHENIX.C>
@@ -9,10 +14,14 @@
 #include <Trkr_Clustering.C>
 #include <Trkr_RecoInit.C>
 
+#include <fun4all/Fun4AllDstInputManager.h>
 #include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllInputManager.h>
 #include <fun4all/Fun4AllOutputManager.h>
 #include <fun4all/Fun4AllServer.h>
 
+#include <calotrigger/MinimumBiasClassifier.h>
+#include <centrality/CentralityReco.h>
 #include <g4centrality/PHG4CentralityReco.h>
 #include <trackreco/PHTruthVertexing.h>
 
@@ -24,36 +33,58 @@ R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libffamodules.so)
 R__LOAD_LIBRARY(libdNdEtaINTT.so)
 R__LOAD_LIBRARY(libcentrality_io.so)
+R__LOAD_LIBRARY(libcentrality.so)
 R__LOAD_LIBRARY(libg4centrality.so)
+R__LOAD_LIBRARY(libcalotrigger.so)
 
-int Fun4All_G4_sPHENIX(const bool rundata = true, const string generator = "HIJING", const int nEvents = -1, const string &outputFile = "dataNtuple.root", const int process = 0)
+int Fun4All_G4_sPHENIX(                           //
+    const bool rundata = true,                    //
+    const int runnumber = 20869,                  //
+    const string generator = "HIJING",            //
+    const int nEvents = 1,                        //
+    const string &outputFile = "testNtuple.root", //
+    const int process = 0                         //
+)
 {
     int skip;
     if (rundata)
         skip = nEvents * process;
     else
         skip = 0;
+
     Fun4AllServer *se = Fun4AllServer::instance();
     se->Verbosity(1);
 
     recoConsts *rc = recoConsts::instance();
+    rc->set_StringFlag("CDB_GLOBALTAG", "ProdA_2023");
+    // 64 bit timestamp
+    rc->set_uint64Flag("TIMESTAMP", runnumber);
 
-    Input::VERBOSITY = INT_MAX;
+    Input::VERBOSITY = 1E9;
     Input::READHITS = true;
 
     string infile;
     if (rundata)
     {
-        infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/data/run_00020869/ana.382/beam_intt_combined-dst-00020869-0000.root";
+        // hardcoded for now, fix in the future
+        // infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/data/run_00020869/ana.382/beam_intt_combined-dst-00020869-0000.root";
+        infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/data/run_" + std::string(TString::Format("%08d", runnumber).Data()) + "/ana.382/beam_intt_combined-dst-" +
+                 std::string(TString::Format("%08d", runnumber).Data()) + "-0000.root";
     }
     else
     {
         if (generator == "HIJING")
-            infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/simulation/ana.398/HIJING/fullSim/magOff/detectorAligned/dstSet_00000/dNdeta-sim-HIJING-000-" + std::string(TString::Format("%05d", process).Data()) + ".root";
+            infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/simulation/ana.398/HIJING/fullSim/magOff/detectorAligned/dstSet_00000/dNdeta-sim-HIJING-000-" +
+                     std::string(TString::Format("%05d", process).Data()) + ".root";
+        // infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/simulation/ana.376/HIJING/fullSim/magOff/detectorAligned/dstSet_00000/dNdeta-sim-HIJING-000-" +
+        //          std::string(TString::Format("%04d", process).Data()) + ".root";
+        // infile = "/direct/sphenix+tg+tg01/bulk/dNdeta_INTT_run2023/dNdeta_sPHENIX_simulations/macro/dstSet_00000/HIJING_sim_no_beam_angle-000-00000.root";
         else if (generator == "EPOS")
-            infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/simulation/ana.399/EPOS/fullSim/magOff/detectorAligned/dstSet_00000/dNdeta-sim-EPOS-000-" + std::string(TString::Format("%05d", process).Data()) + ".root";
+            infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/simulation/ana.399/EPOS/fullSim/magOff/detectorAligned/dstSet_00000/dNdeta-sim-EPOS-000-" +
+                     std::string(TString::Format("%05d", process).Data()) + ".root";
         else if (generator == "AMPT")
-            infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/simulation/new/AMPT/fullSim/magOff/detectorAligned/dstSet_00000/dNdeta-sim-AMPT-000-" + std::string(TString::Format("%05d", process).Data()) + ".root";
+            infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/simulation/new/AMPT/fullSim/magOff/detectorAligned/dstSet_00000/dNdeta-sim-AMPT-000-" +
+                     std::string(TString::Format("%05d", process).Data()) + ".root";
         else
         {
             std::cout << "Generator " << generator << " is not [HIJING, EPOS, AMPT]. Exit" << std::endl;
@@ -69,9 +100,7 @@ int Fun4All_G4_sPHENIX(const bool rundata = true, const string generator = "HIJI
     //     INPUTREADHITS::listfile[i] = filelist[i];
     // }
 
-    // register all input generators with Fun4All
-    // InputRegister();
-
+    // Enable subsystems
     Enable::MBD = true;
     Enable::PIPE = true;
     Enable::MVTX = true;
@@ -79,46 +108,42 @@ int Fun4All_G4_sPHENIX(const bool rundata = true, const string generator = "HIJI
     Enable::TPC = true;
     Enable::MICROMEGAS = true;
 
-    //===============
-    // conditions DB flags
-    //===============
-    Enable::CDB = true;
-    Enable::VERBOSITY = 2;
-    // global tag
-    rc->set_StringFlag("CDB_GLOBALTAG", CDB::global_tag);
-    // 64 bit timestamp
-    rc->set_uint64Flag("TIMESTAMP", CDB::timestamp);
-
+    // Set up GEANT
     G4Init();
     G4Setup();
 
-    // TrkrHit reconstructions
-    // Mvtx_Cells();
     // Load ActsGeometry object
     TrackingInit();
+
     // Reco clustering
-    // Mvtx_Clustering();
     if (rundata)
         Intt_Clustering();
 
-    //-----------------
-    // Centrality Determination
-    //-----------------
-    // Centrality();
     PHG4CentralityReco *cent = new PHG4CentralityReco();
     cent->Verbosity(0);
     cent->GetCalibrationParameters().ReadFromFile("centrality", "xml", 0, 0, string(getenv("CALIBRATIONROOT")) + string("/Centrality/"));
     se->registerSubsystem(cent);
+
+    // CentralityReco *cr = new CentralityReco();
+    // cr->Verbosity(1);
+    // se->registerSubsystem(cr);
+
+    // MinimumBiasClassifier *mb = new MinimumBiasClassifier();
+    // mb->Verbosity(0);
+    // se->registerSubsystem(mb);
 
     auto vtxing = new PHTruthVertexing;
     vtxing->associate_tracks(false); // This is set to false because we do not run tracking
     se->registerSubsystem(vtxing);
 
     dNdEtaINTT *myAnalyzer = new dNdEtaINTT("dNdEtaAnalyzer", outputFile, rundata);
-    myAnalyzer->GetTruthPV(true);
+    myAnalyzer->GetHEPMC(true);
+    myAnalyzer->GetTruthCluster(true);
     myAnalyzer->GetRecoCluster(true);
     myAnalyzer->GetCentrality(true);
+    myAnalyzer->GetInttRawHit(true);
     myAnalyzer->GetTrkrHit(true);
+    myAnalyzer->GetPHG4(true);
     se->registerSubsystem(myAnalyzer);
 
     //--------------
