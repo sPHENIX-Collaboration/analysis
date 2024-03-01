c048fc86bd8793388240b7246f9e08d77431c021
cdean-github <cameron.dean@cern.ch>
2024-03-01 18:23:33 -0500

    Updated dN/deta nTuplizer for centrality info

--- dNdEta_Run2023/dNdEtaINTT/dNdEtaINTT.h
+++ dNdEta_Run2023/dNdEtaINTT/dNdEtaINTT.h
@@ -20,11 +20,10 @@
 #include <g4detectors/PHG4CylinderGeomContainer.h>
 #include <intt/CylinderGeomIntt.h>
 
-#include <centrality/CentralityInfo.h>
 #include <phool/getClass.h>
 #include <trackbase/ActsGeometry.h>
-#include <trackbase/MvtxDefs.h>
 #include <trackbase/InttDefs.h>
+#include <trackbase/MvtxDefs.h>
 #include <trackbase/TrkrCluster.h>
 #include <trackbase/TrkrClusterContainerv4.h>
 #include <trackbase/TrkrClusterHitAssoc.h>
@@ -39,10 +38,18 @@
 // #include <trackbase_historic/SvtxVertexMap.h>
 // #include <centrality/CentralityInfov1.h>
 
+#include <calotrigger/MinimumBiasInfo.h>
+#include <centrality/CentralityInfo.h>
+#include <globalvertex/GlobalVertex.h>
+#include <globalvertex/GlobalVertexMap.h>
+#include <globalvertex/MbdVertex.h>
+#include <globalvertex/MbdVertexMap.h>
+#include <mbd/MbdOut.h>
+
+#include <math.h>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <math.h>
 #include <string>
 
 #include <TFile.h>
@@ -56,119 +63,135 @@ class SvtxTrackMap;
 class TrkrHitSetContainer;
 class TrkrClusterContainer;
 class CentralityInfo;
+class MinimumBiasInfo;
+class MbdOut;
 
 class dNdEtaINTT : public SubsysReco
 {
-  public:
-    dNdEtaINTT(const std::string &name = "dNdEtaINTTAnalyzer", const std::string &outputfile = "INTTdNdEta.root", const bool &isData = false);
+ public:
+  dNdEtaINTT(const std::string &name = "dNdEtaINTTAnalyzer", const std::string &outputfile = "INTTdNdEta.root", const bool &isData = false);
 
-    ~dNdEtaINTT() override;
+  ~dNdEtaINTT() override;
 
-    /** Called during initialization.
-        Typically this is where you can book histograms, and e.g.
-        register them to Fun4AllServer (so they can be output to file
-        using Fun4AllServer::dumpHistos() method).
-     */
-    int Init(PHCompositeNode *topNode) override;
+  /** Called during initialization.
+      Typically this is where you can book histograms, and e.g.
+      register them to Fun4AllServer (so they can be output to file
+      using Fun4AllServer::dumpHistos() method).
+   */
+  int Init(PHCompositeNode *topNode) override;
 
-    /** Called for first event when run number is known.
-        Typically this is where you may want to fetch data from
-        database, because you know the run number. A place
-        to book histograms which have to know the run number.
-     */
-    int InitRun(PHCompositeNode *topNode) override;
+  /** Called for first event when run number is known.
+      Typically this is where you may want to fetch data from
+      database, because you know the run number. A place
+      to book histograms which have to know the run number.
+   */
+  int InitRun(PHCompositeNode *topNode) override;
 
-    /** Called for each event.
-        This is where you do the real work.
-     */
-    int process_event(PHCompositeNode *topNode) override;
+  /** Called for each event.
+      This is where you do the real work.
+   */
+  int process_event(PHCompositeNode *topNode) override;
 
-    /// Clean up internals after each event.
-    int ResetEvent(PHCompositeNode *topNode) override;
+  /// Clean up internals after each event.
+  int ResetEvent(PHCompositeNode *topNode) override;
 
-    /// Called at the end of each run.
-    int EndRun(const int runnumber) override;
+  /// Called at the end of each run.
+  int EndRun(const int runnumber) override;
 
-    /// Called at the end of all processing.
-    int End(PHCompositeNode *topNode) override;
+  /// Called at the end of all processing.
+  int End(PHCompositeNode *topNode) override;
 
-    /// Reset
-    int Reset(PHCompositeNode * /*topNode*/) override;
+  /// Reset
+  int Reset(PHCompositeNode * /*topNode*/) override;
 
-    void Print(const std::string &what = "ALL") const override;
+  void Print(const std::string &what = "ALL") const override;
 
-    void GetTruthPV(bool b) { _get_truth_pv = b; }
-    void GetRecoCluster(bool b) { _get_reco_cluster = b; }
-    void GetCentrality(bool b) { _get_centrality = b; }
-    void GetTrkrHit(bool b) { _get_trkr_hit = b; }
+  void GetTruthPV(bool b) { _get_truth_pv = b; }
+  void GetRecoCluster(bool b) { _get_reco_cluster = b; }
+  void GetINTTdata(bool b) { _get_intt_data = b; }
+  void GetCentrality(bool b) { _get_centrality = b; }
+  void GetTrkrHit(bool b) { _get_trkr_hit = b; }
 
-  private:
-    void ResetVectors();
-    void GetTruthPVInfo(PHCompositeNode *topNode);
-    void GetRecoClusterInfo(PHCompositeNode *topNode);
-    void GetTruthClusterInfo(PHCompositeNode *topNode);
-    void GetCentralityInfo(PHCompositeNode *topNode);
-    void GetTrkrHitInfo(PHCompositeNode *topNode);
-    PHG4Particle *GetG4PAncestor(PHG4Particle *p);
+ private:
+  void ResetVectors();
+  void GetTruthPVInfo(PHCompositeNode *topNode);
+  void GetRecoClusterInfo(PHCompositeNode *topNode);
+  void GetTruthClusterInfo(PHCompositeNode *topNode);
+  void GetCentralityInfo(PHCompositeNode *topNode);
+  void GetTrkrHitInfo(PHCompositeNode *topNode);
+  PHG4Particle *GetG4PAncestor(PHG4Particle *p);
 
-    bool _get_truth_pv;
-    bool _get_reco_cluster;
-    bool _get_centrality;
-    bool _get_trkr_hit;
+  bool _get_truth_pv;
+  bool _get_reco_cluster;
+  bool _get_centrality;
+  bool _get_intt_data;
+  bool _get_trkr_hit;
 
-    unsigned int eventNum = 0;
-    std::string _outputFile;
-    bool IsData;
-    // int InputFileListIndex;
-    // int NEvtPerFile;
+  unsigned int eventNum = 0;
+  std::string _outputFile;
+  bool IsData;
+  // int InputFileListIndex;
+  // int NEvtPerFile;
 
-    TTree *outtree;
-    int event_;
-    float centrality_bimp_, centrality_impactparam_, centrality_mbd_, centrality_mbdquantity_;
-    int ncoll_, npart_; // number of collisions and participants
-    // Truth primary vertex information
-    float TruthPV_trig_x_, TruthPV_trig_y_, TruthPV_trig_z_;
-    int NTruthVtx_;
-    // Reconstructed cluster information
-    int NClus_, NClus_Layer1_;
-    std::vector<int> ClusLayer_, ClusHitcount_, ClusTimeBucketId_;
-    std::vector<float> ClusX_, ClusY_, ClusZ_, ClusR_, ClusPhi_, ClusEta_;
-    std::vector<unsigned int> ClusAdc_;
-    std::vector<float> ClusPhiSize_, ClusZSize_;
-    std::vector<uint8_t> ClusLadderZId_, ClusLadderPhiId_;
-    std::vector<uint32_t> ClusTrkrHitSetKey_;
-    std::vector<int> ClusTruthCKeys_;
-    std::vector<int> TruthClusPhiSize_;
-    std::vector<int> TruthClusZSize_;
-    std::vector<int> PrimaryTruthClusPhiSize_;
-    std::vector<int> PrimaryTruthClusZSize_;
-    std::vector<int> ClusNG4Particles_;
-    std::vector<int> ClusNPrimaryG4Particles_;
-    std::vector<int> TruthClusNRecoClus_;
-    std::vector<int> PrimaryTruthClusNRecoClus_;
-    // TrkrHit information
-    int NTrkrhits_;
-    std::vector<uint16_t> TrkrHitRow_, TrkrHitColumn_, TrkrHitADC_;
-    std::vector<uint8_t> TrkrHitLadderZId_, TrkrHitLadderPhiId_, TrkrHitLayer_;
-    // G4 information Matching for simulation
-    int NGenPart_, NTruthLayers_;
-    std::vector<float> UniqueAncG4P_Pt_, UniqueAncG4P_Eta_, UniqueAncG4P_Phi_, UniqueAncG4P_E_;
-    std::vector<int> UniqueAncG4P_PID_;
+  TTree *outtree;
+  int event_;
+  // Centrality and MBD stuff
+  float centrality_bimp_, centrality_impactparam_, centrality_mbd_, centrality_mbdquantity_;
+  UShort_t femclk;
+  float mbd_south_charge_sum, mbd_north_charge_sum, mbd_charge_sum, mbd_charge_asymm;
+  float mbd_z_vtx;
+  // std::set<std::pair<int, float>> m_pmt_q;
+  bool is_min_bias;
+  int ncoll_, npart_;  // number of collisions and participants
+  // Truth primary vertex information
+  float TruthPV_trig_x_, TruthPV_trig_y_, TruthPV_trig_z_;
+  int NTruthVtx_;
+  // Reconstructed cluster information
+  int NClus_, NClus_Layer1_;
+  std::vector<int> ClusLayer_, ClusHitcount_, ClusTimeBucketId_;
+  std::vector<float> ClusX_, ClusY_, ClusZ_, ClusR_, ClusPhi_, ClusEta_;
+  std::vector<unsigned int> ClusAdc_;
+  std::vector<float> ClusPhiSize_, ClusZSize_;
+  std::vector<uint8_t> ClusLadderZId_, ClusLadderPhiId_;
+  std::vector<uint32_t> ClusTrkrHitSetKey_;
+  std::vector<int> ClusTruthCKeys_;
+  std::vector<int> TruthClusPhiSize_;
+  std::vector<int> TruthClusZSize_;
+  std::vector<int> PrimaryTruthClusPhiSize_;
+  std::vector<int> PrimaryTruthClusZSize_;
+  std::vector<int> ClusNG4Particles_;
+  std::vector<int> ClusNPrimaryG4Particles_;
+  std::vector<int> TruthClusNRecoClus_;
+  std::vector<int> PrimaryTruthClusNRecoClus_;
+  // TrkrHit information
+  int NTrkrhits_;
+  std::vector<uint16_t> TrkrHitRow_, TrkrHitColumn_, TrkrHitADC_;
+  std::vector<uint8_t> TrkrHitLadderZId_, TrkrHitLadderPhiId_, TrkrHitLayer_;
+  // G4 information Matching for simulation
+  int NGenPart_, NTruthLayers_;
+  std::vector<float> UniqueAncG4P_Pt_, UniqueAncG4P_Eta_, UniqueAncG4P_Phi_, UniqueAncG4P_E_;
+  std::vector<int> UniqueAncG4P_PID_;
 
-    EventHeader *eventheader = nullptr;
+  EventHeader *eventheader = nullptr;
 
-    SvtxEvalStack *svtx_evalstack = nullptr;
-    SvtxTruthEval *truth_eval = nullptr;
-    SvtxClusterEval *clustereval = nullptr;
-    SvtxHitEval *hiteval = nullptr;
+  SvtxEvalStack *svtx_evalstack = nullptr;
+  SvtxTruthEval *truth_eval = nullptr;
+  SvtxClusterEval *clustereval = nullptr;
+  SvtxHitEval *hiteval = nullptr;
 
-    TrkrClusterContainerv4 *dst_clustermap = nullptr;
-    TrkrClusterHitAssoc *clusterhitmap = nullptr;
-    TrkrHitSetContainer *hitsets = nullptr;
-    ActsGeometry *_tgeometry = nullptr;
-    PHG4CylinderGeomContainer *_intt_geom_container = nullptr;
-    PHG4TruthInfoContainer *m_truth_info = nullptr;
-    CentralityInfo *m_CentInfo = nullptr;
+  TrkrClusterContainerv4 *dst_clustermap = nullptr;
+  TrkrClusterHitAssoc *clusterhitmap = nullptr;
+  TrkrHitSetContainer *hitsets = nullptr;
+  ActsGeometry *_tgeometry = nullptr;
+  PHG4CylinderGeomContainer *_intt_geom_container = nullptr;
+  PHG4TruthInfoContainer *m_truth_info = nullptr;
+  CentralityInfo *m_CentInfo = nullptr;
+  MinimumBiasInfo *_minimumbiasinfo = nullptr;
+  MbdOut *m_mbdout = nullptr;
+  GlobalVertexMap *m_glbvtxmap = nullptr;
+  GlobalVertex *m_glbvtx = nullptr;
+  //MbdVertexMap *m_mbdvtxmap = nullptr;
+  //MbdVertex *m_mbdvtx = nullptr;
 };
 
-#endif // DNDETAINTT_H
+#endif  // DNDETAINTT_H
--- dNdEta_Run2023/macros/Fun4All_G4_sPHENIX.C
+++ dNdEta_Run2023/macros/Fun4All_G4_sPHENIX.C
@@ -1,6 +1,8 @@
 #ifndef MACRO_FUN4ALLG4SPHENIX_C
 #define MACRO_FUN4ALLG4SPHENIX_C
 
+#include <string>
+
 #include <GlobalVariables.C>
 
 #include <G4Setup_sPHENIX.C>
@@ -16,6 +18,9 @@
 #include <g4centrality/PHG4CentralityReco.h>
 #include <trackreco/PHTruthVertexing.h>
 
+#include <centrality/CentralityReco.h>
+#include <calotrigger/MinimumBiasClassifier.h>
+
 #include <dndetaintt/dNdEtaINTT.h>
 
 #include <phool/recoConsts.h>
@@ -25,26 +30,45 @@ R__LOAD_LIBRARY(libffamodules.so)
 R__LOAD_LIBRARY(libdNdEtaINTT.so)
 R__LOAD_LIBRARY(libcentrality_io.so)
 R__LOAD_LIBRARY(libg4centrality.so)
+R__LOAD_LIBRARY(libcentrality.so)
+R__LOAD_LIBRARY(libcalotrigger.so)
 
 int Fun4All_G4_sPHENIX(const bool rundata = true, const string generator = "HIJING", const int nEvents = -1, const string &outputFile = "dataNtuple.root", const int process = 0)
 {
+    bool getINTTData = true;
+    bool getCentralityData = false;
+    if (rundata && getINTTData && getCentralityData)
+    {
+      std::cout << "We currently can't get INTT and Centrality info from the same file for real data, exiting!" << std::endl;
+      exit (1);
+    }
+
+    const int runNumber = 20869;
+    std::string productionTag = "2023p011";//"ProdA_2023";
+
     int skip;
     if (rundata)
-        skip = nEvents * process;
+    {
+      skip = nEvents * process;
+    }
     else
-        skip = 0;
+    {
+      skip = 0;
+    }
+
     Fun4AllServer *se = Fun4AllServer::instance();
-    se->Verbosity(1);
+    se->Verbosity(0);
 
     recoConsts *rc = recoConsts::instance();
 
-    Input::VERBOSITY = INT_MAX;
+    Input::VERBOSITY = 0;
     Input::READHITS = true;
 
     string infile;
-    if (rundata)
+    if (rundata && getINTTData)
     {
-        infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/data/run_00020869/ana.382/beam_intt_combined-dst-00020869-0000.root";
+        infile = "/gpfs/mnt/gpfs02/sphenix/user/cdean/software/macros/InttProduction/intt-00020869.root";
+        //infile = "/sphenix/tg/tg01/bulk/dNdeta_INTT_run2023/data/data/run_00020869/ana.382/beam_intt_combined-dst-00020869-0000.root";
     }
     else
     {
@@ -61,7 +85,12 @@ int Fun4All_G4_sPHENIX(const bool rundata = true, const string generator = "HIJI
         }
     }
 
-    INPUTREADHITS::filename[0] = infile;
+    //Generate MBD data list with `CreateDstList.pl --run 20869 --build ana403 --cdb 2023p011 DST_CALO`
+    if (rundata && getCentralityData) 
+    {
+      INPUTREADHITS::listfile[0] = "dst_calo-000" + std::to_string(runNumber) + ".list";
+    }
+    else INPUTREADHITS::filename[0] = infile;
     // const vector<string> &filelist = {"/sphenix/user/hjheng/sPHENIXdNdEta/macros/list/dNdEta_INTT/dst_INTTdNdEta_data.list"};
 
     // for (unsigned int i = 0; i < filelist.size(); ++i)
@@ -72,53 +101,79 @@ int Fun4All_G4_sPHENIX(const bool rundata = true, const string generator = "HIJI
     // register all input generators with Fun4All
     // InputRegister();
 
-    Enable::MBD = true;
-    Enable::PIPE = true;
-    Enable::MVTX = true;
-    Enable::INTT = true;
-    Enable::TPC = true;
-    Enable::MICROMEGAS = true;
+    Enable::MBD = getINTTData;
+    Enable::PIPE = getINTTData;
+    Enable::MVTX = getINTTData;
+    Enable::INTT = getINTTData;
+    Enable::TPC = getINTTData;
+    Enable::MICROMEGAS = getINTTData;
 
     //===============
     // conditions DB flags
     //===============
     Enable::CDB = true;
-    Enable::VERBOSITY = 2;
-    // global tag
-    rc->set_StringFlag("CDB_GLOBALTAG", CDB::global_tag);
-    // 64 bit timestamp
-    rc->set_uint64Flag("TIMESTAMP", CDB::timestamp);
+    Enable::VERBOSITY = 0;
+    if (!getCentralityData)
+    {
+      // global tag
+      rc->set_StringFlag("CDB_GLOBALTAG", CDB::global_tag);
+      // 64 bit timestamp
+      rc->set_uint64Flag("TIMESTAMP", CDB::timestamp);
+    }
+    else
+    {
+      rc->set_StringFlag("CDB_GLOBALTAG",productionTag);
+      // 64 bit timestamp                                                                                                                                
+      rc->set_uint64Flag("TIMESTAMP",runNumber);
+    }
 
-    G4Init();
-    G4Setup();
+    if (getINTTData)
+    {
+      G4Init();
+      G4Setup();
 
     // TrkrHit reconstructions
     // Mvtx_Cells();
     // Load ActsGeometry object
-    TrackingInit();
-    // Reco clustering
-    // Mvtx_Clustering();
-    if (rundata)
-        Intt_Clustering();
+      TrackingInit();
+      // Reco clustering
+      // Mvtx_Clustering();
+      if (rundata) Intt_Clustering();
+    }
 
     //-----------------
     // Centrality Determination
     //-----------------
     // Centrality();
-    PHG4CentralityReco *cent = new PHG4CentralityReco();
-    cent->Verbosity(0);
-    cent->GetCalibrationParameters().ReadFromFile("centrality", "xml", 0, 0, string(getenv("CALIBRATIONROOT")) + string("/Centrality/"));
-    se->registerSubsystem(cent);
+    if (!rundata)
+    {
+      PHG4CentralityReco *cent = new PHG4CentralityReco();
+      cent->Verbosity(0);
+      cent->GetCalibrationParameters().ReadFromFile("centrality", "xml", 0, 0, string(getenv("CALIBRATIONROOT")) + string("/Centrality/"));
+      se->registerSubsystem(cent);
 
-    auto vtxing = new PHTruthVertexing;
-    vtxing->associate_tracks(false); // This is set to false because we do not run tracking
-    se->registerSubsystem(vtxing);
+      auto vtxing = new PHTruthVertexing;
+      vtxing->associate_tracks(false); // This is set to false because we do not run tracking
+      se->registerSubsystem(vtxing);    
+    }
+
+    if (getCentralityData)
+    {
+      CentralityReco *cr = new CentralityReco();
+      cr->Verbosity(0);
+      se->registerSubsystem(cr);
+      
+      MinimumBiasClassifier *mb = new MinimumBiasClassifier();
+      mb->Verbosity(0);
+      se->registerSubsystem(mb);
+    }
 
     dNdEtaINTT *myAnalyzer = new dNdEtaINTT("dNdEtaAnalyzer", outputFile, rundata);
-    myAnalyzer->GetTruthPV(true);
-    myAnalyzer->GetRecoCluster(true);
-    myAnalyzer->GetCentrality(true);
-    myAnalyzer->GetTrkrHit(true);
+    myAnalyzer->GetTruthPV(!rundata);
+    myAnalyzer->GetRecoCluster(getINTTData);
+    myAnalyzer->GetTrkrHit(getINTTData);
+    myAnalyzer->GetINTTdata(getINTTData);
+    myAnalyzer->GetCentrality(getCentralityData);
     se->registerSubsystem(myAnalyzer);
 
     //--------------
