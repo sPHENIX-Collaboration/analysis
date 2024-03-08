4c221d1a477cc2f4dfda02647eaf7d17b3702fe0
Hao-Ren Jheng <hrjheng@mit.edu>
2024-03-08 11:49:55 -0500

    re-organize

--- dNdEta_Run2023/production/config_combinechain.py
+++ dNdEta_Run2023/production/config_combinechain.py
@@ -6,7 +6,7 @@ InttNtupleDir = 'Data_NtupleIntt_Run{}'.format(runnumber)
 CentralityNtupleDir = 'Data_NtupleCentrality_Run{}'.format(runnumber)
 softwareversion = 'ana.405'
 submitcondor_InttNtupleProduction = False
-submitcondor_CentralityNtupleProduction = True
+submitcondor_CentralityNtupleProduction = False
 productionTag = '2023p011'
 combinedNtupleName = 'Data_CombinedNtuple_Run{}.root'.format(runnumber)
 
