f7ae1b8414cfb04389b76bee0d0f42775cc20549
Apurva Narde <apurva.narde@rutgers.edu>
2024-03-27 17:42:14 -0500

    Update genCuts.py
    - Added new cuts to analyze
    - deltaR max and asymmetric energy cuts
    - added header to specify columns of csv

--- pi0-vn/emcalTreeMaker/genCuts.py
+++ pi0-vn/emcalTreeMaker/genCuts.py
@@ -16,19 +16,26 @@ if __name__ == '__main__':
     output = os.path.realpath(args.output)
     print(f'Output: {output}')
 
-    e      = getRange(0.5, 1, 0.25)
-    chi    = [4]
-    deltaR = getRange(0, 0.1, 0.02)
-    asym   = getRange(0.5, 0.8, 0.3)
+    e          = [1]
+    e2         = [1, 1.25]
+    asym       = [0.3, 0.4, 0.5, 0.8]
+    deltaR     = [0]
+    deltaR_max = [0.5, 1, 2, 3]
+    chi        = [4]
 
-    print(f'Cluster E:           {e}')
-    print(f'Cluster chi:         {chi}')
-    print(f'Cluster pair deltaR: {deltaR}')
-    print(f'Cluster pair asym:   {asym}')
+    print(f'Cluster E:               {e}')
+    print(f'Cluster E2:              {e2}')
+    print(f'Cluster pair asym:       {asym}')
+    print(f'Cluster pair min deltaR: {deltaR}')
+    print(f'Cluster pair max deltaR: {deltaR_max}')
+    print(f'Max Cluster chi:         {chi}')
 
     with open(f'{output}', mode='w') as file:
-        for i in e:
-            for j in asym:
-                for k in deltaR:
-                    for l in chi:
-                       file.write(f'{i}, {j}, {k}, {l}\n')
+        file.write(f'E1_min,E2_min,asym_max,deltaR_min,deltaR_max,chi_max\n')
+        for i1 in e:
+            for i2 in e2:
+                for i3 in asym:
+                    for i4 in deltaR:
+                        for i5 in deltaR_max:
+                            for i6 in chi:
+                                file.write(f'{i1},{i2},{i3},{i4},{i5},{i6}\n')
