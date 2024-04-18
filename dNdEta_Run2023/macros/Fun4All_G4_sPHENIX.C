e8c4d0f6ac9d4dc0746ab3cc9f385e1ab52c0cdb
Hao-Ren Jheng <hrjheng@mit.edu>
2024-04-18 17:24:13 -0400

    update

--- dNdEta_Run2023/macros/Fun4All_G4_sPHENIX.C
+++ dNdEta_Run2023/macros/Fun4All_G4_sPHENIX.C
@@ -93,7 +93,7 @@ int Fun4All_G4_sPHENIX(                           //
             if (file == NULL)
             {
                 std::cout << "File: " << infile << " does not exist. Use the pre-generated file" << std::endl;
-                infile = "/gpfs/mnt/gpfs02/sphenix/user/cdean/software/macros/InttProduction/intt-" + std::string(TString::Format("%08d", runnumber).Data()) + ".root";
+                infile = "/sphenix/user/hjheng/sPHENIXRepo/macros/InttProduction/ProdDST-HotDead-ADC-Survey/intt-" + std::string(TString::Format("%08d", runnumber).Data()) + ".root";
             }
             else
             {
@@ -102,9 +102,9 @@ int Fun4All_G4_sPHENIX(                           //
 
             INPUTREADHITS::filename[0] = infile;
         }
-        else if (getCentralityData) // Generate MBD data list with `CreateDstList.pl --run 20869 --build ana403 --cdb 2023p011 DST_CALO`
+        else if (getCentralityData) // Generate MBD data list with `CreateDstList.pl --run 20869 --build ana403 --cdb 2023p011 DST_CALO_run1auau`
         {
-            INPUTREADHITS::listfile[0] = "dst_calo-000" + std::to_string(runnumber) + ".list";
+            INPUTREADHITS::listfile[0] = "dst_calo_run1auau-000" + std::to_string(runnumber) + ".list";
         }
     }
     else
@@ -177,8 +177,8 @@ int Fun4All_G4_sPHENIX(                           //
         // Load ActsGeometry object
         TrackingInit();
         // Reco clustering
-        if (rundata)
-            Intt_Clustering();
+        // if (rundata)
+            // Intt_Clustering();
     }
 
     //-----------------
@@ -209,16 +209,15 @@ int Fun4All_G4_sPHENIX(                           //
     }
 
     dNdEtaINTT *myAnalyzer = new dNdEtaINTT("dNdEtaAnalyzer", outputFile, rundata);
-    myAnalyzer->GetHEPMC(true);
-    myAnalyzer->GetRecoCluster(true);
-    myAnalyzer->GetCentrality(true);
-    myAnalyzer->GetInttRawHit(false);
-    myAnalyzer->GetTrkrHit(false);
     myAnalyzer->GetINTTdata(getINTTData);
+    myAnalyzer->GetRecoCluster(getINTTData);
+    myAnalyzer->GetInttRawHit(false);
+    myAnalyzer->GetTrkrHit(getINTTData);
     myAnalyzer->GetCentrality(getCentralityData);
     bool getPMTinfo = getCentralityData && false;
     myAnalyzer->GetPMTInfo(getPMTinfo);
-    myAnalyzer->GetPHG4(true);
+    myAnalyzer->GetPHG4(!rundata);
+    myAnalyzer->GetHEPMC(!rundata);
 
     se->registerSubsystem(myAnalyzer);
 
