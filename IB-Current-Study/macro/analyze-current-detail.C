20059c917d3f62429fa83d808c3403eebe9cdc4e
Apurva Narde <58493193+Steepspace@users.noreply.github.com>
2024-06-24 10:42:57 -0400

    Update analyze-current-detail macro
    - Changed the x-axis to be in units of phi and taking care of mapping accordingly.
    - Analyzing a fixed number of samples starting with the latest and working downwards.
    - Plotting the overlay of currents along phi of ib 4 for both north and south.

--- IB-Current-Study/macro/analyze-current-detail.C
+++ IB-Current-Study/macro/analyze-current-detail.C
@@ -39,38 +39,57 @@ namespace myAnalysis {
 
     void analyze(const string &i_input, const string &i_output, const string& outputPDF);
 
+    // there are 6 interface boards for north and south (total 12 along eta)
     UInt_t  bins_eta = 12;
     Float_t eta_low  = -6;
     Float_t eta_high = 6;
 
+    // there are 32 sectors in phi
     UInt_t  bins_phi = 32;
     Float_t phi_low  = 0;
-    Float_t phi_high = 32;
+    Float_t phi_high = 2*M_PI;
 
     UInt_t  bins_current = 100;
     Float_t current_low  = 1e3;
     Float_t current_high = 3.5e3;
+
+    // tune the number of samples to process
+    Int_t process_samples = 60;
 }
 
 void myAnalysis::analyze(const string &i_input, const string &i_output, const string &outputPDF) {
     TFile input(i_input.c_str());
 
-    auto h2 = (TH2F*)input.Get("h2Current_0");
 
-    vector<TH1F*> hCurrent;
-    vector<string> names;
+    vector<TH1F*> hCurrentNorth;
+    vector<TH1F*> hCurrentSouth;
 
-    for(UInt_t ieta = 1; ieta <= bins_eta; ++ieta) {
+    for(UInt_t i = 0; i < process_samples; ++i) {
 
-        string name  = "hCurrent_"+to_string(ieta-1);
-        string title = string(h2->GetTitle()) + ", IB #eta Index: " + to_string((Int_t)(eta_low+ieta-1)) + "; Current [#mu A]; Counts";
+        string name  = "h2Current_"+to_string(i);
+        string name1 = "hNorth_"+to_string(i);
+        string name2 = "hSouth_"+to_string(i);
 
-        auto h1 = new TH1F(name.c_str(), title.c_str(), bins_current, current_low, current_high);
+        auto h2 = (TH2F*)input.Get(name.c_str());
 
-        for(UInt_t iphi = 1; iphi <= bins_phi; ++iphi) h1->Fill(h2->GetBinContent(iphi,ieta));
+        string title = string(h2->GetTitle()) + ", North IB: 4; #phi; Current [#mu A]";
 
-        hCurrent.push_back(h1);
-        names.push_back(name);
+        auto hNorth = new TH1F(name1.c_str(), title.c_str(), bins_phi, phi_low, phi_high);
+
+        title = string(h2->GetTitle()) + ", South IB: 4; #phi; Current [#mu A]";
+
+        auto hSouth = new TH1F(name2.c_str(), title.c_str(), bins_phi, phi_low, phi_high);
+
+        for(UInt_t iphi = 1; iphi <= bins_phi; ++iphi) {
+            Float_t val = h2->GetBinContent(iphi,bins_eta-1);
+            if(val) hNorth->SetBinContent(iphi, val);
+
+            val = h2->GetBinContent(iphi,2);
+            if(val) hSouth->SetBinContent(iphi, val);
+        }
+
+        hCurrentNorth.push_back(hNorth);
+        hCurrentSouth.push_back(hSouth);
     }
 
     TFile output(i_output.c_str(), "recreate");
@@ -82,32 +101,44 @@ void myAnalysis::analyze(const string &i_input, const string &i_output, const st
     c1->SetTicky();
 
     c1->SetCanvasSize(1200, 1000);
-    c1->SetLeftMargin(.08);
+    c1->SetLeftMargin(.13);
     c1->SetRightMargin(.05);
 
-    c1->Print((outputPDF + "[").c_str(), "pdf portrait");
+    c1->cd();
 
-    cout << "Samples: " << hCurrent.size() << endl;
-    for(UInt_t i = 0; i < hCurrent.size(); ++i) {
+    hCurrentNorth[0]->SetStats(0);
+    hCurrentNorth[0]->SetLineColor(kRed);
+    // hCurrentNorth[0]->SetLineWidth(1);
+    hCurrentNorth[0]->GetYaxis()->SetRangeUser(0, 3.5e3);
+    hCurrentNorth[0]->SetTitle("Thu Jun 13 2024 09:16:56 to 10:00:49");
+    hCurrentNorth[0]->Draw();
 
-        hCurrent[i]->Write();
+    hCurrentSouth[0]->SetLineColor(kBlue);
+    // hCurrentSouth[0]->SetLineWidth(1);
+    hCurrentSouth[0]->Draw("same");
 
-        c1->cd();
+    TLegend *leg = new TLegend(0.7,0.7,0.95,0.9);
+    leg->AddEntry(hCurrentNorth[0], "North IB 4");
+    leg->AddEntry(hCurrentSouth[0], "South IB 4");
+    // leg->SetBorderSize(0);
+    leg->Draw("same");
 
-        hCurrent[i]->Draw();
-        gPad->Update();
+    for(UInt_t i = 0; i < hCurrentNorth.size(); ++i) {
+        if(i == 14 || (i >= 22 && i <= 25)) continue;
 
-        auto st = (TPaveStats*)hCurrent[i]->GetListOfFunctions()->FindObject("stats");
-        st->SetX1NDC(0.8);
-        st->SetX2NDC(0.95);
-        st->SetY1NDC(0.7);
-        st->SetY2NDC(0.9);
+        hCurrentNorth[i]->Write();
+        hCurrentSouth[i]->Write();
 
-        c1->Print((outputPDF).c_str(), "pdf portrait");
-        c1->Print((names[i] + ".png").c_str());
+        hCurrentNorth[i]->SetLineColor(kRed);
+        // hCurrentNorth[i]->SetLineWidth(1);
+        hCurrentNorth[i]->Draw("same");
+
+        hCurrentSouth[i]->SetLineColor(kBlue);
+        // hCurrentSouth[i]->SetLineWidth(1);
+        hCurrentSouth[i]->Draw("same");
     }
 
-    c1->Print((outputPDF + "]").c_str(), "pdf portrait");
+    c1->Print("hNorthSouth.png");
 
     output.Close();
     input.Close();
