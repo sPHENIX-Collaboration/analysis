05926acc97f4b4b6962ee5423ef563e5b8072b66
Apurva Narde <58493193+Steepspace@users.noreply.github.com>
2024-07-04 10:52:59 -0400

    Added extraction of raw trigger values
    - raw trigger values extracted from gl1_scalers table.

--- IB-Current-Study/scripts/extract.py
+++ IB-Current-Study/scripts/extract.py
@@ -7,39 +7,77 @@ import pandas as pd
 import pandas.io.sql as sqlio
 import argparse
 
-parser = argparse.ArgumentParser(description='Extract the IB Currents from the database.')
+parser = argparse.ArgumentParser(description='Extract data from the DAQ Database.')
 
-parser.add_argument('-n', '--samples', type=int, default=5000000, help='Number of samples to read from the database. Default: 5e6')
-parser.add_argument('-d', '--dbname', type=str, default='daq', help='Database name. Default: daq')
-parser.add_argument('-u', '--user', type=str, default='phnxro', help='User. Default: phnxro')
-parser.add_argument('-s', '--host', type=str, default='db1.sphenix.bnl.gov', help='Database host. Default: db1.sphenix.bnl.gov')
-parser.add_argument('-t', '--threshold', type=float, default=1.5, help='Maximum difference in voltage between vmeas (measured voltage) and vset (set voltage). Default: 1.5 V')
-parser.add_argument('-o', '--output', type=str, default='test.csv', help='Name of output CSV. Default: test.csv')
+subparser = parser.add_subparsers(dest='command')
+
+currents = subparser.add_parser('currents', help='Extract the IB Currents from the database.')
+
+currents.add_argument('-n', '--samples', type=int, default=5000000, help='Number of samples to read from the database. Default: 5e6')
+currents.add_argument('-d', '--dbname', type=str, default='daq', help='Database name. Default: daq')
+currents.add_argument('-u', '--user', type=str, default='phnxro', help='User. Default: phnxro')
+currents.add_argument('-s', '--host', type=str, default='db1.sphenix.bnl.gov', help='Database host. Default: db1.sphenix.bnl.gov')
+currents.add_argument('-t', '--threshold', type=float, default=1.5, help='Maximum difference in voltage between vmeas (measured voltage) and vset (set voltage). Default: 1.5 V')
+currents.add_argument('-o', '--output', type=str, default='currents.csv', help='Name of output CSV. Default: currents.csv')
+
+raw = subparser.add_parser('raw', help='Extract the raw counts from the database.')
+
+raw.add_argument('-n', '--samples', type=int, default=5000, help='Number of samples to read from the database. Default: 1e5')
+raw.add_argument('-t', '--trigger', type=int, default=24, help='Trigger type. Default: 24 (Photon 2 GeV + MBD NS >= 1)')
+raw.add_argument('-d', '--dbname', type=str, default='daq', help='Database name. Default: daq')
+raw.add_argument('-u', '--user', type=str, default='phnxro', help='User. Default: phnxro')
+raw.add_argument('-s', '--host', type=str, default='db1.sphenix.bnl.gov', help='Database host. Default: db1.sphenix.bnl.gov')
+raw.add_argument('-o', '--output', type=str, default='raw.csv', help='Name of output CSV. Default: raw.csv')
 
 args = parser.parse_args()
 
 if __name__ == '__main__':
+    if(args.command == 'currents'):
+        samples   = args.samples
+        threshold = args.threshold
+        dbname    = args.dbname
+        user      = args.user
+        host      = args.host
+        output    = args.output
 
-    samples   = args.samples
-    dbname    = args.dbname
-    user      = args.user
-    host      = args.host
-    threshold = args.threshold
-    output    = args.output
+        print(f'samples: {samples}')
+        print(f'ensure |vmeas - vset| < {threshold}')
+        print(f'dbname: {dbname}')
+        print(f'user: {user}')
+        print(f'host: {host}')
+        print(f'output CSV: {output}')
 
-    print(f'samples: {samples}')
-    print(f'dbname: {dbname}')
-    print(f'user: {user}')
-    print(f'host: {host}')
-    print(f'Ensure |vmeas - vset| < {threshold}')
-    print(f'Output CSV: {output}')
+        with psycopg2.connect(f"host='{host}' dbname='{dbname}' user='{user}'") as conn:
+            sql = f'select readtime, sector, ib, vmeas, vset, imeas from emcal_mpodlog where abs(vmeas-vset) < {threshold} and vmeas != 0 and vset != 0 order by readtime desc limit {samples};'
 
-    with psycopg2.connect(f"host='{host}' dbname='{dbname}' user='{user}'") as conn:
-        sql = f'select readtime, sector, ib, vmeas, vset, imeas from emcal_mpodlog where abs(vmeas-vset) < {threshold} and vmeas != 0 and vset != 0 order by readtime desc limit {samples};'
+            df = pd.read_sql_query(sql, conn)
 
-        df = pd.read_sql_query(sql, conn)
+            df.to_csv(output,index=False)
 
-        df.to_csv(output,index=False)
+            # preview of contents
+            print(df.head(20))
 
-        # preview of contents
-        print(df.head(20))
+    if(args.command == 'raw'):
+        samples   = args.samples
+        trigger   = args.trigger
+        dbname    = args.dbname
+        user      = args.user
+        host      = args.host
+        output    = args.output
+
+        print(f'samples: {samples}')
+        print(f'trigger: {trigger}')
+        print(f'dbname: {dbname}')
+        print(f'user: {user}')
+        print(f'host: {host}')
+        print(f'output CSV: {output}')
+
+        with psycopg2.connect(f"host='{host}' dbname='{dbname}' user='{user}'") as conn:
+            sql = f'select updated as readtime, index as trigger, raw from gl1_scalers where index={trigger} order by updated desc limit {samples};'
+
+            df = pd.read_sql_query(sql, conn)
+
+            df.to_csv(output,index=False)
+
+            # preview of contents
+            print(df.head(20))
