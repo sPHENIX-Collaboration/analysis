91032a9a6478f873289f0ce8bf8bfe987f9fd379
Silas K Grossberndt <silas.grossberndt@gmail.com>
2024-07-18 23:56:24 -0400

    added more of the actual program just need to get the EEC coded in and add the hists

--- EnergyCorrelatorsJets/CalorimeterTowerENC/src/CalorimeterTowerENC.cc
+++ EnergyCorrelatorsJets/CalorimeterTowerENC/src/CalorimeterTowerENC.cc
@@ -25,21 +25,58 @@ int CalorimeterTowerENC::Init(PHCompositeNode *topNode)
 {	
 	//book histograms and TTrees
 }
-float CalorimeterTowerENC::getPt()
+
+float CalorimeterTowerENC::getPt(PHG4Particle* p)
 {
 	float pt=0;
-	pt=pow(p->momentum().px(), 2) + pow(p->momentum().py(), 2);
+	pt=pow(p->get_px(), 2) + pow(p->get_py(), 2);
 	pt=sqrt(pt);
 	return pt;
 }
-float CalorimeterTowerENC::getR(){
-	float eta_dist=p1->momentum().pseudoRapidity() - p2->momentum().pseudoRapidity();
-	float phi_dist=p1->momentum().phi() - p2->momentum().phi();
+
+float CalorimeterTowerENC::getR(std::pair<float, float> p1, std::pair<float,float> p2){
+	float eta_dist=p1.first - p2.first;
+	float phi_dist=p1.second - p2.second;
 	phi_dist=abs(phi_dist);
 	if(phi_dist > PI) phi_dist=2*PI-phi_dist;
 	float R=sqrt(pow(eta_dist,2) + pow(phi_dist, 2));
 	return R;
 }
+void CalorimeterTowerENC::GetE2C(PHCompositeNode* topNode, std::unordered_set<int> em, std::unordered_set<int> ih, std::unordered_set<int> oh, int version)
+{
+	//this is the processor that allows for read out of the tower energies given the set of towers in the phi-eta plane
+	auto emcal_geom =  findNode::getClass<RawTowerGeomContainer_Cylinderv1>(topNode, "TOWERGEOM_CEMC");
+	auto emcal_tower_energy =  findNode::getClass<TowerInfoContainer>(topNode, "TOWERINFO_CALIB_CEMC");
+	auto ihcal_geom= findNode::getClass<RawTowerGeomContainer_Cylinderv1>(topNode, "TOWERGEOM_HCALIN");
+	auto ihcal_tower_energy= findNode::getClass<TowerInfoContainer>(topNode, "TOWERINFO_CALIB_HCALIN");
+	auto ohcal_geom=findNode::getClass<RawTowerGeomContainer_Cylinderv1>(topNode, "TOWERGEOM_HCALOUT");
+	auto ohcal_tower_energy=findNode::getClass<TowerInfoContainer>(topNode, "TOWERINFO_CALIB_HCALOUT");
+	for(auto i:em)
+	{
+		emcal_geom->set_calorimeter_id(RawTowerDefs::CEMC);
+		try{
+			auto tower_e=emcal_tower_energy->get_tower_at_key(i);
+		}
+		catch(std::exception& e){
+			std::cout<<"Could not access the tower of that key" <<std::endl;
+		}
+		
+}
+void CalorimeterTowerENC::GetE3C(PHCompositeNode* topNode, std::unordered_set<int> em, std::unordered_set<int> ih, std::unordered_set<int> oh, int version)
+{
+	//this is the processor that allows for read out of the tower energies given the set of towers in the phi-eta plane
+}
+}
+void CalorimeterTowerENC::GetE2C(PHCompositeNode* topNode, std::map<PHG4Particles*, std::pair<float, float>> jet, int version)
+{
+	//this is the processor that allows for particle read of jets phi-eta plane
+}
+}
+void CalorimeterTowerENC::GetE2C(PHCompositeNode* topNode, std::map<PHG4Particles*, std::pair<float, float>> jet, int version)
+{
+	//this is the processor that allows for particle read of jet in the phi-eta plane
+}
+}	
 std::pair<std::map<float, std::map<float, int>>, std::pair<float, float>> CalorimeterTowerENC::GetTowerMaps(RawTowerGeomContainer_Cylinderv1* geom, RawTowerDefs::CalorimenterID caloid, TowerInfoContainer* calokey)
 {
 	//ge thte geometry map for the towers in the first event to may it easy to serarc
@@ -107,10 +144,11 @@ int CalorimeterTowerENC::GetTowerNumber(std::pair<float, float> part, std::map<f
 	}
 	return -1;
 }
-int CalorimeterTowerENC::RecordHits( Jet* truth_jet){
+int CalorimeterTowerENC::RecordHits(PHCompositeNode* topNode, Jet* truth_jet){
 	//record where the particles are located and which towers are seeing hits
 	std::unordered_set<std::pair<float, float>> particle_coords; //This will store the location of each particle which then I can use tho capture the relevant towe
-	std::unordered_set<PHG4Particle*> jetparticles;
+	std::unordered_map<int, PHG4Particle*> jetparticles;
+	std::unordered_map<int, PHG4Hit*> jethits;
 	for( auto& iter:truth_jet->get_comp_vec()){
 		Jet::SRC source = iter.first;
 		unsigned int idx = iter.second;
@@ -119,17 +157,36 @@ int CalorimeterTowerENC::RecordHits( Jet* truth_jet){
 			return -1;
 		}
 		PHG4Particle* truth_part = _truthinfo->GetParticles(idx);
+		PHG4Hit* truth_hit =_truth_info->GetHits(idx);
 		jetparticles.insert(truth_part); 
+		jethits.insert(truth_hit);
 	}
-	getE2C(jetparticles, 1); //mode 1 uses the reco particles, 2 for towers, 3 (eventually) for thruth
-	getE3C(jetparticles, 1);
-	for(auto p:jetparticles){
+	std::map<PHG4Particle*, std::pair<float, float>> jet_particle_map;
+	for(auto ph:truth_hit){
+		auto p=ph.second;
+		auto pt=jetparticles.at(ph.first).second;
 	       //take in the identified particles that make up the subjet and then we overap that in the towers
 		std::pair<float, float> particle_coords; 
-		particle_coords.first = p->get_eta();
-		particle_coords.second = p->get_phi();
+		particle_coords.first = atanh(p->getz()/sqrt(pow(p->get_y(),2) + pow(p->getx(),2)));
+		particle_coords.second = atan2(p->get_y(), p->get_x());
 		particle_coord.insert(particle_coords);	
+		jet_particle_map[pt]=particle_coords;
 	}
+	getE2C(jet_particle_map, 1, topNode); //mode 1 uses the reco particles, 2 for towers, 3 (eventually) for thruth
+	getE3C(jet_particle_map, 1, topNode);
+	std::unordered_set<int> jet_towers_ihcal, jet_towers_ohcal, jet_towers_emcal; 
+	for(auto pc:jet_coord)
+	{
+		int tne=GetTowerNumber(pc, EMCALMAP.second, EMCALMAP.first);
+		jet_towers_emcal.insert(tne);
+		int tni=GetTowerNumber(pc, IHCALMAP.second, IHCALMAP.first);
+		jet_towers_ihcal.insert(tni);
+		int tno=GetTowerNumber(pc, OHCALMAP.second, OHCALMAP.first);
+		jet_towers_ohcal.insert(tno);
+	}
+	getE2C(jet_towers_ihcal, jet_towers_ohcal, jet_towers_emcal, 2, topNode);
+	getE3C(jet_towers_ihcal, jet_towers_ohcal, jet_towers_emcal, 2, topNode);
+	return 1;
 }
 	       	
 int CalorimeterTowerENC::process_event(PHCompositeNode *topNode){
