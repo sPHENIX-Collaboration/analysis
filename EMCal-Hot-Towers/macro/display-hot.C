3310197933f567b3121f0cbb6c8728fe4d01f3d0
Apurva Narde <58493193+Steepspace@users.noreply.github.com>
2024-07-23 22:51:03 -0400

    Added display-hot macro
    - plots comparison of hot and reference tower energies

---
+++ EMCal-Hot-Towers/macro/display-hot.C
@@ -0,0 +1,203 @@
+// -- c++ includes --
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+// -- root includes --
+#include <TH2F.h>
+#include <TF1.h>
+#include <TFile.h>
+#include <TLine.h>
+#include <TLegend.h>
+#include <TLatex.h>
+#include <TCanvas.h>
+
+// -- Tower Info
+#include <calobase/TowerInfoDefs.h>
+
+// -- sPHENIX Style
+#include "sPhenixStyle.C"
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::to_string;
+using std::vector;
+using std::stringstream;
+using std::min;
+using std::max;
+using std::ofstream;
+using std::pair;
+
+R__LOAD_LIBRARY(libcalo_io.so)
+
+namespace myAnalysis {
+    void plots(const string& i_input, const string &output);
+    Int_t readHotTowerIndexFile(const string &hotList);
+
+    vector<pair<UInt_t,UInt_t>> hotTowerIndex;
+
+    // UInt_t  ntowers   = 24576;
+    // Float_t threshold = 150;
+    // Float_t zMax      = 500;
+}
+
+Int_t myAnalysis::readHotTowerIndexFile(const string &hotList) {
+
+  cout << "Reading Hot and Ref tower indices" << endl;
+
+  std::ifstream file(hotList);
+
+  // Check if the file was successfully opened
+  if (!file.is_open()) {
+      cerr << "Failed to open file: " << hotList << endl;
+      return 1;
+  }
+
+  string line;
+
+  // skip header
+  std::getline(file, line);
+
+  // loop over each run
+  while (std::getline(file, line)) {
+      std::istringstream lineStream(line);
+
+      UInt_t towerHotIndex;
+      UInt_t towerRefIndex;
+      Char_t comma;
+
+      if (lineStream >> towerHotIndex >> comma >> towerRefIndex) {
+          hotTowerIndex.push_back(std::make_pair(towerHotIndex, towerRefIndex));
+          cout << "Hot: " << towerHotIndex << ", Ref: " << towerRefIndex << endl;
+      }
+      else {
+          cerr << "Failed to parse line: " << line << endl;
+          return 1;
+      }
+  }
+
+  // Close the file
+  file.close();
+
+  return 0;
+}
+
+void myAnalysis::plots(const string& i_input, const string &output) {
+    TFile input(i_input.c_str());
+
+    TCanvas* c1 = new TCanvas();
+    c1->SetTickx();
+    c1->SetTicky();
+
+    c1->SetCanvasSize(1500, 1000);
+    c1->SetLeftMargin(.1);
+    c1->SetRightMargin(.05);
+
+    c1->Print((output + "[").c_str(), "pdf portrait");
+
+    c1->cd();
+
+    UInt_t i = 0;
+    // initialize histograms
+    for(auto idx : hotTowerIndex) {
+
+        UInt_t key    = TowerInfoDefs::encode_emcal(idx.first);
+        UInt_t etabin = TowerInfoDefs::getCaloTowerEtaBin(key);
+        UInt_t phibin = TowerInfoDefs::getCaloTowerPhiBin(key);
+
+        string name  = "Hot/HotTower_"+to_string(phibin)+"_"+to_string(etabin);
+        string lHot  = "Hot: (" + to_string(phibin) + "," + to_string(etabin) + ")";
+        auto hHot    = (TH1F*)input.Get(name.c_str());
+
+        key    = TowerInfoDefs::encode_emcal(idx.second);
+        etabin = TowerInfoDefs::getCaloTowerEtaBin(key);
+        phibin = TowerInfoDefs::getCaloTowerPhiBin(key);
+
+        name         = "Ref/RefTower_"+to_string(phibin)+"_"+to_string(etabin)+"_"+to_string(i);
+        string lRef  = "Ref: (" + to_string(phibin) + "," + to_string(etabin) + ")";
+        auto hRef    = (TH1F*)input.Get(name.c_str());
+
+        gPad->SetLogy();
+        hHot->SetStats(0);
+        hHot->SetLineColor(kRed);
+        hHot->GetYaxis()->SetTitleOffset(0.9);
+        hHot->Draw();
+        hRef->SetLineColor(kBlue);
+        hRef->Draw("same");
+
+        TLegend *leg = new TLegend(0.7,.82,0.9,.92);
+        leg->SetFillStyle(0);
+        leg->AddEntry(hHot,lHot.c_str(),"f");
+        leg->AddEntry(hRef,lRef.c_str(),"f");
+        leg->Draw("same");
+
+        c1->Print((string(hHot->GetName()) + ".png").c_str());
+        c1->Print((output).c_str(), "pdf portrait");
+
+        gPad->SetLogy(0);
+        hHot->Rebin(10);
+        hRef->Rebin(10);
+        hHot->Divide(hRef);
+        hHot->GetYaxis()->SetTitle("Hot/Ref");
+        hHot->Draw();
+
+        TLine* line = new TLine(0, 1, 1.5e4, 1);
+        line->SetLineColor(kBlack);
+        line->SetLineStyle(9);
+        // line->SetLineWidth(1);
+        line->Draw("same");
+
+        c1->Print((string(hHot->GetName()) + "-ratio.png").c_str());
+        c1->Print((output).c_str(), "pdf portrait");
+
+        ++i;
+    }
+
+    c1->Print((output + "]").c_str(), "pdf portrait");
+
+    input.Close();
+}
+
+void display_hot(const string &input, const string &hotList, const string &output="plots.pdf") {
+    cout << "#############################" << endl;
+    cout << "Run Parameters" << endl;
+    cout << "input: "  << input << endl;
+    cout << "hotList: "  << hotList << endl;
+    cout << "output: " << output << endl;
+    cout << "#############################" << endl;
+
+    // set sPHENIX plotting style
+    SetsPhenixStyle();
+
+    Int_t ret = myAnalysis::readHotTowerIndexFile(hotList);
+    if(ret) return;
+
+    myAnalysis::plots(input, output);
+}
+
+# ifndef __CINT__
+Int_t main(Int_t argc, char* argv[]) {
+if(argc < 3 || argc > 4){
+        cout << "usage: ./plots-hot input hotList [output]" << endl;
+        cout << "input: input root file" << endl;
+        cout << "hotList: list of hot and reference towers" << endl;
+        cout << "output: output pdf file" << endl;
+        return 1;
+    }
+
+    string output  = "plots.pdf";
+
+    if(argc >= 4) {
+        output = argv[3];
+    }
+
+    display_hot(argv[1], argv[2], output);
+
+    cout << "======================================" << endl;
+    cout << "done" << endl;
+    return 0;
+}
+# endif
