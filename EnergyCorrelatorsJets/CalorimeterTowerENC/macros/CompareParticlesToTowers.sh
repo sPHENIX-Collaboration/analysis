01440c4cc536babb5a59c6a70ed144d96f0eb0eb
Silas K Grossberndt <silas.grossberndt@gmail.com>
2024-07-31 15:05:58 -0400

    Switched to new run to get jetv2 class instead of jetv1 to look at 30GeV trigger I'll need to find another way

--- EnergyCorrelatorsJets/CalorimeterTowerENC/macros/CompareParticlesToTowers.sh
+++ EnergyCorrelatorsJets/CalorimeterTowerENC/macros/CompareParticlesToTowers.sh
@@ -6,5 +6,5 @@ echo $MYINSTALL
 source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL
 runs=${5:-'0'}
 
-root.exe -x /sphenix/user/sgross/sphenix_analysis/EnergyCorrelatorsJets/CalorimeterTowerENC/macros/CompareParticlesToTowers.C\(\"$1\",\"$2\",\"$3\",\"$4\",\"$runs\"\)
+/cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/utils/bin/valgrind  --track-origins=yes --log-file=/gpfs/mnt/gpfs02/sphenix/user/sgross/sphenix_analysis/EnergyCorrelatorsJets/CalorimeterTowerENC/macros/valdrind-out.txt /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/root-6.26.06.p01/bin/root.exe -x /sphenix/user/sgross/sphenix_analysis/EnergyCorrelatorsJets/CalorimeterTowerENC/macros/CompareParticlesToTowers.C\(\"$1\",\"$2\",\"$3\",\"$4\",\"$runs\"\)
 #the structre is truth jet, calorimer tower file, truth reco and global files then the number of events, defaults to all events
