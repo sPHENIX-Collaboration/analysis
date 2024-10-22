fb8dd01e68de7480d8b5a2c62e48e322412ab4ae
Silas Grossberndt <silas.grossberndt@gmail.com>
2024-10-22 16:55:14 -0400

    starting development of the analysis with inbuilt cut QA tree

--- EnergyCorrelatorsJets/LargeRLCaloENC/src/LargeRLENC.cc
+++ EnergyCorrelatorsJets/LargeRLCaloENC/src/LargeRLENC.cc
@@ -23,21 +23,92 @@ LargeRLENC::LargeRLENC(const int n_run/*=0*/, const int n_segment/*=0*/, const f
 	this->nRun=n_run;
 	this->nSegment=n_segment;
 	this->jetMinpT=jet_min_pT;
+	this->algo="Tower";
+	this->radius="r04";
+	this->eventCut=new EventSelectionCut;
 }
-
+JetContainerv2* LargeRLENC::getJets(std::string algo, std::string radius, PHCompositeNode* topNode)
+{
+	//This is just running the Fastjet reco 
+	
+}
+void LargeRLENC::addTower(int n, TowerInfoContainer* energies, RawTowerGeomContainer_Cylinderv1* geom, std::map<std::pair<float, float>, float>* towers)
+{
+	auto key=energies->encode_key(n);
+	auto tower=energies->get_tower_at_channel(n);
+	int phibin=energies->getTowerPhiBin(key);
+	int etabin=energies->getTowerEtabin(key);
+	float phicenter=geom->get_phicenter(phibin);
+	float etacenter=geom->get_etacenter(etabin);
+	std::pair<float, float> center {etacenter, phicenter};
+	(*towers)[center]=tower->get_energy();
+	return;
+}
 int LargeRLENC::process_event(PHCompositeNode* topNode)
 {
 	//This is where I can be a bit clever with allowing for the parrelization for easier crosschecks
 	n_evts++;
+	auto emcal_geom=findNode::getClass<RawTowerGeomContainer_Cylinderv1>(topNode, "TOWERGEOM_CEMC"   );
+	auto emcal_tower_energy=findNode::getClass<TowerInfoContainer>(topNode, "TOWERINFO_CALIB_CEMC"   );
 	auto ihcal_geom= findNode::getClass<RawTowerGeomContainer_Cylinderv1>(topNode, "TOWERGEOM_HCALIN");
 	auto ihcal_tower_energy= findNode::getClass<TowerInfoContainer>(topNode, "TOWERINFO_CALIB_HCALIN");
 	auto ohcal_geom=findNode::getClass<RawTowerGeomContainer_Cylinderv1>(topNode, "TOWERGEOM_HCALOUT");
 	auto ohcal_tower_energy=findNode::getClass<TowerInfoContainer>(topNode, "TOWERINFO_CALIB_HCALOUT");
-	
 	//look for the jet objects 
-	JetContainerv2* jets;
+	JetContainerv2* jets=NULL;
 	bool foundJetConts=false, isDijet=false;
 	try{
 		if(!isRealData) jets = findNode::getClass<JetContainer>(topNode, "AntiKt_Truth_r04"); //look for the r_04 truth jets
 		else{
-			jet
+			std::string recoJetName="AntiKt_"+algo+radius;
+			jets = findNode::getClass<JetContainer>(topNode, recoJetName); //check for already reconstructed jets
+		}
+	}
+	catch(std::exception& e){ std::cout<<"Did not find a jet container object, will attempt to reconstruct the jets" <<std::endl;}
+	if(jets==NULL) jets=getJets(algo, radius);
+	if(!jets){
+		std::cout<<"Didn't find any jets, skipping event" <<std::endl;
+		return -1/*FUN4ALL::*/;
+	}
+	float emcal_energy=0., ihcal_energy=0, ohcal_energy=0, total_energy=0, ohcal_rat=0;
+	std::map<std::pair<float, float>, float> *ihcal_towers, *emcal_towers, *ohcal_towers; //these are just to collect the non-zero towers to decrease the processing time 
+	for(int n=0; n<(int) emcal_tower_energy->size(); n++){
+		float energy=emcal_tower_energy->get_tower_at_channel(n)->get_energy();
+		if(energy > 0.07)//put zero supression into effect
+			addTower(n, emcal_tower_energy, emcal_geom, emcal_towers);
+		emcal_energy+=energy;
+	}
+	for(int n=0; n<(int) ihcal_tower_energy->size(); n++){
+		float energy+=ihcal_tower_energy->get_tower_at_channel(n)->get_energy();
+		if(energy > 0.1) //zero suppression is at 100 MeV in IHCAL
+			addTower(n, ihcal_tower_energy, ihcal_geom, ihcal_towers);
+		ihcal_energy+=energy;
+	}
+	for(int n=0; n<(int) ohcal_tower_energy->size(); n++){
+		float energy+=ohcal_tower_energy->get_tower_at_channel(n)->get_energy();
+		if(energy > 0.5) 
+			addTower(n, ohcal_tower_energy, ohcal_geom, ohcal_towers);
+		ohcal_energy+=energy;
+	}
+	total_energy=emcal_energy+ihcal_energy+ohcal_energy;
+	ohcal_rat=ohcal_energy/total_energy; //take the ratio at the whole calo as that is the region of interest
+	float emcal_occupancy=emcal_towers->size()/((float)emcal_tower_energy->size());
+	float ihcal_occupancy=ihcal_towers->size()/((float)ihcal_tower_energy->size());
+	float ohcal_occupancy=ohcal_towers->size()/((float)ohcal_tower_energy->size());
+	emcal_occup->Fill(emcal_occupancy); //filling in the occupancy plots for easy QA while going 
+	ihcal_occup->Fill(ihcal_occupancy);
+	ohcal_occup->Fill(ohcal_occupancy);
+	isDijet=eventCut->passesTheCut(jets, ohcal_rat);	
+	if(!isDijet){ //stores some data about the bad cuts to look for any arrising structure
+		if(ohcal_rat > 0.9){
+		       ohcal_rat_occup->Fill(ohcal_rat, ohcal_occupancy);
+		       for(auto p:ohcal_towers)ohcal_bad_hits->Fill(p.first.first, p.first.second, p.second);
+		}
+		bad_occ_em_oh->Fill(emcal_occupancy, ohcal_occupancy);
+		bad_occ_em_h->Fill(emcal_occupancy, (ohcal_occupancy+ihcal_occupancy)/2.0);
+	       	return -1;
+	}
+	else{
+		
+		
+}
--- EnergyCorrelatorsJets/LargeRLCaloENC/src/LargeRLENC.h
+++ EnergyCorrelatorsJets/LargeRLCaloENC/src/LargeRLENC.h
@@ -10,8 +10,123 @@
 #include <utility>
 #include <vector>
 #include <string>
-class JetCuts{
-	JetCuts( JetContainer* jets,  float lpt=12., slpt=7., float det=0.7, float dph=2.75,float maxpct=0.9, bool dj=true, bool ne=false ): jets(jetsi), leadingpt(lpt), subleadingpt(slpt), deltaeta(det), deltaphi(dph), maxOHCAL(  
+#include <TH1.h>
+#include <TH2.h>
+#include <TFile.h>
+#include <TTree.h>
+
+struct DijetQATypePlots{
+	DijetQATypePlots(){
+		bad_occ_em_oh_rat=new TH2F("h_bad_occupancy_EM_OH_rat", "Occupancy for events that fail the OHCAL ratio cut; #% Towers #geq 70 MeV EMCAL; #% Towers > 500 MeV OHCAL; N_{Evts}", 100, -0.005, 0.995, 100, -0.005, 0.995);
+		bad_occ_em_h_rat=new TH2F("h_bad_occupancy_EM_H_rat", "Occupancy for events that fail OHCAL ratio cut; #% Towers #geq 70 MeV EMCAL; (#% Towers #geq 500 MeV OHCAL + #% Towers #geq 100 MeV IHCAL)/2", 100, -0.005, 0.995, 100, -0.005, 0.995);
+		bad_occ_em_oh=new TH2F("h_bad_occupancy_EM_OH", "Occupancy for events that pass  OHCAL ratio cut but otherwise fail; #% Towers #geq 70 MeV EMCAL; #% Towers > 500 MeV OHCAL; N_{Evts}", 100, -0.005, 0.995, 100, -0.005, 0.995);
+		bad_occ_em_h=new TH2F("h_bad_occupancy_EM_H", "Occupancy for events that pass OHCAL ratio cut but otherwise fail; #% Towers #geq 70 MeV EMCAL; (#% Towers #geq 500 MeV OHCAL + #% Towers #geq 100 MeV IHCAL)/2", 100, -0.005, 0.995, 100, -0.005, 0.995);
+		ohcal_bad_hits=new TH2F("h_ohcal_bad_hits", "Energy depositon for events that fail the OHCAL ratio cut; #eta; #varphi; E [GeV]", 24, -1.1, 1.1, 64, -3.1416, 3.1415);
+		emcal_occup=new TH1F("h_EMCAL_occupancy", "Occupancy of EMCAL all events; #% Towers #geq 70 MeV EMCAL; N_{evts}", 100, -0.005, 0.995);
+		ihcal_occup=new TH1F("h_IHCAL_occupancy", "Occupancy of IHCAL all events; #% Towers #geq 100 MeV IHCAL; N_{evts}", 100, -0.005, 0.995);
+		ohcal_occup=new TH1F("h_OHCAL_occupancy", "Occupancy of OHCAL all events; #% Towers #geq 500 MeV OHCAL; N_{evts}", 100, -0.005, 0.995);
+		ohcal_rat_occup=new TH2F("h_OHCAL_rat", "Occupancy of OHCAL as function of OHCAL ratio; #frac{E_{OHCAL}}{E_{ALL CALS}}; #% Towers #geq 500 MeV; N_{evts}", 100, -0.005, 0.995, 100, -0.005, 0.995);
+	}
+	TH2F* bad_occ_em_oh_rat;
+	TH2F* bad_occ_em_h_rat;
+	TH2F* bad_occ_em_oh;
+	TH2F* bad_occ_em_h;
+	TH2F* ohcal_bad_hits;
+	TH2F* ohcal_rat_occup;
+	TH1F* emcal_occup;
+	TH1F* ihcal_occup;
+	TH1F* ohcal_occup;
+	std::vector<TH2F*> QA_2D_plots {bad_occ_em_oh_rat, bad_occ_em_h_rat, bad_occ_em_oh, bad_occ_em_h, ohcal_bad_hits, ohcal_rat_occup};
+	std::vector<TH1F*> QA_1D_plots {emcal_occup, ihcal_occup, ohcal_occup};
+	}
+class EventSelectionCut{
+	//maybe this gets some histos added for easier QA-ing of cut safety
+	public:
+		EventSelectionCut(float lpt=12., slpt=7., float det=0.7, float dph=2.75,float maxpct=0.9, bool dj=true, bool ne=false, std::string radius="r04" ): leadingpt(lpt), subleadingpt(slpt), etaedge(det), deltaphi(dph), maxOHCAL(maxpct),isdijet(dj), negativeEnergy(ne){
+			JetCuts=new TTree(Form("cuts_%s", radius) , Form("Jet Cut Kinematics for QA on the Jet Event for jet with radius %s", radius));
+			JetCuts->Branch("N_Jets", &m_nJets, "Number of Jets in Container/I");
+			JetCuts->Branch("Lead_pt", &m_lpt, "p_{T} of Leading Jet/F");
+			JetCuts->Branch("Subleading_pt", &m_slpt, "p_{T} of subleading jet (pair if dijet pair exists)/F");
+			JetCuts->Branch("Lead_eta", &m_etal, "#eta of leading jet center/F");
+			JetCuts->Branch("Subleading_eta", &m_etasl, "#eta of subleading jet center/F");
+			JetCuts->Branch("delta_phi", &m_deltaphi, "#Delta #varphi between leading and subleading jet/F");
+			JetCuts->Branch("ohcal_ratio", &m_ohcalrat, "E_{OHCAL} to E_{allCal}/F");
+			JetCuts->Branch("isDijet", &m_isdijet, "Passes Dijet event cuts/B");
+			JetCuts->Branch("negE", &m_hasnege, "Has a negative energy jet/B");
+			JetCuts->Branch("passes", &passesCuts, "Passes cut/B");
+		};
+		TTree* JetCuts; //This is a QA testing ttree to allow for immediate QA
+		bool passesTheCut(JetContianerv2* eventjets, float hcalratio){
+			//check if the particular event passed the cut 
+			bool good=true;
+			m_ohcalrat=hcalratio;
+			if(hcalratio > maxOHCAL) good=false;
+			float leadjetpt=0., subleadjetpt=0.;
+			bool haspartner=false;
+		       	Jet* leadjet, *subleadjet;
+		       	for(auto j:jets){
+				float pt=j->get_pt();
+				if(pt > leadjetpt){
+					if(leadjet)subleadjet=leadjet;
+					leadjet=j;
+				       	subleadjetpt=leadjetpt;
+				       	leadjetpt=pt;
+				       }
+				if(!negativeEnergy){
+					if(j.get_e() < 0){
+						m_hasnege=true;
+				       		good=false;
+					}
+					}
+				}
+			if(leadjetpt < leadingpt) good=false;
+			float leadeta=leadjet->get_eta(), leadphi=leadjet->get_phi();
+			if(abs(leadeta) > deltaeta ) good=false; //getting rid of events that have the leading jet outside of acceptance region
+			for(auto j:jets){
+				float phi=j->get_phi();
+				if(abs(phi-leadphi) > deltaphi){
+				       	subleadjet=j;
+					subleadjetpt=j->get_pt();
+					haspartner=true;
+					break;
+				}
+			}
+			if(subleadjet){ 
+				float sldeta=subleadjet->get_eta();
+				if(abs(sldeta) > deltaeta) good=false;
+				m_etasl=sldeta;
+				m_detaphi=subleadjet->get_phi()-leadjet->get_phi();
+
+			}
+			else good=false;
+			if(subleadjetpt < subleadingpt || !haspartner) good=false;
+			passesCut=true;
+			m_isdijet=haspartner;
+			m_nJets=jets->size();
+			m_lpt=leadjetpt;
+			m_slpt=subleadjetpt;
+			JetCuts->Fill();
+			return good;
+		}
+	private:
+		float leadingpt;
+		float subleadingpt; 
+		float etaedge;
+		float deltaphi;
+		float maxOHCAL;
+		bool isdijet;
+		bool negativeEnergy;
+		bool passesCut=false;
+		int m_nJets=0;
+		float m_lpt=0.;
+		float m_slpt=0.;
+		float m_etal=0.;
+		float m_etasl=0.;
+		float m_deltaphi=0.;
+		float m_ohcalrat=0.;
+		bool m_isdijet=false;
+		bool m_hasnege=false;
+};
 
 class PHCompositeNode;
 
@@ -60,7 +175,7 @@ class LargeRLENC : public SubsysReco
 	bool isRealData;
 	int nRun, nSegment;
 	float jetMinpT;
+	TTree* DijetQA, *EEC, *JetEvtObs;
 	MethodHistograms* FullHcal, *TowardRegion, *AwayRegion, *TransverseRegion;
 };
-
 #endif // LARGERLENC_H
