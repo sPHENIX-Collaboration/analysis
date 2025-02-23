524e607211dc9543377d1b7ef499d662899565af
Apurva Narde <58493193+Steepspace@users.noreply.github.com>
2025-02-23 18:09:55 -0500

    Fixed errors for vop.py
    - removed old library and script runs

--- EMCal-Bias-Study/scripts/vop.py
+++ EMCal-Bias-Study/scripts/vop.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import psycopg2
-import cgi
 
 print("Content-type:text/html\r\n\r\n")
 print('<html>')
@@ -25,23 +24,23 @@ print("</tr>")
 print("</table>")
 print("</form>")
 
-
 try:
     conn = psycopg2.connect(host="sphnxdbmaster.sdcc.bnl.gov", database="emcal", user="phnxrc", password="")
     cur = conn.cursor()
 #    sql_select = "select * from vop "
 #    sql_select = "select *,cast(1000*(bias-69.0) as int) as gs from vop "
 
-    form = cgi.FieldStorage()
-    vmpod = form.getvalue("vmpod","66.5")
-    sql_select = "select *,cast(1000*(bias-"+vmpod+"-2.5) as int) as gs from vop "
+    vmpod = 66.5
+    sql_select = f"select *,cast(1000*(bias-{vmpod}-2.5) as int) as gs from vop "
 #   sql_where = form['sql_where'].value
     sql_where = "where sector="
-    sql_where += form.getvalue("sector","1")
+    sql_where += str(1)
     sql_order = " order by sector,ib,channel asc;"
 
     sql = sql_select + sql_where + sql_order
-        
+
+    print(f'sql command: {sql}')
+
     cur.execute(sql)
 
     print(sql+"<br><hr>\n")
