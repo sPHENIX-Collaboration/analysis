0ce3b2d980fd51d7ac1a31758689b753b068ba11
Silas K Grossberndt <silas.grossberndt@gmail.com>
2025-04-18 15:49:51 -0400

    Updated to properly implement LHAPDF sets for NNPDF30 for Nashville Tune

--- HerwigToHepMCProduction/HepMC_scripts/Herwig_run.sh
+++ HerwigToHepMCProduction/HepMC_scripts/Herwig_run.sh
@@ -5,8 +5,11 @@ filetag=$(printf "%06d" $filenumber)
 triggervalue=${4:-'0.'}
 outfile_name=${5:-' '}
 source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana
+export LHAPDF_DATA_PATH=$LHAPDF_DATA_PATH:$(pwd)/../../config_files
+export LHAPATH=$LHAPATH:$(pwd)/../../config_files
+echo $LHAPDF_DATA_PATH
 seedn=$RANDOM
-echo $seedn
+#echo $seedn
 run_events=$goalevents
 if [[ $triggervalue -gt 0 ]]; then 
 	run_events=$(( run_events * 100 )) #worst case assume a 1% good event case
@@ -16,6 +19,7 @@ outfile_nameseed=$outfile_name"-S"$seedn"-"$filetag".hepmc"
 outfile_namestem=$outfile_name
 outfile_name=$outfile_name"-"$filetag".hepmc"
 /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/Herwig/bin/Herwig run $1 -N $run_events -t $filetag --seed=$seedn
+echo $filetag
 mv $outfile_namestem"-S"$seedn"-"$filetag".log" $outfile_namestem"-"$filetag".log"
 mv $outfile_namestem"-S"$seedn"-"$filetag"-EvtGen.log" $outfile_namestem"-"$filetag"-EvtGen.log"
 mv $outfile_namestem"-S"$seedn"-"$filetag".out" $outfile_namestem"-"$filetag".out"
