1d4cd8c744ed045a6c5c3a1ad8fa626fe7523472
Apurva Narde <58493193+Steepspace@users.noreply.github.com>
2025-06-15 16:40:33 -0400

    Update display macro
    - removed extra comments
    - no functional changes introduced

--- EMCal-Calibration-Study/macros/display.C
+++ EMCal-Calibration-Study/macros/display.C
@@ -46,31 +46,9 @@ namespace fs = std::filesystem;
 namespace myAnalysis {
     void make_plots(const string &outputDir);
     Int_t readHists();
-    // void initHists();
-    // void saveHists(const string &outputDir);
-    // void findBestPulseWidth();
-
-    // unordered_map<string, Int_t> m_best_pulse; // key: sector,ib ; value: best width
-    // unordered_map<string,Int_t> m_runInfo; // key: width,offset ; value: run number
-    // unordered_map<Int_t,string> m_runInfoRev; // key: run number ; value: width,offset
     map<string,TH1*> m_hists;
 
-    // Double_t kFactor  = 4.42e-4; // Units: [1 / mV]
-
-    // unordered_set<string> m_knownBadIB = {"4.1","10.3","10.5","25.2","50.1","54.4"}; // sector.IB
-
-    // Int_t m_bins_gain = 80;
-    // Double_t m_gain_low = 0;
-    // Double_t m_gain_high = 4;
-
-    // Int_t m_bins_saturate = 800;
-    // Double_t m_saturate_low  = 0;  // GeV
-    // Double_t m_saturate_high = 80; // GeV
-
-    // Double_t m_noiseThreshold = 100; // ADC
-
     Bool_t m_saveFig = true;
-    // Int_t m_verbosity = 2;
 }
 
 Int_t myAnalysis::readHists() {
@@ -155,13 +133,10 @@ void myAnalysis::make_plots(const string &outputDir) {
 
     // ----------------------------------------
 
-    // m_hists["h_InvMass_2024"]->Scale(1.*1e4/m_hists["h_event_2024"]->GetBinContent(1));
-    // m_hists["h_InvMass_2025"]->Scale(1.*1e4/m_hists["h_event_2025"]->GetBinContent(1));
     m_hists["h_InvMass_2024"]->Scale(1./m_hists["h_InvMass_2024"]->Integral(1, m_hists["h_InvMass_2024"]->FindBin(1)-1));
     m_hists["h_InvMass_2025"]->Scale(1./m_hists["h_InvMass_2025"]->Integral(1, m_hists["h_InvMass_2025"]->FindBin(1)-1));
 
     m_hists["h_InvMass_2025"]->Draw("hist");
-    // m_hists["h_InvMass_2025"]->SetTitle("Invariant Mass; M_{#gamma#gamma} [GeV]; Counts / 10k Events / 5 MeV");
     m_hists["h_InvMass_2025"]->SetTitle("Invariant Mass; M_{#gamma#gamma} [GeV]; Normalized Counts");
     m_hists["h_InvMass_2025"]->GetXaxis()->SetTitleOffset(1.f);
     m_hists["h_InvMass_2025"]->GetYaxis()->SetTitleOffset(1.4f);
@@ -194,300 +169,6 @@ void myAnalysis::make_plots(const string &outputDir) {
     // ----------------------------------------
 
     c1->Print((output + "]").c_str(), "pdf portrait");
-
-    // for (const auto &[name, hist] : m_hists) {
-    //     if(name.starts_with("h2CEMC")) {
-    //         Int_t run = std::stoi(myUtils::split(name,'_')[1]);
-    //         vector<string> width_offset = myUtils::split(m_runInfoRev[run],',');
-    //         stringstream title;
-    //         title << "EMCal [ADC], Run: " << run << ", Pulse Width: " << width_offset[0] << " ns, Offset: " << width_offset[1];
-
-    //         hist->SetTitle(title.str().c_str());
-
-    //         hist->Draw("COLZ1");
-    //         m_hists["h2DummySector"]->Draw("TEXT MIN0 same");
-    //         m_hists["h2DummyIB"]->Draw("TEXT MIN0 same");
-
-    //         hist->SetMaximum(1.64e4);
-
-    //         c1->Print(output.c_str(), "pdf portrait");
-    //         if (m_saveFig) c1->Print((outputDir + "/images/" + name + ".png").c_str());
-    //     }
-    // }
-
-    // // ----------------------------------------
-
-    // m_hists["h2ResponseDefault"]->Draw("COLZ1");
-    // m_hists["h2DummySector"]->Draw("TEXT MIN0 same");
-    // m_hists["h2DummyIB"]->Draw("TEXT MIN0 same");
-
-    // m_hists["h2ResponseDefault"]->SetMaximum(1.64e4);
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2ResponseDefault.png").c_str());
-
-    // // ----------------------------------------
-
-    // m_hists["h2ResponseNew"]->Draw("COLZ1");
-    // m_hists["h2DummySector"]->Draw("TEXT MIN0 same");
-    // m_hists["h2DummyIB"]->Draw("TEXT MIN0 same");
-
-    // m_hists["h2ResponseNew"]->SetMaximum(1.64e4);
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2ResponseNew.png").c_str());
-
-    // // ----------------------------------------
-
-    // m_hists["h2GainFactor"]->Draw("COLZ1");
-    // m_hists["h2DummySector"]->Draw("TEXT MIN0 same");
-    // m_hists["h2DummyIB"]->Draw("TEXT MIN0 same");
-
-    // m_hists["h2GainFactor"]->SetTitle("Gain");
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2GainFactor.png").c_str());
-
-    // m_hists["h2GainFactor"]->SetMinimum(0.1);
-    // m_hists["h2GainFactor"]->SetMaximum(0.9);
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2GainFactor-zoom.png").c_str());
-
-    // // ----------------------------------------
-
-    // m_hists["h2ResponseRatio"]->Draw("COLZ1");
-    // m_hists["h2DummySector"]->Draw("TEXT MIN0 same");
-    // m_hists["h2DummyIB"]->Draw("TEXT MIN0 same");
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2ResponseRatio.png").c_str());
-
-    // m_hists["h2ResponseRatio"]->SetMinimum(0.1);
-    // m_hists["h2ResponseRatio"]->SetMaximum(0.9);
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2ResponseRatio-zoom.png").c_str());
-
-    // // ----------------------------------------
-
-    // c1->SetCanvasSize(1400, 1000);
-    // c1->SetLeftMargin(.14f);
-    // c1->SetRightMargin(.05f);
-    // c1->SetTopMargin(.1f);
-    // c1->SetBottomMargin(.12f);
-    // gStyle->SetOptStat(1111);
-
-    // m_hists["hResponseRatio"]->Draw("hist e");
-    // m_hists["hResponseRatio"]->SetStats();
-    // m_hists["hResponseRatio"]->SetLineColor(kRed);
-    // m_hists["hResponseRatio"]->SetMarkerColor(kRed);
-    // m_hists["hResponseRatio"]->GetXaxis()->SetTitleOffset(0.9f);
-    // m_hists["hResponseRatio"]->GetXaxis()->SetRangeUser(0,1.5);
-
-    // gPad->Update();
-
-    // TPaveStats* st = static_cast<TPaveStats*>(m_hists["hResponseRatio"]->FindObject("stats"));
-    // Double_t xlow = 0.68;
-    // Double_t ylow = 0.62;
-    // st->SetX1NDC(xlow);
-    // st->SetY1NDC(ylow);
-    // st->SetX2NDC(xlow+0.27);
-    // st->SetY2NDC(ylow+0.28);
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/hResponseRatio.png").c_str());
-
-    // gPad->SetLogy();
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/hResponseRatio-logy.png").c_str());
-
-    // gPad->SetLogy(0);
-
-    // // ----------------------------------------
-    // c1->SetCanvasSize(2900, 1000);
-    // c1->SetLeftMargin(.06f);
-    // c1->SetRightMargin(.1f);
-    // c1->SetTopMargin(.1f);
-    // c1->SetBottomMargin(.12f);
-
-    // m_hists["h2ResponseRatioDivGain"]->Draw("COLZ1");
-    // m_hists["h2DummySector"]->Draw("TEXT MIN0 same");
-    // m_hists["h2DummyIB"]->Draw("TEXT MIN0 same");
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2ResponseRatioDivGain.png").c_str());
-
-    // m_hists["h2ResponseRatioDivGain"]->SetMinimum(0.4);
-    // m_hists["h2ResponseRatioDivGain"]->SetMaximum(1.1);
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2ResponseRatioDivGain-zoom.png").c_str());
-
-    // // ----------------------------------------
-
-    // c1->SetCanvasSize(1400, 1000);
-    // c1->SetLeftMargin(.14f);
-    // c1->SetRightMargin(.05f);
-    // c1->SetTopMargin(.1f);
-    // c1->SetBottomMargin(.12f);
-    // gStyle->SetOptFit(1111); // Shows Probability, Chi2/NDF, Errors, Values
-
-    // TFitResultPtr fitResult = myUtils::doGausFit(m_hists["hResponseRatioDivGain"], 0.87, 1.08);
-
-    // m_hists["hResponseRatioDivGain"]->Draw("e");
-    // m_hists["hResponseRatioDivGain"]->SetStats();
-    // m_hists["hResponseRatioDivGain"]->SetMarkerColor(kBlack);
-    // m_hists["hResponseRatioDivGain"]->GetXaxis()->SetTitleOffset(0.9f);
-    // m_hists["hResponseRatioDivGain"]->GetXaxis()->SetRangeUser(0,2);
-
-    // gPad->Update();
-
-    // st = static_cast<TPaveStats*>(m_hists["hResponseRatioDivGain"]->FindObject("stats"));
-    // xlow = 0.65;
-    // ylow = 0.5;
-    // st->SetX1NDC(xlow);
-    // st->SetY1NDC(ylow);
-    // st->SetX2NDC(xlow+0.3);
-    // st->SetY2NDC(ylow+0.4);
-    // st->SetTextSize(0.03f);
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/hResponseRatioDivGain.png").c_str());
-
-    // gPad->SetLogy();
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/hResponseRatioDivGain-logy.png").c_str());
-
-    // // ----------------------------------------
-
-    // gPad->SetLogy(0);
-    // gPad->SetLogz();
-
-    // c1->SetCanvasSize(1400, 1000);
-    // c1->SetLeftMargin(.11f);
-    // c1->SetRightMargin(.13f);
-    // c1->SetTopMargin(.1f);
-    // c1->SetBottomMargin(.12f);
-
-    // m_hists["hResponseRatioVsGain"]->Draw("COLZ1");
-    // m_hists["hResponseRatioVsGain"]->GetXaxis()->SetRangeUser(0.2,1.2);
-    // m_hists["hResponseRatioVsGain"]->GetYaxis()->SetRangeUser(0.2,1.2);
-    // m_hists["hResponseRatioVsGain"]->GetYaxis()->SetTitleOffset(0.85f);
-    // m_hists["hResponseRatioVsGain"]->GetXaxis()->SetTitleOffset(0.9f);
-
-    // TH1* px = static_cast<TH2*>(m_hists["hResponseRatioVsGain"])->ProfileX();
-    // px->SetLineColor(kRed);
-    // px->SetMarkerColor(kRed);
-    // px->Draw("same");
-
-    // TF1* tf = new TF1("tf","x",0,2);
-    // tf->SetLineStyle(kDashed);
-    // tf->Draw("same");
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/hResponseRatioVsGain.png").c_str());
-
-    // // ----------------------------------------
-
-    // c1->SetCanvasSize(1400, 1000);
-    // c1->SetLeftMargin(.13f);
-    // c1->SetRightMargin(.05f);
-    // c1->SetTopMargin(.1f);
-    // c1->SetBottomMargin(.12f);
-    // gPad->SetGrid(0,0);
-
-    // cout << "Saturate Hists Entries" << endl;
-    // cout << "hSaturate: " << m_hists["hSaturate"]->Integral(1, m_bins_saturate) << endl;
-    // cout << "hSaturateV2: " << m_hists["hSaturateV2"]->Integral(1, m_bins_saturate) << endl;
-    // cout << "hSaturateV3: " << m_hists["hSaturateV3"]->Integral(1, m_bins_saturate) << endl;
-    // cout << "hSaturateV4: " << m_hists["hSaturateV4"]->Integral(1, m_bins_saturate) << endl;
-
-    // Double_t normalize = m_hists["hSaturateV3"]->Integral(1, m_bins_saturate) / m_hists["hSaturateV4"]->Integral(1, m_bins_saturate);
-
-    // m_hists["hSaturateV4"]->Scale(normalize);
-
-    // cout << "hSaturateV4 (after normalization): " << m_hists["hSaturateV4"]->Integral(1, m_bins_saturate) << endl;
-
-    // m_hists["hSaturate"]->Draw("hist e");
-    // m_hists["hSaturate"]->GetXaxis()->SetTitleOffset(0.9f);
-    // m_hists["hSaturate"]->GetYaxis()->SetRangeUser(0,4e3);
-    // m_hists["hSaturate"]->Rebin(10);
-
-    // m_hists["hSaturateV2"]->Draw("hist e same");
-    // m_hists["hSaturateV2"]->Rebin(10);
-    // m_hists["hSaturateV2"]->SetLineColor(kRed);
-    // m_hists["hSaturateV2"]->SetMarkerColor(kRed);
-
-    // m_hists["hSaturateV3"]->Draw("hist e same");
-    // m_hists["hSaturateV3"]->Rebin(10);
-    // m_hists["hSaturateV3"]->SetLineColor(kBlue);
-    // m_hists["hSaturateV3"]->SetMarkerColor(kBlue);
-
-    // m_hists["hSaturateV4"]->Draw("hist e same");
-    // m_hists["hSaturateV4"]->Rebin(10);
-    // m_hists["hSaturateV4"]->SetLineColor(kMagenta);
-    // m_hists["hSaturateV4"]->SetMarkerColor(kMagenta);
-
-    // stringstream legA, legB, legC, legD;
-
-    // legA << "Before Temperature Correction";
-    // legB << "After Temperature Correction";
-    // legC << "After Adjusting Gain (Expected)";
-    // legD << "After Adjusting Gain (Observed)";
-
-    // Double_t xshift = -0.08;
-    // Double_t yshift = 0.03;
-
-    // auto leg = new TLegend(0.2+xshift,.65+yshift,0.54+xshift,.85+yshift);
-    // leg->SetFillStyle(0);
-    // leg->SetTextSize(0.04f);
-    // leg->AddEntry(m_hists["hSaturate"],legA.str().c_str(),"lpe");
-    // leg->AddEntry(m_hists["hSaturateV2"],legB.str().c_str(),"lpe");
-    // leg->AddEntry(m_hists["hSaturateV3"],legC.str().c_str(),"lpe");
-    // leg->AddEntry(m_hists["hSaturateV4"],legD.str().c_str(),"lpe");
-    // leg->Draw("same");
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/hSaturate-overlay.png").c_str());
-
-    // // ----------------------------------------
-
-    // c1->SetCanvasSize(2900, 1000);
-    // c1->SetLeftMargin(.06f);
-    // c1->SetRightMargin(.1f);
-    // c1->SetTopMargin(.1f);
-    // c1->SetBottomMargin(.12f);
-    // gPad->SetLogz(0);
-    // gPad->SetGrid();
-
-    // m_hists["h2SaturateV3"]->Draw("COLZ1");
-    // m_hists["h2DummySector"]->Draw("TEXT MIN0 same");
-    // m_hists["h2DummyIB"]->Draw("TEXT MIN0 same");
-
-    // m_hists["h2SaturateV3"]->SetMinimum(10);
-    // m_hists["h2SaturateV3"]->SetMaximum(60);
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2SaturateV3.png").c_str());
-
-    // // ----------------------------------------
-
-    // m_hists["h2SaturateV4"]->Draw("COLZ1");
-    // m_hists["h2DummySector"]->Draw("TEXT MIN0 same");
-    // m_hists["h2DummyIB"]->Draw("TEXT MIN0 same");
-
-    // m_hists["h2SaturateV4"]->SetMinimum(10);
-    // m_hists["h2SaturateV4"]->SetMaximum(60);
-
-    // c1->Print(output.c_str(), "pdf portrait");
-    // if (m_saveFig) c1->Print((outputDir + "/images/h2SaturateV4.png").c_str());
-
-    // // ----------------------------------------
-
-    // c1->Print((output + "]").c_str(), "pdf portrait");
 }
 
 void display(const string &outputDir=".") {
@@ -503,7 +184,6 @@ void display(const string &outputDir=".") {
 
     // create output directories
     fs::create_directories(outputDir);
-    // fs::create_directories(plotDir);
     fs::create_directories(outputDir+"/images");
 
     TH1::AddDirectory(kFALSE);
@@ -511,35 +191,6 @@ void display(const string &outputDir=".") {
     if(myAnalysis::readHists()) return;
 
     myAnalysis::make_plots(outputDir);
-
-    // if(!myUtils::readCSV(input_raw_info, myAnalysis::readRunInfo)) return;
-
-    // if(myAnalysis::m_verbosity > 1) {
-    //     cout << "####################" << endl;
-    //     for (const auto& [key, value] : myAnalysis::m_runInfo) {
-    //         cout << "Key: " << key << ", Run: " << value << endl;
-    //     }
-    //     cout << "Total: " << myAnalysis::m_runInfo.size() << endl;
-    //     cout << "####################" << endl;
-    // }
-
-    // // do not skip the first line as it's not the usual header
-    // if(!myUtils::readCSV(input_raw, myAnalysis::readRun, false)) return;
-
-    // if(myAnalysis::m_verbosity > 1) {
-    //     cout << "####################" << endl;
-    //     for (const auto& [key, value] : myAnalysis::m_hists) {
-    //         cout << "Key: " << key << ", Run: " << value->GetName() << endl;
-    //     }
-    //     cout << "Total: " << myAnalysis::m_hists.size() << endl;
-    //     cout << "####################" << endl;
-    // }
-
-    // myAnalysis::findBestPulseWidth();
-
-    // myAnalysis::initHists();
-    // myAnalysis::analyze();
-    // myAnalysis::saveHists(outputDir);
 }
 
 # ifndef __CINT__
