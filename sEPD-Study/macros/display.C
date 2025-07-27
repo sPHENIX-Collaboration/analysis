d19c9339b5a5ed9f40ff77cb65e2629b40187cd0
Apurva Narde <58493193+Steepspace@users.noreply.github.com>
2025-07-26 21:49:44 -0400

    display macro - Add MBD and MinBias QA
    - Add plots for total MBD charge
    - Compute fraction of events that fail the various MB checks for events with |z| < 10 cm
    - Add option to save each plot as its own png
    - MBD charge distribution - show centrality 2% line computed from the reference histogram

--- sEPD-Study/macros/display.C
+++ sEPD-Study/macros/display.C
@@ -10,6 +10,7 @@
 #include <map>
 #include <unordered_set>
 #include <stdexcept> // For exception handling
+#include <format>
 
 // -- root includes --
 #include <TFile.h>
@@ -50,15 +51,18 @@ namespace fs = std::filesystem;
 namespace myAnalysis {
     void make_plots_psi(TH1* hPsi_x, TH1* hPsi_y, TLegend* leg, TCanvas* c, Int_t n, const string &title, const string &x_title);
     Bool_t make_plots_QA(const string &input, const string &outputDir, const string &qa_file_path);
+    void make_plots_QA_overlay(const string &outputDir);
     void make_plots_Q_vec(const string &input, const string &outputDir);
     void print_stats();
 
     struct QA_general {
         std::unique_ptr<TH1> hEvent;
+        std::unique_ptr<TH1> hEventMinBias;
         std::unique_ptr<TH1> hCentrality;
         std::unique_ptr<TH1> hVtxZ;
         std::unique_ptr<TH1> hVtxZ_MB;
         std::unique_ptr<TH2> h2SEPD_Charge;
+        std::unique_ptr<TH2> h2MBD_Total_Charge;
         std::unique_ptr<TH3> h3SEPD_Total_Charge;
     };
 
@@ -69,9 +73,14 @@ namespace myAnalysis {
         std::unique_ptr<TH3> h3SEPD_EventPlaneInfo_Psi_3;
     };
 
+    // MBD Charge corresponding to 2% centrality
+    double m_cent2;
+
+    map<string, std::unique_ptr<TH1>> m_hists;
     map<string,UInt_t> m_ctr;
 
     Bool_t m_saveFig = true;
+    Bool_t m_save_individual_Fig = true;
 }
 
 void myAnalysis::make_plots_psi(TH1 *hPsi_x, TH1 *hPsi_y, TLegend *leg, TCanvas *c, Int_t n, const string &title, const string &x_title)
@@ -171,6 +180,65 @@ void myAnalysis::make_plots_Q_vec(const string &input, const string &outputDir) 
     if (m_saveFig) c1->Print((outputDir + "/images/QA-Q-vec-" + run + ".png").c_str());
 }
 
+void myAnalysis::make_plots_QA_overlay(const string &outputDir) {
+    std::unique_ptr<TCanvas> c1 = std::make_unique<TCanvas>();
+    c1->SetTickx();
+    c1->SetTicky();
+
+    c1->SetCanvasSize(1400, 1000);
+    c1->SetBottomMargin(1.f);
+    c1->SetLeftMargin(1.f);
+
+    gStyle->SetOptTitle(1);
+    gStyle->SetTitleStyle(0);
+    gStyle->SetTitleW(1);
+    gStyle->SetTitleH(0.06f);
+    gStyle->SetTitleY(1.f);
+    gStyle->SetTitleFillColor(0);
+    gStyle->SetTitleBorderSize(0);
+
+    c1->cd();
+    gPad->SetLogy();
+
+    string output = outputDir + "/plots-MBD-overlay.pdf";
+
+    double ylow = 1e-7;
+    double yhigh = 1e-1;
+
+    for (const auto &[name, hist] : m_hists)
+    {
+        if (name.starts_with("h2MBD_Total_Charge_x"))
+        {
+          hist->Draw("HIST same");
+          hist->Scale(1. / hist->Integral());
+
+          double area = hist->Integral(1, hist->FindBin(2000));
+          if (area < 0.98)
+          {
+              hist->SetLineColor(kRed);
+          }
+
+          hist->SetTitle("|z| < 10 cm and MB");
+          hist->GetYaxis()->SetTitle("Normalized Events");
+          hist->GetYaxis()->SetRangeUser(ylow, yhigh);
+          hist->GetYaxis()->SetTitleOffset(1.f);
+          hist->GetXaxis()->SetTitleOffset(0.98f);
+        }
+    }
+
+    std::unique_ptr<TLine> line = std::make_unique<TLine>(m_cent2, ylow, m_cent2, yhigh);
+    line->Draw("same");
+    line->SetLineColor(kBlue);
+
+    TLatex latex;
+    latex.SetTextAngle(90);
+    latex.SetTextSize(0.08f);
+    latex.DrawLatex(m_cent2, 1e-6, "#color[4]{Centrality: 2%}");
+
+    c1->Print(output.c_str(), "pdf portrait");
+    if (m_saveFig) c1->Print((outputDir + "/images/QA-MBD-overlay.png").c_str());
+}
+
 Bool_t myAnalysis::make_plots_QA(const string &input, const string &outputDir, const string &qa_file_path) {
     fs::path p(input);
     string stem = p.stem().string();
@@ -186,10 +254,12 @@ Bool_t myAnalysis::make_plots_QA(const string &input, const string &outputDir, c
     QA_general qa =
     {
       .hEvent = std::unique_ptr<TH1>(static_cast<TH1*>(tfile->Get("hEvent"))),
+      .hEventMinBias = std::unique_ptr<TH1>(static_cast<TH1*>(tfile->Get("hEventMinBias"))),
       .hCentrality = std::unique_ptr<TH1>(static_cast<TH1*>(tfile->Get("hCentrality"))),
       .hVtxZ = std::unique_ptr<TH1>(static_cast<TH1*>(tfile->Get("hVtxZ"))),
       .hVtxZ_MB = std::unique_ptr<TH1>(static_cast<TH1*>(tfile->Get("hVtxZ_MB"))),
       .h2SEPD_Charge = std::unique_ptr<TH2>(static_cast<TH2*>(tfile->Get("h2SEPD_Charge"))),
+      .h2MBD_Total_Charge = std::unique_ptr<TH2>(static_cast<TH2*>(tfile->Get("h2MBD_Total_Charge"))),
       .h3SEPD_Total_Charge = std::unique_ptr<TH3>(static_cast<TH3*>(tfile->Get("h3SEPD_Total_Charge")))
     };
 
@@ -197,7 +267,13 @@ Bool_t myAnalysis::make_plots_QA(const string &input, const string &outputDir, c
     c1->SetTickx();
     c1->SetTicky();
 
-    c1->SetCanvasSize(1200, 1000);
+    c1->SetCanvasSize(1700, 1000);
+
+    std::unique_ptr<TCanvas> c2 = std::make_unique<TCanvas>();
+    c2->SetTickx();
+    c2->SetTicky();
+
+    c2->SetCanvasSize(1200, 1000);
 
     gStyle->SetOptTitle(1);
     gStyle->SetTitleStyle(0);
@@ -206,7 +282,7 @@ Bool_t myAnalysis::make_plots_QA(const string &input, const string &outputDir, c
     gStyle->SetTitleFillColor(0);
     gStyle->SetTitleBorderSize(0);
 
-    c1->Divide(2, 2, 0.00025f, 0.00025f);
+    c1->Divide(3, 2, 0.00025f, 0.00025f);
 
     string output = outputDir + "/pdf/plots-" + run + ".pdf";
 
@@ -229,37 +305,85 @@ Bool_t myAnalysis::make_plots_QA(const string &input, const string &outputDir, c
 
     c1->cd(1);
     gPad->SetTopMargin(0.1f);
+    gStyle->SetTitleY(.97f);
 
     stringstream title;
     title << "Run: " << run;
 
     qa.hEvent->Draw();
+    qa.hEvent->SetTitle(std::format("Run {}", run).c_str());
     qa.hEvent->GetXaxis()->SetTitleOffset(1.f);
     qa.hEvent->GetYaxis()->SetTitleOffset(1.6f);
     qa.hEvent->GetXaxis()->SetLabelSize(0.05f);
     qa.hEvent->GetYaxis()->SetRangeUser(0, qa.hEvent->GetMaximum()*1.1);
     qa.hEvent->SetTitle(title.str().c_str());
-    gStyle->SetTitleY(.97f);
+
+    if (m_save_individual_Fig)
+    {
+        c2->cd();
+
+        gPad->SetTopMargin(0.1f);
+        gStyle->SetTitleY(.97f);
+        qa.hEvent->Draw();
+
+        c2->Print((outputDir + "/images/QA-Events-" + run + ".png").c_str());
+    }
 
     c1->cd(2);
     gPad->SetTopMargin(0.1f);
+    gStyle->SetTitleY(.97f);
 
-    qa.hCentrality->Draw();
-    qa.hCentrality->GetXaxis()->SetTitleOffset(1.f);
-    qa.hCentrality->GetYaxis()->SetTitleOffset(1.6f);
+    qa.hEventMinBias->Draw("HIST");
+    qa.hEventMinBias->Scale(100./qa.hEvent->GetBinContent(2));
+    qa.hEventMinBias->GetXaxis()->SetTitleOffset(1.f);
+    qa.hEventMinBias->GetYaxis()->SetTitleOffset(1.3f);
+    qa.hEventMinBias->GetXaxis()->SetLabelSize(0.04f);
+    qa.hEventMinBias->GetYaxis()->SetRangeUser(0, qa.hEventMinBias->GetMaximum()*1.1);
+    qa.hEventMinBias->SetTitle(std::format("Run {}: |z| < 10 cm", run).c_str());
+    qa.hEventMinBias->GetYaxis()->SetTitle("% of Events: |z| < 10 cm");
 
-    c1->Update();
+    if (m_save_individual_Fig)
+    {
+        c2->cd();
+
+        gPad->SetTopMargin(0.1f);
+        gStyle->SetTitleY(.97f);
+        qa.hEventMinBias->Draw("HIST");
+
+        c2->Print((outputDir + "/images/QA-Events-MinBias-" + run + ".png").c_str());
+    }
+
     c1->cd(3);
     gPad->SetTopMargin(0.1f);
+
+    qa.hCentrality->Draw();
+    qa.hCentrality->SetTitle(std::format("Run {}: |z| < 10 cm and MB", run).c_str());
+    qa.hCentrality->GetXaxis()->SetTitleOffset(1.f);
+    qa.hCentrality->GetYaxis()->SetTitleOffset(1.7f);
+
+    if (m_save_individual_Fig)
+    {
+        c2->cd();
+
+        gPad->SetTopMargin(0.1f);
+        qa.hCentrality->Draw();
+
+        c2->Print((outputDir + "/images/QA-Centrality-" + run + ".png").c_str());
+    }
+
+    c1->cd(4);
+
+    gPad->SetTopMargin(0.1f);
+    gStyle->SetTitleY(1.f);
 
     Int_t vtx_z_rebin_factor = 4;
 
     qa.hVtxZ->Draw();
+    qa.hVtxZ->SetTitle(std::format("Run {}", run).c_str());
     qa.hVtxZ->Rebin(vtx_z_rebin_factor);
     qa.hVtxZ->GetXaxis()->SetTitleOffset(1.f);
-    qa.hVtxZ->GetYaxis()->SetTitleOffset(1.6f);
+    qa.hVtxZ->GetYaxis()->SetTitleOffset(1.7f);
     qa.hVtxZ->GetYaxis()->SetRangeUser(0, qa.hVtxZ->GetMaximum()*1.1);
-    gStyle->SetTitleY(1.f);
 
     qa.hVtxZ_MB->Draw("same");
     qa.hVtxZ_MB->Rebin(vtx_z_rebin_factor);
@@ -273,7 +397,7 @@ Bool_t myAnalysis::make_plots_QA(const string &input, const string &outputDir, c
         ++m_ctr["high_avg_zvtx"];
     }
 
-    std::unique_ptr<TLine> line = std::make_unique<TLine>(z_vtx_mb_avg, 0, z_vtx_mb_avg, qa.hVtxZ->GetMaximum()*1);
+    std::unique_ptr<TLine> line = std::make_unique<TLine>(z_vtx_mb_avg, 0, z_vtx_mb_avg, qa.hVtxZ->GetMaximum());
     line->Draw("same");
     line->SetLineColor(kBlue);
 
@@ -292,7 +416,22 @@ Bool_t myAnalysis::make_plots_QA(const string &input, const string &outputDir, c
     leg->AddEntry(qa.hVtxZ_MB.get(),legB.str().c_str(),"l");
     leg->Draw("same");
 
-    c1->cd(4);
+    if (m_save_individual_Fig)
+    {
+        c2->cd();
+
+        gPad->SetTopMargin(0.1f);
+        gStyle->SetTitleY(1.f);
+
+        qa.hVtxZ->Draw();
+        qa.hVtxZ_MB->Draw("same");
+        line->Draw("same");
+        leg->Draw("same");
+
+        c2->Print((outputDir + "/images/QA-Z-Vtx-" + run + ".png").c_str());
+    }
+
+    c1->cd(5);
     gPad->SetTopMargin(0.1f);
     gPad->SetRightMargin(0.15f);
     gPad->SetLogz();
@@ -300,15 +439,74 @@ Bool_t myAnalysis::make_plots_QA(const string &input, const string &outputDir, c
 
     std::unique_ptr<TH2> h3SEPD_Total_Charge_yx(static_cast<TH2*>(qa.h3SEPD_Total_Charge->Project3D("yx")));
     h3SEPD_Total_Charge_yx->Draw("COLZ1");
-    h3SEPD_Total_Charge_yx->SetTitle("sEPD Total Charge: |z| < 10 cm and MB");
+    h3SEPD_Total_Charge_yx->SetTitle(std::format("Run {}: |z| < 10 cm and MB", run).c_str());
+    h3SEPD_Total_Charge_yx->GetXaxis()->SetTitle("sEPD Total Charge South");
+    h3SEPD_Total_Charge_yx->GetYaxis()->SetTitle("sEPD Total Charge North");
     h3SEPD_Total_Charge_yx->GetXaxis()->SetLabelSize(0.035f);
-    h3SEPD_Total_Charge_yx->GetYaxis()->SetTitleOffset(1.6f);
+    h3SEPD_Total_Charge_yx->GetYaxis()->SetTitleOffset(1.7f);
     h3SEPD_Total_Charge_yx->GetXaxis()->SetTitleOffset(1.f);
 
+    if (m_save_individual_Fig)
+    {
+        c2->cd();
+
+        gPad->SetTopMargin(0.1f);
+        gPad->SetRightMargin(0.15f);
+        gPad->SetLogz();
+        gStyle->SetTitleY(1.f);
+
+        h3SEPD_Total_Charge_yx->Draw("COLZ1");
+        c2->Print((outputDir + "/images/QA-sEPD-Charge-" + run + ".png").c_str());
+    }
+
+    c1->cd(6);
+    gPad->SetTopMargin(0.1f);
+    gPad->SetLogy();
+
+    std::unique_ptr<TH1> h2MBD_Total_Charge_x(static_cast<TH1*>(qa.h2MBD_Total_Charge->ProjectionX()));
+
+    h2MBD_Total_Charge_x->Draw();
+    h2MBD_Total_Charge_x->GetYaxis()->SetTitle("Events");
+    h2MBD_Total_Charge_x->SetTitle(std::format("Run {}: |z| < 10 cm and MB", run).c_str());
+    h2MBD_Total_Charge_x->GetXaxis()->SetRangeUser(0, 4e3);
+    h2MBD_Total_Charge_x->GetYaxis()->SetRangeUser(5e-1, h2MBD_Total_Charge_x->GetMaximum()*10);
+    h2MBD_Total_Charge_x->GetYaxis()->SetTitleOffset(1.6f);
+    h2MBD_Total_Charge_x->GetXaxis()->SetTitleOffset(1.f);
+
+    std::unique_ptr<TH1> h2MBD_Total_Charge_y(static_cast<TH1*>(qa.h2MBD_Total_Charge->ProfileY()));
+    m_cent2 = h2MBD_Total_Charge_y->GetBinContent(h2MBD_Total_Charge_y->FindBin(2));
+
+    std::unique_ptr<TLine> line2 = std::make_unique<TLine>(m_cent2, 0, m_cent2, h2MBD_Total_Charge_x->GetMaximum());
+    line2->Draw("same");
+    line2->SetLineColor(kBlue);
+
+    TLatex latex;
+    latex.SetTextAngle(90);
+    latex.SetTextSize(0.06f);
+    latex.DrawLatex(m_cent2, 1, "#color[4]{Centrality: 2%}");
+
+    if (m_save_individual_Fig)
+    {
+        c2->cd();
+        gPad->SetTopMargin(0.1f);
+        gPad->SetLogy();
+        h2MBD_Total_Charge_x->Draw();
+        line2->Draw("same");
+        latex.DrawLatex(m_cent2, 1, "#color[4]{Centrality: 2%}");
+        c2->Print((outputDir + "/images/QA-MBD-Charge-" + run + ".png").c_str());
+    }
+
+    if (is_good)
+    {
+        m_hists["h2MBD_Total_Charge_x_" + run] = std::unique_ptr<TH1>(static_cast<TH1 *>(h2MBD_Total_Charge_x->Clone(("h2MBD_Total_Charge_x_" + run).c_str())));
+    }
     c1->Update();
 
     c1->Print(output.c_str(), "pdf portrait");
-    if (m_saveFig) c1->Print((outputDir + "/images/QA-" + run + ".png").c_str());
+    if (m_saveFig)
+    {
+        c1->Print((outputDir + "/images/QA-" + run + ".png").c_str());
+    }
 
     // Open the CSV file for writing
     std::ofstream qa_file(qa_file_path, std::ios::app);
@@ -345,14 +543,21 @@ void myAnalysis::print_stats() {
     cout << "=====================" << endl;
 }
 
-void display(const string &input, const string &outputDir=".") {
+void display(const string &input,
+             const string &outputDir=".",
+             bool saveFig = true,
+             bool saveIndividualFig = true) {
 
     cout << "#############################" << endl;
     cout << "Run Parameters" << endl;
     cout << "input: " << input << endl;
     cout << "outputDir: " << outputDir << endl;
-    cout << "save figs: " << myAnalysis::m_saveFig << endl;
+    cout << "save figs: " << saveFig << endl;
+    cout << "save individual figs: " << saveIndividualFig << endl;
     cout << "#############################" << endl;
+
+    myAnalysis::m_saveFig = saveFig;
+    myAnalysis::m_save_individual_Fig = saveIndividualFig;
 
     // set sPHENIX plotting style
     SetsPhenixStyle();
@@ -363,7 +568,7 @@ void display(const string &input, const string &outputDir=".") {
     fs::create_directories(outputDir+"/QA-Q-vec/images");
     fs::create_directories(outputDir+"/QA-Q-vec/pdf");
 
-    // TH1::AddDirectory(kFALSE);
+    TH1::AddDirectory(kFALSE);
 
     std::ifstream input_file(input);
 
@@ -387,25 +592,37 @@ void display(const string &input, const string &outputDir=".") {
         }
     }
 
+    myAnalysis::make_plots_QA_overlay(outputDir+"/QA");
+
     myAnalysis::print_stats();
 }
 
 # ifndef __CINT__
 Int_t main(Int_t argc, const char* const argv[]) {
-if(argc < 2 || argc > 3){
-        cout << "usage: ./" << argv[0] << " input [outputDir]" << endl;
+if(argc < 2 || argc > 5){
+        cout << "usage: ./" << argv[0] << " input [outputDir] [saveFig] [saveIndividualFig]" << endl;
         cout << "input: list of root files" << endl;
         cout << "outputDir: output directory" << endl;
+        cout << "saveFig: save png files" << endl;
+        cout << "saveIndividualFig: save individual png files" << endl;
         return 1;
     }
 
     string outputDir = ".";
+    bool saveFig = true;
+    bool saveIndividualFig = true;
 
     if(argc >= 3) {
         outputDir = argv[2];
     }
+    if(argc >= 4) {
+        saveFig = atoi(argv[3]);
+    }
+    if(argc >= 5) {
+        saveIndividualFig = atoi(argv[4]);
+    }
 
-    display(argv[1], outputDir);
+    display(argv[1], outputDir, saveFig, saveIndividualFig);
 
     cout << "======================================" << endl;
     cout << "done" << endl;
