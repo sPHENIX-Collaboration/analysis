413c651ef232d269ee1954c97abb29083effd610
Apurva Narde <58493193+Steepspace@users.noreply.github.com>
2025-09-05 20:55:30 -0400

    displayHotMap - Add Frac Chi2 Map and plots
    - Frac Chi2 is used for tagging Hot Towers.

--- sEPD-Study/macros/displayHotMap.C
+++ sEPD-Study/macros/displayHotMap.C
@@ -24,18 +24,20 @@
 #include <memory>
 #include <string>
 
-void displayHotMap(unsigned int runnumber, const std::string &outputDir = ".", const std::string &dbtag = "newcdbtag", const std::string &overWriteURL = "")
+void displayHotMap(unsigned int runnumber, const std::string &outputDir = ".", const std::string &dbtag = "newcdbtag", const std::string &overWriteURL = "", const std::string &overWriteFracChi2URL = "")
 {
   std::cout << "##############################################" << std::endl;
   std::cout << "Processing" << std::endl;
-  std::cout << std::format("Run: {}", runnumber) << std::endl;
-  std::cout << std::format("Output Directory: {}", outputDir) << std::endl;
-  std::cout << std::format("dbtag: {}", dbtag) << std::endl;
-  std::cout << std::format("Overwrite URL: {}", overWriteURL) << std::endl;
+  std::cout << std::format("Run: {}\n", runnumber);
+  std::cout << std::format("Output Directory: {}\n", outputDir);
+  std::cout << std::format("dbtag: {}\n", dbtag);
+  std::cout << std::format("Overwrite URL Bad Towers: {}\n", overWriteURL);
+  std::cout << std::format("Overwrite URL Frac Chi2: {}\n", overWriteFracChi2URL);
   std::cout << "##############################################" << std::endl;
 
   setGlobalTag(dbtag);
   std::string emcal_hotmap = (overWriteURL.empty()) ? getCalibration("CEMC_BadTowerMap", runnumber) : overWriteURL;
+  std::string emcal_chi2 = (overWriteFracChi2URL.empty()) ? getCalibration("CEMC_hotTowers_fracBadChi2", runnumber) : overWriteFracChi2URL;
 
   if (emcal_hotmap.starts_with("DataBaseException"))
   {
@@ -43,9 +45,18 @@ void displayHotMap(unsigned int runnumber, const std::string &outputDir = ".", c
     return;
   }
 
+  bool doHotChi2 = !emcal_chi2.starts_with("DataBaseException");
+
   std::cout << std::format("EMCal Bad Tower Map: {}", emcal_hotmap) << std::endl;
+  std::cout << std::format("EMCal Frac Bad Chi2: {}", emcal_chi2) << std::endl;
+
   std::unique_ptr<CDBTTree> cdbttree_hotMap = std::make_unique<CDBTTree>(emcal_hotmap);
+
+  std::unique_ptr<CDBTTree> cdbttree_chi2 = (doHotChi2) ? std::make_unique<CDBTTree>(emcal_chi2) : nullptr;
+
   const std::string fieldname = "status";
+  const std::string fieldname_chi2 = "fraction";
+  const float fraction_badChi2_threshold = 0.01f;
 
   // set sPHENIX plotting style
   SetsPhenixStyle();
@@ -68,6 +79,9 @@ void displayHotMap(unsigned int runnumber, const std::string &outputDir = ".", c
 
   title = std::format("EMCal Cold: Run {} ; Tower Index #phi; Tower Index #eta", runnumber);
   hists["h2EMCalBadTowersCold"] = std::make_unique<TH2F>("h2EMCalBadTowersCold", title.c_str(), myUtils::m_nphi, 0, myUtils::m_nphi, myUtils::m_neta, 0, myUtils::m_neta);
+
+  title = std::format("EMCal Fraction badChi2: Run {}; Tower Index #phi; Tower Index #eta", runnumber);
+  hists["h2EMCalBadTowersFracBadChi2"] = std::make_unique<TH2F>("h2EMCalBadTowersFracBadChi2", title.c_str(), myUtils::m_nphi, 0, myUtils::m_nphi, myUtils::m_neta, 0, myUtils::m_neta);
 
   // dummy hists for labeling
   hists["h2DummySector"] = std::make_unique<TH2F>("h2DummySector", "", myUtils::m_nsector / 2, 0, myUtils::m_nsector / 2, 2, 0, 2);
@@ -107,6 +121,7 @@ void displayHotMap(unsigned int runnumber, const std::string &outputDir = ".", c
     unsigned int eta = TowerInfoDefs::getCaloTowerEtaBin(key);
 
     int hotMap_val = cdbttree_hotMap->GetIntValue(static_cast<int>(key), fieldname);
+    float fraction_badChi2 = (doHotChi2) ? cdbttree_chi2->GetFloatValue(static_cast<int>(key), fieldname_chi2) : 0;
 
     // nonzero means tower is bad
     if (hotMap_val != 0)
@@ -129,9 +144,15 @@ void displayHotMap(unsigned int runnumber, const std::string &outputDir = ".", c
         ++ctr["cold"];
       }
     }
+
+    if(fraction_badChi2 > fraction_badChi2_threshold)
+    {
+      hists["h2EMCalBadTowersFracBadChi2"]->Fill(phi, eta);
+      ++ctr["fracBadChi2"];
+    }
   }
 
-  std::cout << std::format("Bad: {}, Dead: {}, Hot: {}, Cold: {}\n", ctr["bad"], ctr["dead"], ctr["hot"], ctr["cold"]);
+  std::cout << std::format("Bad: {}, Dead: {}, Hot: {}, Cold: {}, Frac Bad Chi2: {}\n", ctr["bad"], ctr["dead"], ctr["hot"], ctr["cold"], ctr["fracBadChi2"]);
 
   std::unique_ptr<TCanvas> c1 = std::make_unique<TCanvas>("c1");
   c1->SetTickx();
@@ -178,13 +199,14 @@ int main(int argc, const char *const argv[])
 {
   const std::vector<std::string> args(argv, argv + argc);
 
-  if (args.size() < 2 || args.size() > 5)
+  if (args.size() < 2 || args.size() > 6)
   {
-    std::cerr << "usage: " << args[0] << " <runnumber> [outputDir] [dbtag] [overWriteURL]" << std::endl;
+    std::cerr << "usage: " << args[0] << " <runnumber> [outputDir] [dbtag] [overWriteURL] [overWriteFracChi2URL]" << std::endl;
     std::cerr << "  runnumber: Run" << std::endl;
     std::cerr << "  outputDir: Output Plot Directory" << std::endl;
     std::cerr << "  dbtag: (optional) database tag (default: newcdbtag)" << std::endl;
     std::cerr << "  overWriteURL: (optional) Direct URL to EMCal Bad Tower Map (default: none)" << std::endl;
+    std::cerr << "  overWriteFracChi2URL: (optional) Direct URL to EMCal Frac Chi2 (default: none)" << std::endl;
     return 1;  // Indicate error
   }
 
@@ -192,6 +214,7 @@ int main(int argc, const char *const argv[])
   std::string outputDir = ".";
   std::string dbtag = "newcdbtag";
   std::string overWriteURL = "";
+  std::string overWriteFracChi2URL = "";
 
   if (args.size() >= 3)
   {
@@ -205,8 +228,12 @@ int main(int argc, const char *const argv[])
   {
     overWriteURL = args[4];
   }
+  if (args.size() >= 6)
+  {
+    overWriteFracChi2URL = args[5];
+  }
 
-  displayHotMap(runnumber, outputDir, dbtag, overWriteURL);
+  displayHotMap(runnumber, outputDir, dbtag, overWriteURL, overWriteFracChi2URL);
 
   std::cout << "======================================" << std::endl;
   std::cout << "done" << std::endl;
