8f58eaaa7eab4cbcce179d898e43357bfc725ac5
Genki Nukazuka <genki.nukazuka@riken.jp>
2025-09-11 01:51:38 -0400

    Analysis codes for the polarization plots were added to get the performance preliminary.

---
+++ Polarization/macro/functions.hh
@@ -0,0 +1,66 @@
+#pragma once
+
+int GetSphenixColor()
+{
+  // RGB = (62, 136, 209)
+  Int_t color_index = TColor::GetFreeColorIndex();
+  TColor* my_color = new TColor(color_index,
+                                62.0 / 255.0,
+                                136.0 / 255.0,
+                                209.0 / 255.0);
+
+  return color_index;  // my_color->GetColor();
+}
+
+void WritesPhenix(int mode = 0, double pos_x = 0.2, double pos_y = 0.96, double text_size = 0.04)
+{
+  TLatex* tex = new TLatex();
+  tex->SetTextSize(text_size);
+
+  if (mode == 0)
+    tex->DrawLatexNDC(pos_x, pos_y, "#it{#bf{sPHENIX}} Internal");
+  else if (mode == 1)
+    tex->DrawLatexNDC(pos_x, pos_y, "#it{#bf{sPHENIX}} Preliminary");
+  else if (mode == 2)
+    tex->DrawLatexNDC(pos_x, pos_y, "#it{#bf{sPHENIX}} Work in Progress");
+
+  delete tex;
+};
+
+void WriteRunCondition(double pos_x = 0.2, double pos_y = 0.675, bool is_mc = false, double text_size = 0.04)
+{
+  TLatex* tex = new TLatex();
+  tex->SetTextSize(text_size);
+
+  if (is_mc == false)
+  {
+    tex->DrawLatexNDC(pos_x, pos_y, "p^{#uparrow}+p 2024");
+    tex->DrawLatexNDC(pos_x - 0.01, pos_y - 0.05, "#sqrt{s} = 200 GeV");
+  }
+  else
+    tex->DrawLatexNDC(pos_x, pos_y, "Simulation");
+
+  delete tex;
+}
+
+void WriteDate(double pos_x = 0.775, double pos_y = 0.955, double text_size = 0.04)
+{
+  TLatex* tex = new TLatex();
+  tex->SetTextSize(text_size);
+
+  TDatime now;
+  int year = now.GetYear();
+  int month = now.GetMonth();
+  int day = now.GetDay();
+  stringstream message;
+  message << month << "/" << day << "/" << year;
+  tex->DrawLatexNDC(pos_x, pos_y, message.str().c_str());
+
+  delete tex;
+};
+
+void HistSetting(TH1D* hist, int color)
+{
+  hist->SetLineColor(color);
+  hist->SetFillColorAlpha(color, 0.1);
+}
