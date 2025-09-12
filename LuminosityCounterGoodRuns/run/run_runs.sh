a696b1ea929ebc5e59947c22d61201bd09b4e0fd
Joseph Clement <jocl6895@colorado.edu>
2025-09-12 11:41:42 -0400

    update to use correct dst list names

--- LuminosityCounterGoodRuns/run/run_runs.sh
+++ LuminosityCounterGoodRuns/run/run_runs.sh
@@ -6,7 +6,7 @@ echo `pwd`
 LISTDIR=lists
 for rn in `cat fullgoodrunlist.list`; do #`ls  ${LISTDIR}/dst_calofitting_run2pp*.list | awk -F'.' '{print $1}' | awk -F'/' '{print $2}' | awk -F'-' '{print $2}'`; do #`cat fullgoodrunlist.list`; do
     rn=$(expr $rn + 0)
-    nfile=`wc -l ${LISTDIR}/dst_calofitting_run2pp-000${rn}.list | awk '{print $1}'`
+    nfile=`wc -l ${LISTDIR}/dst_calofitting-000${rn}.list | awk '{print $1}'`
     njob=$(( $nfile + 19 ))
     njob=$(( $njob / 20 ))
     filecounter=$(( $filecounter + $njob ))
