0153a19d24be7d98cc20a1a41ae0bde2af43d32d
Genki Nukazuka <genki.nukazuka@riken.jp>
2025-09-13 03:00:30 -0400

    The polarization and the relative luminosity analysis codes were updated/added. There is no change in the analysis itself, but the plot formats were improved.

--- Polarization_RelativeLuminosity/macro/relative_luminosity.cc
+++ Polarization_RelativeLuminosity/macro/relative_luminosity.cc
@@ -1,69 +1,6 @@
-//#include <uspin/SpinDBContentv1.h> // for ana.505 or later
-#include <uspin/SpinDBContent.h>  // for ana.504 or earlier
-#include <uspin/SpinDBOutput.h>
+#include "polarization.cc"
 
-#include "functions.hh"
-#include "sPhenixStyle.C"
-
-const int kBunch_num_max = 120;
-
-// It writes, for example,
-//     <Pb> = 50%
-// on the canvas
-void WriteAveragePolarization( double value, string beam, double pos_x, double pos_y, int digits=2 )
-{
-  TLatex* tex = new TLatex();
-  tex->SetTextSize( 0.04 );
-  stringstream ss;
-  ss << "#LTP_{" << beam << "}#GT = " << setprecision( digits ) << value << "%";
-
-  tex->DrawLatexNDC( pos_x, pos_y, ss.str().c_str() );
-  delete tex;
-}
-
-void WriteAdditionalMessage( double pos_x, double pos_y )
-{
-  TLatex* tex = new TLatex();
-  tex->SetTextSize( 0.04 );
-  stringstream ss;
-  ss << "Preliminary RHIC Polarimetry Group Results";
-  //ss << "Preliminary CNI Polarimetry Group Results";
-
-  tex->DrawLatexNDC( pos_x, pos_y, ss.str().c_str() );
-  delete tex;
-}
-
-long long GetPolarizedLuminosity(const long long scalers[kBunch_num_max], const int pattern[kBunch_num_max], const int polarity)
-{
-  long long sum = 0;
-  for (int i = 0; i < kBunch_num_max; i++)
-  {
-    if (pattern[i] == polarity)
-    {
-      sum += scalers[i];
-    }
-  }
-
-  return sum;
-}
-
-bool GetGoldenRuns(string list, vector<int>& runs)
-{
-  ifstream ifs(list.c_str());
-  if (ifs.fail())
-  {
-    cerr << "Fail to open " << list << endl;
-    return false;
-  }
-
-  int temp;
-  while (ifs >> temp)
-    runs.push_back(temp);
-
-  return true;
-}
-
-int polarization()
+int relative_luminosity()
 {
   SetsPhenixStyle();
   string golden_run_list = "list/Full_ppGoldenRunList_Version2.list";
@@ -72,7 +9,7 @@ int polarization()
   cout << "- Golden run list: " << golden_run_list << endl;
   cout << "- " << golden_runs.size() << " golden runs taken" << endl;
 
-  string output_base = "results/polarization";
+  string output_base = "results/relative_luminosity";
   string output_root = output_base + ".root";
 
   TFile* tf = new TFile(output_root.c_str(), "RECREATE");
@@ -80,8 +17,7 @@ int polarization()
   /////////////////////////////////////////////////////////////////////////////////////
   // variables
   /////////////////////////////////////////////////////////////////////////////////////
-  int run = 150045;  // 0;
-  bool is_bad_run = true;
+  int run = 0;
 
   ////////////////////////
   // for Blue ring
@@ -89,8 +25,6 @@ int polarization()
   double pol_error_b[kBunch_num_max] = {-9999.9};  // Error of blue beam polarization for each bunch
   double pol_sys_error_b[kBunch_num_max] = {0};
   int pattern_b[kBunch_num_max] = {-9999};  // spin pattern of blue beam
-  Long64_t lumi_b = 0, lumi_pos_b = 0, lumi_nega_b = 0;
-  double lumi_ratio_b = 0;
 
   ////////////////////////
   // for Yellow ring
@@ -98,9 +32,7 @@ int polarization()
   double pol_error_y[kBunch_num_max] = {-9999.9};  // Error of yellow beam polarization for each bunch
   double pol_sys_error_y[kBunch_num_max] = {-9999};
   int pattern_y[kBunch_num_max] = {-9999};  // spin pattern of yellow beam
-  Long64_t lumi_y = 0, lumi_pos_y = 0, lumi_nega_y = 0;
-  double lumi_ratio_y = 0;
-
+  
   ////////////////////////
   // for both beam
   Long64_t mbd_scalers_no_cut[kBunch_num_max] = {0};
@@ -109,8 +41,6 @@ int polarization()
   /////////////////////////////////////////////////////////////////////////////////////
   // Database setup
   /////////////////////////////////////////////////////////////////////////////////////
-  int spin_db_status = 0;
-
   // Get the spin patterns from the spin DB
   //  0xffff is the qa_level from XingShiftCal //////
   unsigned int qa_level = 0;  // 0xffff; // <--- old version
@@ -130,17 +60,19 @@ int polarization()
   /////////////////////////////////////////////////////////////////////////////////////
   // Histograms setup
   /////////////////////////////////////////////////////////////////////////////////////
-  TH1D* hist_pol_b = new TH1D("hist_pol_b", "Blue beam polarization;Polarization [%];Counts", 100, 0, 100);
-  HistSetting(hist_pol_b, GetSphenixColor());
+  double min_x = 0.8, max_x = 1.2;
+  int bin = 80;
+  TH1D* hist_lumi_b = new TH1D("hist_lumi_b", "Blue beam relative luminosity;Relative luminosity;Counts", bin, min_x, max_x);
+  HistSetting(hist_lumi_b, GetSphenixColor());
 
-  TH1D* hist_weighted_pol_b = new TH1D("hist_weighted_pol_b", "Blue beam polarization with luminosity weight;Polarization [%];Counts [a.u.]", 100, 0, 100);
-  HistSetting(hist_weighted_pol_b, GetSphenixColor());
+  TH1D* hist_weighted_lumi_b = new TH1D("hist_weighted_lumi_b", "Blue beam relative luminosity with luminosity weight;Relative luminosity = #mathcal{L};Counts [a.u.]", bin, min_x, max_x);
+  HistSetting(hist_weighted_lumi_b, GetSphenixColor());
 
-  TH1D* hist_pol_y = new TH1D("hist_pol_y", "Yellow beam polarization;Polarization [%];Counts", 100, 0, 100);
-  HistSetting(hist_pol_y, kOrange + 1);
+  TH1D* hist_lumi_y = new TH1D("hist_lumi_y", "Yellow beam relative luminosity;Relative luminosity;Counts", bin, min_x, max_x);
+  HistSetting(hist_lumi_y, kOrange + 1);
 
-  TH1D* hist_weighted_pol_y = new TH1D("hist_weighted_pol_y", "Yellow beam polarization with luminosity weight;Polarization [%];Counts [a.u.]", 100, 0, 100);
-  HistSetting(hist_weighted_pol_y, kOrange + 1);
+  TH1D* hist_weighted_lumi_y = new TH1D("hist_weighted_lumi_y", "Yellow beam relative luminosity with luminosity weight;Relative luminosity;Counts [a.u.]", bin, min_x, max_x); // #equiv#font[12]{L}^{#uparrow}/#font[12]{L}^{#downarrow}
+  HistSetting(hist_weighted_lumi_y, kOrange + 1);
 
   Long64_t luminosity_sum = 0;
   for (auto& run : golden_runs)
@@ -151,10 +83,12 @@ int polarization()
       continue;
     }
 
-    is_bad_run = bool(spin_db_content.GetBadRunFlag());  // int 0 -> false, int 1 -> true
+    bool is_bad_run = bool(spin_db_content.GetBadRunFlag());  // int 0 -> false, int 1 -> true
 
     if (is_bad_run == true)
+    {
       continue;
+    }     
 
     for (int i = 0; i < kBunch_num_max; i++)
     {
@@ -165,8 +99,7 @@ int polarization()
 
       long long mbd_scaler = spin_db_content.GetScalerMbdNoCut(i);
       mbd_scalers_no_cut[i] = mbd_scaler;
-
-      /*
+       /*
       cout << setw(4) << i << " "
            << setw(3) << pattern_b[i] << "\t"
            << pol_b[i] << "\t"
@@ -179,36 +112,35 @@ int polarization()
     /////////////////////////////////////////////////////////////////////////////////////
     // luminosity calc for blue/yellow depending on polarization direction
     // blue beam
-    lumi_pos_b = GetPolarizedLuminosity(mbd_scalers_no_cut, pattern_b, 1);
-    lumi_nega_b = GetPolarizedLuminosity(mbd_scalers_no_cut, pattern_b, -1);
+    Long64_t lumi_pos_b = GetPolarizedLuminosity(mbd_scalers_no_cut, pattern_b, 1);
+    Long64_t lumi_nega_b = GetPolarizedLuminosity(mbd_scalers_no_cut, pattern_b, -1);
+    double lumi_ratio_b = 0;
     if (lumi_nega_b != 0)
       lumi_ratio_b = 1. * lumi_pos_b / lumi_nega_b;
-    else
-      lumi_ratio_b = 0;
 
-    lumi_b = lumi_pos_b + lumi_nega_b;
+    auto lumi_b = lumi_pos_b + lumi_nega_b;
 
     // yellow beam
-    lumi_pos_y = GetPolarizedLuminosity(mbd_scalers_no_cut, pattern_y, 1);
-    lumi_nega_y = GetPolarizedLuminosity(mbd_scalers_no_cut, pattern_y, -1);
+    Long64_t lumi_pos_y = GetPolarizedLuminosity(mbd_scalers_no_cut, pattern_y, 1);
+    Long64_t lumi_nega_y = GetPolarizedLuminosity(mbd_scalers_no_cut, pattern_y, -1);
+    double lumi_ratio_y = 0;
     if (lumi_nega_y != 0)
       lumi_ratio_y = 1. * lumi_pos_y / lumi_nega_y;
-    else
-      lumi_ratio_y = 0;
 
-    lumi_y = lumi_pos_y + lumi_nega_y;
+    auto lumi_y = lumi_pos_y + lumi_nega_y;
 
     luminosity_sum += lumi_b;
-    hist_pol_b->Fill(pol_b[0]);
-    hist_weighted_pol_b->Fill(pol_b[0], lumi_b);
-    hist_pol_y->Fill(pol_y[0]);
-    hist_weighted_pol_y->Fill(pol_y[0], lumi_y);
 
-  }
+    hist_lumi_b->Fill( lumi_ratio_b );
+    hist_weighted_lumi_b->Fill(lumi_ratio_b, lumi_b);
+    hist_lumi_y->Fill( lumi_ratio_y );
+    hist_weighted_lumi_y->Fill( lumi_ratio_y , lumi_y);
 
-  hist_weighted_pol_b->Scale(1.0 / luminosity_sum);
-  hist_weighted_pol_y->Scale(1.0 / luminosity_sum);
-  cout << hist_pol_b->GetEntries() << " good runs" << endl;
+  } // end of loop over runs
+
+  hist_weighted_lumi_b->Scale(1.0 / luminosity_sum);
+  hist_weighted_lumi_y->Scale(1.0 / luminosity_sum);
+  cout << hist_lumi_b->GetEntries() << " good runs" << endl;
 
   ////////////////////////////////////////////////////////////////////
   // Draw!
@@ -217,19 +149,20 @@ int polarization()
   c->SetFillStyle( 0 );
   gPad->SetFrameFillStyle( 0 );
 
-  hist_weighted_pol_b->GetXaxis()->SetRangeUser(35, 65);
-  hist_weighted_pol_b->GetXaxis()->SetLabelOffset( 0.0125 );
-  hist_weighted_pol_b->GetXaxis()->CenterTitle();
-  hist_weighted_pol_b->GetYaxis()->SetTitleOffset( 1.5 );
-  hist_weighted_pol_b->GetYaxis()->CenterTitle();
-  hist_weighted_pol_b->GetYaxis()->SetRangeUser( 0, 0.25 );
-
+  hist_weighted_lumi_y->GetXaxis()->SetRangeUser( 0.9, 1.1);
+  hist_weighted_lumi_y->GetXaxis()->SetLabelOffset( 0.0125 );
+  hist_weighted_lumi_y->GetXaxis()->SetLabelSize( 0.04 );
+  hist_weighted_lumi_y->GetXaxis()->CenterTitle();
+  hist_weighted_lumi_y->GetYaxis()->SetTitleOffset( 1.5 );
+  hist_weighted_lumi_y->GetYaxis()->CenterTitle();
+  hist_weighted_lumi_y->GetYaxis()->SetLabelSize( 0.04 );
+  hist_weighted_lumi_y->GetYaxis()->SetRangeUser( 0.0, 0.2 );
 
   for( int mode=0; mode<3; mode++ ) // loop over modes: preliminary, internal, work in progress
   {
     // polarization with luminosity weight
-    hist_weighted_pol_b->Draw("HIST");
-    hist_weighted_pol_y->Draw("HIST same");
+    hist_weighted_lumi_y->Draw("HIST");
+    hist_weighted_lumi_b->Draw("HIST same");
 
     string output_pdf = output_base;
     if( mode == 0 )
@@ -242,36 +175,27 @@ int polarization()
     int digits = 2;
     WriteDate();
     WritesPhenix( mode );
-    double dy = 0.05, y = 0.89 + dy;
-        
+    double dy = 0.05, y = 0.875 + dy;
+
     y -= dy;
-    WriteRunCondition( 0.2, y); // p+p 2024 sqrt{s} = 200 GeV
+    WriteRunCondition( 0.2, y, false, 0.04, true );
 
-    y -= dy;//*1.25;
-    WriteAdditionalMessage( 0.2, y); // Preliminary RHIC Polarimetry Group Results
-
-    y -= dy/2/2;
-    TLegend* leg = new TLegend( 0.2, y-0.1, 0.4, y );
+    y += dy*3/4;
+    TLegend* leg = new TLegend( 0.675, y - 0.1, 0.875, y );
     leg->SetTextSize( 0.04 );
     leg->SetBorderSize( 0 );
     leg->SetFillStyle( 0 );
-    leg->AddEntry( hist_weighted_pol_b, "Blue beam", "l" );
-    leg->AddEntry( hist_weighted_pol_y, "Yellow beam", "l" );
-
-    y -= dy*2.75;
-    WriteAveragePolarization( hist_weighted_pol_b->GetMean(), "b", 0.2, y, digits );
-
-    y -= dy;
-    WriteAveragePolarization( hist_weighted_pol_y->GetMean(), "y", 0.2, y, digits );
+    leg->AddEntry( hist_weighted_lumi_b, "Blue beam", "l" );
+    leg->AddEntry( hist_weighted_lumi_y, "Yellow beam", "l" );
   
     leg->Draw();
     c->Print( output_pdf.c_str() );
   }
 
-  tf->WriteTObject(hist_pol_b, hist_pol_b->GetName());
-  tf->WriteTObject(hist_weighted_pol_b, hist_weighted_pol_b->GetName());
-  tf->WriteTObject(hist_pol_y, hist_pol_y->GetName());
-  tf->WriteTObject(hist_weighted_pol_y, hist_weighted_pol_y->GetName());
+  tf->WriteTObject(hist_lumi_b, hist_lumi_b->GetName());
+  tf->WriteTObject(hist_weighted_lumi_b, hist_weighted_lumi_b->GetName());
+  tf->WriteTObject(hist_lumi_y, hist_lumi_y->GetName());
+  tf->WriteTObject(hist_weighted_lumi_y, hist_weighted_lumi_y->GetName());
   tf->Close();
   // tr->Print();
   return 0;
