c227143588bd723c0ada66ce298f471e9584836f
Silas K Grossberndt <silas.grossberndt@gmail.com>
2026-03-05 17:41:33 -0500

    Fixed the Truth running to get everything working. Removed GeV from the names of the samples to keep it in line with the standard naming conventions we use. Added local CaloCalib as a temporary workaround

--- VandySkimmer/macros/run_VandySkimmerTruth.sh
+++ VandySkimmer/macros/run_VandySkimmerTruth.sh
@@ -11,5 +11,5 @@ source /opt/sphenix/core/bin/sphenix_setup.sh -n
 source /opt/sphenix/core/bin/setup_local.sh $installdir
 
 n_files=`wc -l < $caloDSTlist`
-root -b -q ${thisDir}/Fun4All_VandySkimmerTruth.C\(\"$caloDSTlist\",\"$jetDSTlist\",\"$truthDSTlist\",\"$globalDSTlist\",\"$outDir\",\"$n_files\"\)
+/cvmfs/sphenix.sdcc.bnl.gov/alma9.2-gcc-14.2.0/opt/sphenix/utils/bin/valgrind -s --track-origins=yes --log-file=valgrind.txt /cvmfs/sphenix.sdcc.bnl.gov/alma9.2-gcc-14.2.0/opt/sphenix/core/root-6.32.06/bin/root -b -q ${thisDir}/Fun4All_VandySkimmerTruth.C\(\"$caloDSTlist\",\"$jetDSTlist\",\"$truthDSTlist\",\"$globalDSTlist\",\"$outDir\",\"$n_files\"\)
 
