9e834e9fdce2756da04ee22fce02177d986b3839
Apurva Narde <58493193+Steepspace@users.noreply.github.com>
2026-04-25 15:59:04 -0400

    Calo_Calib: Add Verbosity for Skimmer
    - Add verbosity to CaloStatusSkimmer module

--- sEPD-Study/macros/Calo_Calib.C
+++ sEPD-Study/macros/Calo_Calib.C
@@ -49,6 +49,7 @@ void Process_Calo_Calib()
   if (!isSim)
   {
     CaloStatusSkimmer *css = new CaloStatusSkimmer("CaloStatusSkimmer");
+    se->Verbosity(1);
     se->registerSubsystem(css);
   }
 
