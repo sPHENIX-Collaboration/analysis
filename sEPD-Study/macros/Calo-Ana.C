38e939d56758797e0254b53f5b312e7aef206288
Apurva Narde <58493193+Steepspace@users.noreply.github.com>
2026-08-23 20:29:44 -0400

    Calo-Ana: Add Jets

--- sEPD-Study/macros/Calo-Ana.C
+++ sEPD-Study/macros/Calo-Ana.C
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <map>
 #include <memory>
+#include <optional>
 #include <stdexcept>
 #include <string>
 #include <unordered_set>
@@ -51,10 +52,12 @@ class CaloAnalysis
   void set_do_iter(bool b = true) { m_do_iter = b; }
   void set_do_mult(bool b = true) { m_do_mult = b; }
   void set_do_rcone(bool b = true) { m_do_rcone = b; }
+  void set_do_jets(bool b = true) { m_do_jets = b; }
 
   bool get_do_iter() const { return m_do_iter; }
   bool get_do_mult() const { return m_do_mult; }
   bool get_do_rcone() const { return m_do_rcone; }
+  bool get_do_jets() const { return m_do_jets; }
 
   void set_verbosity(int verbosity) { m_verbosity = verbosity; }
   int get_verbosity() const { return m_verbosity; }
@@ -123,6 +126,27 @@ class CaloAnalysis
 
     std::vector<int> *rcone_r03_ohcal_tower_index{nullptr};
     std::vector<double> *rcone_r03_sub1_ohcal_tower_energy{nullptr};
+
+    // Iterative Subtracted Jet Constituent detailed towers
+    // R = 0.2
+    std::vector<std::vector<int>> *emcal_tower_index_iter_r02{nullptr};
+    std::vector<std::vector<double>> *emcal_tower_energy_iter_r02{nullptr};
+
+    std::vector<std::vector<int>> *ihcal_tower_index_iter_r02{nullptr};
+    std::vector<std::vector<double>> *ihcal_tower_energy_iter_r02{nullptr};
+
+    std::vector<std::vector<int>> *ohcal_tower_index_iter_r02{nullptr};
+    std::vector<std::vector<double>> *ohcal_tower_energy_iter_r02{nullptr};
+
+    // R = 0.3
+    std::vector<std::vector<int>> *emcal_tower_index_iter_r03{nullptr};
+    std::vector<std::vector<double>> *emcal_tower_energy_iter_r03{nullptr};
+
+    std::vector<std::vector<int>> *ihcal_tower_index_iter_r03{nullptr};
+    std::vector<std::vector<double>> *ihcal_tower_energy_iter_r03{nullptr};
+
+    std::vector<std::vector<int>> *ohcal_tower_index_iter_r03{nullptr};
+    std::vector<std::vector<double>> *ohcal_tower_energy_iter_r03{nullptr};
   };
 
   struct TowerDataGroup
@@ -131,7 +155,20 @@ class CaloAnalysis
     std::string title_desc;
     const std::vector<int> *indices{nullptr};
     const std::vector<double> *energies{nullptr};
+    std::optional<int> jet_idx{std::nullopt};
     bool is_base_emcal{false};
+  };
+
+  struct JetCaloTowerPointers
+  {
+    std::string r_tag;   // "r02" or "r03"
+    std::string r_desc;  // "R=0.2" or "R=0.3"
+    const std::vector<std::vector<int>> *emcal_idx{nullptr};
+    const std::vector<std::vector<double>> *emcal_e{nullptr};
+    const std::vector<std::vector<int>> *ihcal_idx{nullptr};
+    const std::vector<std::vector<double>> *ihcal_e{nullptr};
+    const std::vector<std::vector<int>> *ohcal_idx{nullptr};
+    const std::vector<std::vector<double>> *ohcal_e{nullptr};
   };
 
   // --- Member Variables ---
@@ -146,12 +183,14 @@ class CaloAnalysis
   bool m_do_iter{true};
   bool m_do_mult{false};
   bool m_do_rcone{true};
+  bool m_do_jets{true};
 
   std::map<std::string, std::unique_ptr<TH2>> m_hists2D;
 
   // --- Private Helper Methods ---
   void setup_chain();
   void make_and_fill_hist(const TowerDataGroup &group, int run, int event);
+  void process_jet_collection(const JetCaloTowerPointers &jet_ptrs, int run, int event);
   void process_events();
   void save_results() const;
 };
@@ -210,6 +249,18 @@ void CaloAnalysis::setup_chain()
         "rcone_r03_emcal_tower_index", "rcone_r03_sub1_emcal_tower_energy",
         "rcone_r03_ihcal_tower_index", "rcone_r03_sub1_ihcal_tower_energy",
         "rcone_r03_ohcal_tower_index", "rcone_r03_sub1_ohcal_tower_energy"
+    });
+  }
+
+  if (m_do_jets && m_do_iter)
+  {
+    branchNames.insert({
+        "emcal_tower_index_iter_r02", "emcal_tower_energy_iter_r02",
+        "ihcal_tower_index_iter_r02", "ihcal_tower_energy_iter_r02",
+        "ohcal_tower_index_iter_r02", "ohcal_tower_energy_iter_r02",
+        "emcal_tower_index_iter_r03", "emcal_tower_energy_iter_r03",
+        "ihcal_tower_index_iter_r03", "ihcal_tower_energy_iter_r03",
+        "ohcal_tower_index_iter_r03", "ohcal_tower_energy_iter_r03"
     });
   }
 
@@ -290,6 +341,23 @@ void CaloAnalysis::setup_chain()
     m_chain->SetBranchAddress("rcone_r03_sub1_ohcal_tower_energy", &m_event_data.rcone_r03_sub1_ohcal_tower_energy);
   }
 
+  if (m_do_jets && m_do_iter)
+  {
+    m_chain->SetBranchAddress("emcal_tower_index_iter_r02", &m_event_data.emcal_tower_index_iter_r02);
+    m_chain->SetBranchAddress("emcal_tower_energy_iter_r02", &m_event_data.emcal_tower_energy_iter_r02);
+    m_chain->SetBranchAddress("ihcal_tower_index_iter_r02", &m_event_data.ihcal_tower_index_iter_r02);
+    m_chain->SetBranchAddress("ihcal_tower_energy_iter_r02", &m_event_data.ihcal_tower_energy_iter_r02);
+    m_chain->SetBranchAddress("ohcal_tower_index_iter_r02", &m_event_data.ohcal_tower_index_iter_r02);
+    m_chain->SetBranchAddress("ohcal_tower_energy_iter_r02", &m_event_data.ohcal_tower_energy_iter_r02);
+
+    m_chain->SetBranchAddress("emcal_tower_index_iter_r03", &m_event_data.emcal_tower_index_iter_r03);
+    m_chain->SetBranchAddress("emcal_tower_energy_iter_r03", &m_event_data.emcal_tower_energy_iter_r03);
+    m_chain->SetBranchAddress("ihcal_tower_index_iter_r03", &m_event_data.ihcal_tower_index_iter_r03);
+    m_chain->SetBranchAddress("ihcal_tower_energy_iter_r03", &m_event_data.ihcal_tower_energy_iter_r03);
+    m_chain->SetBranchAddress("ohcal_tower_index_iter_r03", &m_event_data.ohcal_tower_index_iter_r03);
+    m_chain->SetBranchAddress("ohcal_tower_energy_iter_r03", &m_event_data.ohcal_tower_energy_iter_r03);
+  }
+
   std::cout << "Finished... setup_chain" << std::endl;
 }
 
@@ -303,9 +371,21 @@ void CaloAnalysis::make_and_fill_hist(const TowerDataGroup &group, int run, int 
   int n_eta = group.is_base_emcal ? CaloGeometry::CEMC_ETA_BINS : CaloGeometry::HCAL_ETA_BINS;
   int n_phi = group.is_base_emcal ? CaloGeometry::CEMC_PHI_BINS : CaloGeometry::HCAL_PHI_BINS;
 
-  std::string name = std::format("h2_{}_energy_run{}_event{}", group.tag, run, event);
-  std::string title = std::format("{} (Run {}, Event {}); Tower #eta bin; Tower #phi bin; Energy [GeV]",
-                                  group.title_desc, run, event);
+  std::string name;
+  std::string title;
+
+  if (group.jet_idx.has_value())
+  {
+    name = std::format("h2_{}_energy_run{}_event{}_jet{}", group.tag, run, event, *group.jet_idx);
+    title = std::format("{} (Run {}, Event {}, Jet {}); Tower #eta bin; Tower #phi bin; Energy [GeV]",
+                        group.title_desc, run, event, *group.jet_idx);
+  }
+  else
+  {
+    name = std::format("h2_{}_energy_run{}_event{}", group.tag, run, event);
+    title = std::format("{} (Run {}, Event {}); Tower #eta bin; Tower #phi bin; Energy [GeV]",
+                        group.title_desc, run, event);
+  }
 
   auto h2 = std::make_unique<TH2F>(
       name.c_str(), title.c_str(),
@@ -323,6 +403,38 @@ void CaloAnalysis::make_and_fill_hist(const TowerDataGroup &group, int run, int 
   }
 
   m_hists2D[name] = std::move(h2);
+}
+
+void CaloAnalysis::process_jet_collection(const JetCaloTowerPointers &jet_ptrs, int run, int event)
+{
+  if (!jet_ptrs.emcal_idx)
+  {
+    return;
+  }
+
+  size_t n_jets = jet_ptrs.emcal_idx->size();
+  for (size_t j = 0; j < n_jets; ++j)
+  {
+    auto get_vec = [](const auto *nested, size_t idx) -> const auto * {
+      return (nested && idx < nested->size()) ? &(*nested)[idx] : nullptr;
+    };
+
+    int j_int = static_cast<int>(j);
+    make_and_fill_hist({std::format("jet_iter_emcal_{}", jet_ptrs.r_tag),
+                        std::format("Iter Sub1 Jet {} EMCal Energy", jet_ptrs.r_desc),
+                        get_vec(jet_ptrs.emcal_idx, j), get_vec(jet_ptrs.emcal_e, j),
+                        j_int, false}, run, event);
+
+    make_and_fill_hist({std::format("jet_iter_ihcal_{}", jet_ptrs.r_tag),
+                        std::format("Iter Sub1 Jet {} IHCal Energy", jet_ptrs.r_desc),
+                        get_vec(jet_ptrs.ihcal_idx, j), get_vec(jet_ptrs.ihcal_e, j),
+                        j_int, false}, run, event);
+
+    make_and_fill_hist({std::format("jet_iter_ohcal_{}", jet_ptrs.r_tag),
+                        std::format("Iter Sub1 Jet {} OHCal Energy", jet_ptrs.r_desc),
+                        get_vec(jet_ptrs.ohcal_idx, j), get_vec(jet_ptrs.ohcal_e, j),
+                        j_int, false}, run, event);
+  }
 }
 
 void CaloAnalysis::process_events()
@@ -348,42 +460,63 @@ void CaloAnalysis::process_events()
     int event = m_event_data.event;
 
     std::vector<TowerDataGroup> active_groups = {
-        {"emcal_base", "EMCal Base Energy", m_event_data.emcal_base_tower_index, m_event_data.emcal_base_tower_energy, true},
-        {"emcal_retower", "EMCal Retower Energy", m_event_data.emcal_retower_tower_index, m_event_data.emcal_retower_tower_energy, false},
-        {"ihcal", "IHCal Energy", m_event_data.ihcal_tower_index, m_event_data.ihcal_tower_energy, false},
-        {"ohcal", "OHCal Energy", m_event_data.ohcal_tower_index, m_event_data.ohcal_tower_energy, false},
+        {"emcal_base", "EMCal Base Energy", m_event_data.emcal_base_tower_index, m_event_data.emcal_base_tower_energy, std::nullopt, true},
+        {"emcal_retower", "EMCal Retower Energy", m_event_data.emcal_retower_tower_index, m_event_data.emcal_retower_tower_energy, std::nullopt, false},
+        {"ihcal", "IHCal Energy", m_event_data.ihcal_tower_index, m_event_data.ihcal_tower_energy, std::nullopt, false},
+        {"ohcal", "OHCal Energy", m_event_data.ohcal_tower_index, m_event_data.ohcal_tower_energy, std::nullopt, false},
     };
 
     if (m_do_iter)
     {
-      active_groups.push_back({"iter_emcal", "Iter Sub1 EMCal Energy", m_event_data.iter_emcal_tower_index, m_event_data.iter_emcal_tower_energy, false});
-      active_groups.push_back({"iter_ihcal", "Iter Sub1 IHCal Energy", m_event_data.iter_ihcal_tower_index, m_event_data.iter_ihcal_tower_energy, false});
-      active_groups.push_back({"iter_ohcal", "Iter Sub1 OHCal Energy", m_event_data.iter_ohcal_tower_index, m_event_data.iter_ohcal_tower_energy, false});
+      active_groups.push_back({"iter_emcal", "Iter Sub1 EMCal Energy", m_event_data.iter_emcal_tower_index, m_event_data.iter_emcal_tower_energy, std::nullopt, false});
+      active_groups.push_back({"iter_ihcal", "Iter Sub1 IHCal Energy", m_event_data.iter_ihcal_tower_index, m_event_data.iter_ihcal_tower_energy, std::nullopt, false});
+      active_groups.push_back({"iter_ohcal", "Iter Sub1 OHCal Energy", m_event_data.iter_ohcal_tower_index, m_event_data.iter_ohcal_tower_energy, std::nullopt, false});
     }
 
     if (m_do_mult)
     {
-      active_groups.push_back({"mult_emcal", "Mult Sub1 EMCal Energy", m_event_data.mult_emcal_tower_index, m_event_data.mult_emcal_tower_energy, false});
-      active_groups.push_back({"mult_ihcal", "Mult Sub1 IHCal Energy", m_event_data.mult_ihcal_tower_index, m_event_data.mult_ihcal_tower_energy, false});
-      active_groups.push_back({"mult_ohcal", "Mult Sub1 OHCal Energy", m_event_data.mult_ohcal_tower_index, m_event_data.mult_ohcal_tower_energy, false});
+      active_groups.push_back({"mult_emcal", "Mult Sub1 EMCal Energy", m_event_data.mult_emcal_tower_index, m_event_data.mult_emcal_tower_energy, std::nullopt, false});
+      active_groups.push_back({"mult_ihcal", "Mult Sub1 IHCal Energy", m_event_data.mult_ihcal_tower_index, m_event_data.mult_ihcal_tower_energy, std::nullopt, false});
+      active_groups.push_back({"mult_ohcal", "Mult Sub1 OHCal Energy", m_event_data.mult_ohcal_tower_index, m_event_data.mult_ohcal_tower_energy, std::nullopt, false});
     }
 
     if (m_do_rcone)
     {
       // R = 0.2 subtracted random cone
-      active_groups.push_back({"rcone_sub1_emcal_r02", "RCone R=0.2 Sub1 EMCal Energy", m_event_data.rcone_r02_emcal_tower_index, m_event_data.rcone_r02_sub1_emcal_tower_energy, false});
-      active_groups.push_back({"rcone_sub1_ihcal_r02", "RCone R=0.2 Sub1 IHCal Energy", m_event_data.rcone_r02_ihcal_tower_index, m_event_data.rcone_r02_sub1_ihcal_tower_energy, false});
-      active_groups.push_back({"rcone_sub1_ohcal_r02", "RCone R=0.2 Sub1 OHCal Energy", m_event_data.rcone_r02_ohcal_tower_index, m_event_data.rcone_r02_sub1_ohcal_tower_energy, false});
+      active_groups.push_back({"rcone_sub1_emcal_r02", "RCone R=0.2 Sub1 EMCal Energy", m_event_data.rcone_r02_emcal_tower_index, m_event_data.rcone_r02_sub1_emcal_tower_energy, std::nullopt, false});
+      active_groups.push_back({"rcone_sub1_ihcal_r02", "RCone R=0.2 Sub1 IHCal Energy", m_event_data.rcone_r02_ihcal_tower_index, m_event_data.rcone_r02_sub1_ihcal_tower_energy, std::nullopt, false});
+      active_groups.push_back({"rcone_sub1_ohcal_r02", "RCone R=0.2 Sub1 OHCal Energy", m_event_data.rcone_r02_ohcal_tower_index, m_event_data.rcone_r02_sub1_ohcal_tower_energy, std::nullopt, false});
 
       // R = 0.3 subtracted random cone
-      active_groups.push_back({"rcone_sub1_emcal_r03", "RCone R=0.3 Sub1 EMCal Energy", m_event_data.rcone_r03_emcal_tower_index, m_event_data.rcone_r03_sub1_emcal_tower_energy, false});
-      active_groups.push_back({"rcone_sub1_ihcal_r03", "RCone R=0.3 Sub1 IHCal Energy", m_event_data.rcone_r03_ihcal_tower_index, m_event_data.rcone_r03_sub1_ihcal_tower_energy, false});
-      active_groups.push_back({"rcone_sub1_ohcal_r03", "RCone R=0.3 Sub1 OHCal Energy", m_event_data.rcone_r03_ohcal_tower_index, m_event_data.rcone_r03_sub1_ohcal_tower_energy, false});
+      active_groups.push_back({"rcone_sub1_emcal_r03", "RCone R=0.3 Sub1 EMCal Energy", m_event_data.rcone_r03_emcal_tower_index, m_event_data.rcone_r03_sub1_emcal_tower_energy, std::nullopt, false});
+      active_groups.push_back({"rcone_sub1_ihcal_r03", "RCone R=0.3 Sub1 IHCal Energy", m_event_data.rcone_r03_ihcal_tower_index, m_event_data.rcone_r03_sub1_ihcal_tower_energy, std::nullopt, false});
+      active_groups.push_back({"rcone_sub1_ohcal_r03", "RCone R=0.3 Sub1 OHCal Energy", m_event_data.rcone_r03_ohcal_tower_index, m_event_data.rcone_r03_sub1_ohcal_tower_energy, std::nullopt, false});
     }
 
     for (const auto &group : active_groups)
     {
       make_and_fill_hist(group, run, event);
+    }
+
+    if (m_do_jets && m_do_iter)
+    {
+      // R = 0.2 jets
+      JetCaloTowerPointers r02_ptrs{
+          "r02", "R=0.2",
+          m_event_data.emcal_tower_index_iter_r02, m_event_data.emcal_tower_energy_iter_r02,
+          m_event_data.ihcal_tower_index_iter_r02, m_event_data.ihcal_tower_energy_iter_r02,
+          m_event_data.ohcal_tower_index_iter_r02, m_event_data.ohcal_tower_energy_iter_r02
+      };
+      process_jet_collection(r02_ptrs, run, event);
+
+      // R = 0.3 jets
+      JetCaloTowerPointers r03_ptrs{
+          "r03", "R=0.3",
+          m_event_data.emcal_tower_index_iter_r03, m_event_data.emcal_tower_energy_iter_r03,
+          m_event_data.ihcal_tower_index_iter_r03, m_event_data.ihcal_tower_energy_iter_r03,
+          m_event_data.ohcal_tower_index_iter_r03, m_event_data.ohcal_tower_energy_iter_r03
+      };
+      process_jet_collection(r03_ptrs, run, event);
     }
   }
 }
@@ -425,9 +558,9 @@ int main(int argc, const char *const argv[])
   gROOT->SetBatch(true);
   TH1::AddDirectory(false);
 
-  if (argc < 2 || argc > 8)
+  if (argc < 2 || argc > 9)
   {
-    std::cout << "Usage: " << argv[0] << " input_file [events] [output_directory] [verbosity] [do_iter] [do_mult] [do_rcone]\n";
+    std::cout << "Usage: " << argv[0] << " input_file [events] [output_directory] [verbosity] [do_iter] [do_mult] [do_rcone] [do_jets]\n";
     return 1;
   }
 
@@ -439,6 +572,7 @@ int main(int argc, const char *const argv[])
   bool do_iter = (argc >= ctr + 1) ? (std::atoi(argv[ctr++]) != 0) : true;
   bool do_mult = (argc >= ctr + 1) ? (std::atoi(argv[ctr++]) != 0) : false;
   bool do_rcone = (argc >= ctr + 1) ? (std::atoi(argv[ctr++]) != 0) : true;
+  bool do_jets = (argc >= ctr + 1) ? (std::atoi(argv[ctr++]) != 0) : true;
 
   std::cout << std::format("{:#<20}\n", "");
   std::cout << "Calo-Ana Parameters\n";
@@ -449,6 +583,7 @@ int main(int argc, const char *const argv[])
   std::cout << std::format("Do Iter: {}\n", do_iter);
   std::cout << std::format("Do Mult: {}\n", do_mult);
   std::cout << std::format("Do RCone: {}\n", do_rcone);
+  std::cout << std::format("Do Jets: {}\n", do_jets);
   std::cout << std::format("{:#<20}\n", "");
 
   try
@@ -458,6 +593,7 @@ int main(int argc, const char *const argv[])
     analysis.set_do_iter(do_iter);
     analysis.set_do_mult(do_mult);
     analysis.set_do_rcone(do_rcone);
+    analysis.set_do_jets(do_jets);
     analysis.run();
   }
   catch (const std::exception &e)
