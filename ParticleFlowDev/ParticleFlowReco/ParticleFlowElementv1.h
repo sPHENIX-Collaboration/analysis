f6fa5f88404cdfe53f2a6b48abed27511c39c5ab
Antonio Silva <antonio.ifusp@gmail.com>
2026-08-31 11:19:39 -0400

    Adding more files

---
+++ ParticleFlowDev/ParticleFlowReco/ParticleFlowElementv1.h
@@ -0,0 +1,89 @@
+#ifndef PARTICLEFLOW_PARTICLEFLOWELEMENTV1_H
+#define PARTICLEFLOW_PARTICLEFLOWELEMENTV1_H
+
+//===========================================================
+/// \file ParticleFlowElementv1.h
+/// \brief v1 Implementation of ParticleFlowElement base class
+/// \author Dennis V. Perepelitsa
+//===========================================================
+
+#include "ParticleFlowElement.h"
+
+#include <iostream>
+#include <utility>  // for pair, make_pair
+
+class SvtxTrack;
+class RawCluster;
+
+class ParticleFlowElementv1 : public ParticleFlowElement
+{
+ public:
+  ParticleFlowElementv1() = default;
+  ~ParticleFlowElementv1() override = default;
+
+  // PHObject virtual overloads
+
+  void identify(std::ostream& os = std::cout) const override;
+  void Reset() override;
+  int isValid() const override;
+
+  // pflow element info
+
+  ParticleFlowElement::PFLOWTYPE get_type() const override { return _type; }
+  void set_type(ParticleFlowElement::PFLOWTYPE type) override { _type = type; }
+
+  unsigned int get_id() const override { return _id; }
+  void set_id(unsigned int id) override { _id = id; }
+
+  float get_px() const override { return _mom[0]; }
+  void set_px(float px) override { _mom[0] = px; }
+
+  float get_py() const override { return _mom[1]; }
+  void set_py(float py) override { _mom[1] = py; }
+
+  float get_pz() const override { return _mom[2]; }
+  void set_pz(float pz) override { _mom[2] = pz; }
+
+  float get_e() const override { return _e; }
+  void set_e(float e) override { _e = e; }
+
+  SvtxTrack* get_track() const override { return _track; }
+  void set_track(SvtxTrack* track) override { _track = track; }
+
+  std::vector<RawCluster*> get_eclusters() const override { return _ecluster; }
+  void set_eclusters(const std::vector<RawCluster*>& ecluster) override { _ecluster = ecluster; }
+
+  std::vector<SvtxTrack*> get_assoc_tracks() const override { return _assoc_tracks; }
+  void set_assoc_tracks(const std::vector<SvtxTrack*>& ecluster) override { _assoc_tracks = ecluster; }
+
+  RawCluster* get_hcluster() const override { return _hcluster; }
+  void set_hcluster(RawCluster* hcluster) override { _hcluster = hcluster; }
+
+  float get_p() const override;
+  float get_pt() const override;
+  float get_et() const override;
+  float get_eta() const override;
+  float get_phi() const override;
+  float get_mass() const override;
+
+ private:
+  /// unique identifier within container
+  unsigned int _id {0};
+
+  // particle flow type
+  ParticleFlowElement::PFLOWTYPE _type {ParticleFlowElement::PFLOWTYPE::UNASSIGNED};
+
+  /// pflow momentum vector (px,py,pz)
+  float _mom[3]{std::numeric_limits<float>::quiet_NaN()};
+
+  /// pflow energy
+  float _e{std::numeric_limits<float>::quiet_NaN()};
+
+  SvtxTrack* _track {nullptr};
+  std::vector<RawCluster*> _ecluster;
+  std::vector<SvtxTrack*> _assoc_tracks;
+  RawCluster* _hcluster {nullptr};
+  ClassDefOverride(ParticleFlowElementv1, 1);
+};
+
+#endif
---
+++ ParticleFlowDev/README.md
@@ -0,0 +1,7 @@
+# Particle Flow development files
+
+Here you will find a new version of the Particle Flow reconstruction and evaluator code. The particle flow reconstruction includes some modifications in the particle flow element code too. As for the evaluator, it contains also Topo Cluster evaluators.
+
+All code here is tested, but maybe it's not straightforward to make it work. In specific, having the right objects such as RawTower vs TowerInfo, or the right EMCal geometry available will make a big difference.
+
+I will try to update this README file with more information soon. If I failed to do this, or if you are feeling hopeless, please contact me: antonio.sphenix@gmail.com 
