b8c754708cffe0ce8d8879da7bf3a45d6ab40d02
devloom <dloom@umich.edu>
2026-09-16 16:30:28 -0400

    comment of run3 first run

--- SpinDBQA/macros/run_SpinDBQA.C
+++ SpinDBQA/macros/run_SpinDBQA.C
@@ -2,9 +2,16 @@
 
 R__LOAD_LIBRARY(libSpinDBQA.so)
 
+// For Run 2 pp
 //First run in spin db: 41935
 //First official spin run: 45236
 //Final spin (pp) run: 53880
+
+// For run 3 pp
+//First run in spin db: 79145
+//First run with Hjet and pC data (first official spin run): 79243 (fill number 36468)
+//
+
 void run_SpinDBQA()
 {
     std::string runfile = "runnumbers.dat";
